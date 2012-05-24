43c38284f22532f6cbb8426202a4105018335d0b
Andy Durant <aj@ury.york.ac.uk>
2012-05-24 22:56:29 +0100

    manage podcasts view

---
+++ src/Views/MyURY/Podcast/managepods.php
@@ -0,0 +1,8 @@
+<?php
+require 'Views/MyURY/bootstrap.php';
+$twig->setTemplate('MyURY/podcast.twig')
+        ->addVariable('title', 'Podcast Manager')
+        ->addVariable('heading', 'Podcast Manager')
+        ->addVariable('standalone', $standalone)
+        ->addVariable('showlinked', $showlinked)
+        ->render();
