c77b10eeabc8b30404c32c60992c8ff11cfcadae
unknown <lpw@w8lt-lpw503.(none)>
2012-06-10 00:27:17 +0100

    singleton broke

--- src/Views/MyURY/Scheduler/default.php
+++ src/Views/MyURY/Scheduler/default.php
@@ -1,6 +1,6 @@
 <?php
 require 'Views/MyURY/Scheduler/bootstrap.php';
-$twig->setTemplate('menu.twig')
+$twig->setTemplate('MyURY/menu.twig')
         ->addVariable('heading', 'Scheduler')
         ->addVariable('menu', array($items))
         ->render();
