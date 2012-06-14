d5732f64efda57898dc86404b768c92161333e43
unknown <lpw@w8lt-lpw503.(none)>
2012-06-14 15:02:59 +0100

    Stuff

--- src/Models/MyURY/Scheduler/pendingAllocations.php
+++ src/Models/MyURY/Scheduler/pendingAllocations.php
@@ -3,5 +3,5 @@
 $pending_allocations = Scheduler::getPendingAllocations();
 
 for ($i = 0; $i < sizeof($pending_allocations); $i++) {
-  $pending_allocations[$i]['createddate'] = CoreUtils::happyTime($pending_allocations[$i]['createddate']);
+  $pending_allocations[$i]['createddate'] = CoreUtils::happyTime($pending_allocations[$i]['createddate'], false);
 }
