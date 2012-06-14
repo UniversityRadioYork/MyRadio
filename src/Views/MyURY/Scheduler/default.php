6246468556ad5c91c7acc490ee09eea922fb141b
unknown <lpw@w8lt-lpw503.(none)>
2012-06-14 14:51:27 +0100

    Stuff

--- src/Views/MyURY/Scheduler/default.php
+++ src/Views/MyURY/Scheduler/default.php
@@ -3,5 +3,6 @@ require 'Views/MyURY/Scheduler/bootstrap.php';
 $twig->setTemplate('table.twig')
         ->addVariable('tablescript', 'schedule_pending')
         ->addVariable('heading', 'Scheduler')
+        ->addVariable('tableheaders', array('summary' => 'Summary', 'createddate' => 'Submitted', 'requestedtime', 'Requested Time'))
         ->addVariable('tabledata', $pending_allocations)
         ->render();
