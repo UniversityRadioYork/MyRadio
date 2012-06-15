e3c63403ed56f8a163b0860955fdee0c16bb4f46
unknown <lpw@w8lt-lpw503.(none)>
2012-06-15 20:15:05 +0100

    Added permission caching

--- src/Views/MyURY/Scheduler/default.php
+++ src/Views/MyURY/Scheduler/default.php
@@ -1,6 +1,14 @@
 <?php
 require 'Views/MyURY/Scheduler/bootstrap.php';
 
+foreach ($pending_allocations as $k => $v) {
+  $pending_allocations[$k]['editlink'] = array(
+      'display' => 'text',
+      'url' => CoreUtils::makeURL('Scheduler', 'entry'),
+      'value' => 'Edit'
+      );
+}
+
 $twig->setTemplate('table.twig')
         ->addVariable('tablescript', 'myury.scheduler.pending')
         ->addVariable('heading', 'Scheduler')
