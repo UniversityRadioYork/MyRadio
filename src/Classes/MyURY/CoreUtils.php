fc8daac171b64eb9f937e2ef0f0f38a9278ae2b9
unknown <lpw@w8lt-lpw503.(none)>
2012-06-15 18:20:54 +0100

    Added permission caching

--- src/Classes/MyURY/CoreUtils.php
+++ src/Classes/MyURY/CoreUtils.php
@@ -84,7 +84,6 @@ class CoreUtils {
     
     $db = Database::getInstance();
     $result = $db->fetch_all('SELECT typeid, phpconstant FROM l_action');
-    print_r($result);
     foreach ($result as $row) {
       define($row['phpconstant'], $row['typeid']);
     }
