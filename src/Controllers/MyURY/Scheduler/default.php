5417cf3c37a656213888bcf324e1bb34cf19faf0
unknown <lpw@w8lt-lpw503.(none)>
2012-06-15 18:21:30 +0100

    Added permission caching

--- src/Controllers/MyURY/Scheduler/default.php
+++
@@ -1,6 +0,0 @@
-<?php
-CoreUtils::requirePermission(AUTH_ALLOCATESHOWS);
-
-require 'Models/MyURY/Scheduler/notices.php';
-require 'Models/MyURY/Scheduler/pendingAllocations.php';
-require 'Views/MyURY/Scheduler/default.php';
--- src/Controllers/MyURY/scheduler/default.php
+++
@@ -1,6 +0,0 @@
-<?php
-CoreUtils::requirePermission(AUTH_ALLOCATESLOTS);
-
-require 'Models/MyURY/Scheduler/notices.php';
-require 'Models/MyURY/Scheduler/pendingAllocations.php';
-require 'Views/MyURY/Scheduler/default.php';
