6b6fdef23fd2d3aeec31d71d537d51ad16826cc0
unknown <lpw@w8lt-lpw503.(none)>
2012-06-15 18:07:35 +0100

    Added permission caching

--- src/Controllers/MyURY/scheduler/default.php
+++ src/Controllers/MyURY/scheduler/default.php
@@ -1,5 +1,6 @@
 <?php
 CoreUtils::requirePermission(AUTH_ALLOCATESHOWS);
+print_r($_SESSION);
 
 require 'Models/MyURY/Scheduler/notices.php';
 require 'Models/MyURY/Scheduler/pendingAllocations.php';
