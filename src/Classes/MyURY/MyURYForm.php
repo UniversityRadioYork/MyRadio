d33e3ec345cc997e404bf23d4f64e5936874828f
unknown <lpw@lpw503-dt.(none)>
2012-06-17 18:17:23 +0100

    MyURY

--- src/Classes/MyURY/MyURYForm.php
+++ src/Classes/MyURY/MyURYForm.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Abstractor for MyURY Form Definitions
  * 
@@ -12,61 +13,72 @@
  *   and calls getter to interpret them
  */
 class MyURYForm {
+
   /**
    * The name of the form
    * @var string 
    */
   private $name = 'autofrm';
+
   /**
    * The module that it will submit to
    * @var string 
    */
   private $module;
+
   /**
    * The action that it will submit to
    * @var string 
    */
   private $action;
+
   /**
    * Whether to enable detailed output of what is happening (or isn't)
    * @var boolean 
    */
   private $debug = false;
+
   /**
    * Additional classes to add to the base form element
    * @var array 
    */
   private $classes = array();
+
   /**
    * Whether to enable Form Validation
    * @var boolean 
    */
   private $validate = true;
+
   /**
    * Whether to use GET instead of POST 
    */
   private $get = false;
+
   /**
    * The Twig template to use for the form. Must be form.twig or a child.
    * @var string 
    */
   private $template = 'form.twig';
+
   /**
    * The form fields in the form (an array of MyURYFormField objects)
    * @var array 
    */
   private $fields = array();
+
   /**
    * Logging output
    * @var array 
    */
   private $debug_log = array();
+
   /**
    * Fields that cannot be edited by params
    * @var array 
    */
   private $restricted_fields = array('name', 'module', 'action', 'fields', 'restricted_fields', 'debug_log');
-  
+
   /**
    * Creates a new MyURYForm object with the given parameters
    * @param string $name The name/id of the form
@@ -90,38 +102,42 @@ class MyURYForm {
     $this->name = $name;
     $this->module = $module;
     $this->action = $action;
-    
+
     //Check all optional parameters
     foreach ($params as $k => $v) {
       //Sanity checks - is this a valid parameter and is it not blacklisted?
-      if (!isset($this->$k)) throw new MyURYException('Tried to set MyURYForm parameter '.$k.' but it does not exist.');
-      if (in_array($k, $this->restricted_fields)) throw new MyURYException('Tried to set MyURYForm parameter '.$k.' but it is not editable.');
+      if (!isset($this->$k))
+        throw new MyURYException('Tried to set MyURYForm parameter ' . $k . ' but it does not exist.');
+      if (in_array($k, $this->restricted_fields))
+        throw new MyURYException('Tried to set MyURYForm parameter ' . $k . ' but it is not editable.');
       $this->$k = $v;
     }
   }
-  
+
   public function addField(MyURYFormField $field) {
     //Sanity check - is this name in use
     foreach ($this->fields as $f) {
-      if ($f->getName() === $field->getName()) throw new MyURYException('Tried to create a duplicate MyURYFormField '.$f->getName());
+      if ($f->getName() === $field->getName())
+        throw new MyURYException('Tried to create a duplicate MyURYFormField ' . $f->getName());
     }
     $this->fields[] = $field;
   }
-  
+
   public function render() {
-    $GLOBALS['twig']->setTemplate($this->template)
+    CoreUtils::getTemplateObject()->setTemplate($this->template)
             ->addVariable('classes', $this->getClasses())
             ->addVariable('action', CoreUtils::makeURL($this->module, $this->action))
             ->addVariable('method', $this->get ? 'get' : 'post')
             ->render();
   }
-  
+
   private function getClasses() {
     $classes = 'myuryfrm';
     foreach ($this->classes as $class) {
       $classes .= " $class";
     }
-    
+
     return $classes;
   }
+
 }
