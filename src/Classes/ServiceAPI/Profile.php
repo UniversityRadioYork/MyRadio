e798761430332878e548d0c6a33cd8d47f8a03db
aj <aj@wallpc1.ad.ury.york.ac.uk>
2012-06-18 00:53:19 +0100

    get members for listing

---
+++ src/Classes/ServiceAPI/Profile.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Abstractor for the Profile Module
+ *
+ * @author aj
+ */
+class Profile extends ServiceAPI {
+  private static $allMembers = null;
+  
+  public static function getAllMembers() {
+      // @todo: set cache
+    self::initDB();
+    if (self::$allMembers === null) {
+      self::$allMembers = 
+        self::$db->fetch_all('SELECT member.memberid, fname, sname, college, year, paid
+        FROM member LEFT JOIN (SELECT * FROM member_year WHERE year = $1) AS member_year
+        ON ( member.memberid = member_year.memberid )
+        ORDER BY sname ASC', CoreUtils::getAcademicYear());
+    }
+    
+    return self::$allMembers;
+  }
+  
+  public static function getThisYearsMembers() {
+      // @todo: set cache
+    self::initDB();
+    if (self::$allMembers === null) {
+      self::$allMembers = 
+        self::$db->fetch_all('SELECT member.memberid, fname, sname, college, year, paid
+        FROM member INNER JOIN (SELECT * FROM member_year WHERE year = $1) AS member_year
+        ON ( member.memberid = member_year.memberid )
+        ORDER BY sname ASC', CoreUtils::getAcademicYear());
+    }
+    
+    return self::$allMembers;
+  }
+  
+  public static function getInstance($serviceObjectId = -1) {
+      throw new MyURYException('Something, Something, Something, Profile. Something, Something, Something, MyURY.');
+  }
+}
