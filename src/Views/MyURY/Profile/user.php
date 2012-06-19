802ece7655c5c8e5f19eada793550f93e91fa986
Andy Durant <aj@ury.york.ac.uk>
2012-06-19 01:45:22 +0100

    info -> twig

--- src/Views/MyURY/Profile/user.php
+++ src/Views/MyURY/Profile/user.php
@@ -1,3 +1,23 @@
 <?php
+require 'Views/MyURY/Profile/bootstrap.php';
 
 
+$twig->setTemplate('MyURY/Profile/user.twig')
+        ->addVariable('title', 'View Member')
+        ->addVariable('heading', 'View Profile')
+        ->addVariable('name', $name)
+        ->addVariable('sex', $sex)
+        ->addVariable('college', $college)
+        ->addVariable('phone', $phone)
+        ->addVariable('uni', $uni)
+        ->addVariable('email', $email)
+        ->addVariable('local_alias', $local_alias)
+        ->addVariable('local_account', $local_account)
+        ->addVariable('receive_email', $receive_email)
+        ->addVariable('account_locked', $account_locked)
+        ->addVariable('last_login', $last_login)
+        // @todo use an array for all this data
+        // @todo use a separate array for years paid
+        // @todo use an array for officerships held
+        // @todo use an array for training status
+        ->render();
