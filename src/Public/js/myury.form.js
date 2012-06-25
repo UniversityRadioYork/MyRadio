cc07838d413a8d7fd4437ff0bc28d3273cd40258
Lloyd Wallis <lpw@ury.york.ac.uk>
2012-06-25 13:11:13 +0100

    Form stuff

--- src/Public/js/myury.form.js
+++ src/Public/js/myury.form.js
@@ -7,6 +7,15 @@ $(document).ready(function() {
   /**
    * Initialises the Date pickers where necessary
    */
-  $('fieldset.myuryfrm input.date').datepicker({dateFormat:"dd/mm/yy"});
-  $('fieldset.myuryfrm input.datetime').datetimepicker({dateFormat:"dd/mm/yy"});
+  $('fieldset.myuryfrm input.date').datepicker({
+    dateFormat:"dd/mm/yy"
+  });
+  /**
+   * Initialises the Datetime pickers where necessary
+   * @todo Make stepminute customisable?
+   */
+  $('fieldset.myuryfrm input.datetime').datetimepicker({
+    dateFormat:"dd/mm/yy",
+    stepMinute: 15
+  });
 });
