e7e2cc704f1c69d8a91bbdb9ba0489866340ccc9
Lloyd Wallis <lpw@ury.york.ac.uk>
2012-06-25 14:00:09 +0100

    Form stuff

--- src/Controllers/MyURY/Core/a-findmember.php
+++ src/Controllers/MyURY/Core/a-findmember.php
@@ -5,5 +5,5 @@
  */
 if (!isset($_REQUEST['term'])) throw new MyURYException('Parameter \'term\' is required but was not provided');
 
-$data = User::findByName($_REQUEST['name'], isset($_REQUEST['limit']) ? intval($_REQUEST['limit']) : Config::$ajax_limit_default);
+$data = User::findByName($_REQUEST['term'], isset($_REQUEST['limit']) ? intval($_REQUEST['limit']) : Config::$ajax_limit_default);
 require 'Views/MyURY/Core/datatojson.php';
