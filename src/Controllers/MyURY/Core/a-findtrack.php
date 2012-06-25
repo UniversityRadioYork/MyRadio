406009441af2df40f808d40b4d3216d001989a04
Lloyd Wallis <lpw@ury.york.ac.uk>
2012-06-25 14:38:17 +0100

    Form stuff

--- src/Controllers/MyURY/Core/a-findtrack.php
+++ src/Controllers/MyURY/Core/a-findtrack.php
@@ -7,5 +7,5 @@
  */
 if (!isset($_REQUEST['term'])) throw new MyURYException('Parameter \'term\' is required but was not provided');
 
-$data = User::findByName($_REQUEST['term'], isset($_REQUEST['limit']) ? intval($_REQUEST['limit']) : Config::$ajax_limit_default);
+$data = Track::findByName($_REQUEST['term'], isset($_REQUEST['limit']) ? intval($_REQUEST['limit']) : Config::$ajax_limit_default);
 require 'Views/MyURY/Core/datatojson.php';
