cab33324b5e75906f992f5435d57fb26207d5042
Lloyd Wallis <lpw@ury.york.ac.uk>
2012-06-26 14:36:14 +0100

    uh oh

--- src/Controllers/MyURY/Profile/timeline.php
+++ src/Controllers/MyURY/Profile/timeline.php
@@ -1,4 +1,4 @@
 <?php
 
-$data = User::getInstance($_GET['memberid'] || $_SESSION['memberid'])->getTimeline();
+$data = User::getInstance(isset($_GET['memberid']) ? $_GET['memberid'] : $_SESSION['memberid'])->getTimeline();
 require 'Views/MyURY/Profile/timeline.php';
