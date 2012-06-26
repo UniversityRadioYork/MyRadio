cb36861a736819eed013c208a438897d4d663a6e
Lloyd Wallis <lpw@ury.york.ac.uk>
2012-06-26 18:07:45 +0100

    uh oh

--- src/Classes/ServiceAPI/User.php
+++ src/Classes/ServiceAPI/User.php
@@ -244,11 +244,11 @@ class User extends ServiceAPI {
       AND memberid=$1 AND till_date IS NOT NULL
       UNION
       SELECT * FROM (
-        SELECT t1.timestamp, \'Started a series of \' || sched_entry.summary AS message
+        (SELECT t1.timestamp, \'Started a series of \' || sched_entry.summary AS message
         FROM sched_entry, sched_memberentry
         WHERE sched_entry.entryid = sched_memberentry.entryid
         AND sched_memberentry.memberid = $1
-        AND sched_entry.entryid IN (SELECT entryid FROM sched_timeslot)
+        AND sched_entry.entryid IN (SELECT entryid FROM sched_timeslot))
         LEFT JOIN (SELECT entryid, starttime AS timestamp FROM sched_timeslot
           ORDER BY timeslot ASC LIMIT 1) AS t1 ON (t1.entryid = sched_entry.entryid)
       ) AS t1
