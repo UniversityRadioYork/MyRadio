4175447768b4b33d4ad5ff9ac7cd4d5547f4975e
Lloyd Wallis <lpw@ury.york.ac.uk>
2012-06-26 14:47:22 +0100

    uh oh

--- src/Views/MyURY/Profile/timeline.php
+++ src/Views/MyURY/Profile/timeline.php
@@ -3,5 +3,5 @@ require 'Views/MyURY/Profile/bootstrap.php';
 
 $twig->setTemplate('MyURY/Profile/timeline.twig')
         ->addVariable('heading', 'Timeline')
-        ->addVariable('timeline', $members)
+        ->addVariable('timeline', $data)
         ->render();
