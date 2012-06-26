39123fb38eab453a8945ea75b74b0c6af30b75b4
Lloyd Wallis <lpw@ury.york.ac.uk>
2012-06-26 18:20:32 +0100

    uh oh

--- src/Classes/ServiceAPI/User.php
+++ src/Classes/ServiceAPI/User.php
@@ -252,8 +252,7 @@ class User extends ServiceAPI {
           (SELECT entryid FROM sched_timeslot)
         ) AS t0
         LEFT JOIN (SELECT DISTINCT entryid, starttime AS timestamp FROM sched_timeslot
-          ORDER BY timestamp ASC) AS t1 ON (t1.entryid = t0.entryid)
-          GROUP BY entryid) AS t1
+          ORDER BY timestamp ASC GROUP BY entryid) AS t1 ON (t1.entryid = t0.entryid)
       
       ORDER BY timestamp DESC', array($this->memberid));
     
