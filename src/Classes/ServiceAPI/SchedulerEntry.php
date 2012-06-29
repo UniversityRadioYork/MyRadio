7aa58a3301195acf194adaf68e0c01108da5d301
Lloyd Wallis <lpw@ury.york.ac.uk>
2012-06-29 03:55:09 +0100

    myury

--- src/Classes/ServiceAPI/SchedulerEntry.php
+++ src/Classes/ServiceAPI/SchedulerEntry.php
@@ -9,7 +9,8 @@ class SchedulerEntry extends ServiceAPI {
   
   function __construct($id) {}
   
-  public static function getInstance($id) {
+  public static function getInstance($id = 0) {
+    if ($id === 0) throw new MyURYException('entryid must be specified');
     return new self((int)$id);
   }
 }
