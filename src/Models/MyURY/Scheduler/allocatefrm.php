434a6ec9736d557205eaa94ea27738449802538d
Lloyd Wallis <lpw@ury.york.ac.uk>
2012-06-29 03:45:02 +0100

    myury

--- src/Models/MyURY/Scheduler/allocatefrm.php
+++ src/Models/MyURY/Scheduler/allocatefrm.php
@@ -1,64 +1,32 @@
 <?php
+
 $form = new MyURYForm('sched_allocate', $module, 'doAllocate',
-        array(
-            'debug' => true,
-            'title' => 'Allocate Show'
-            //'template' => 'MyURY/Scheduler/allocate.twig'
+                array(
+                    'debug' => true,
+                    'title' => 'Allocate Show'
+                //'template' => 'MyURY/Scheduler/allocate.twig'
         ));
 
+$terms = Scheduler::getTerms();
+$term_options = array();
+
+foreach ($terms as $term) {
+  $term_options[] = array(
+      'value' => $term['termid'],
+      'text' => $term['descr'] . ' ' . date('Y', $term['start'])
+  );
+}
+
 $form->addField(
-        new MyURYFormField('Text', MyURYFormField::TYPE_TEXT)
+                new MyURYFormField('Schedule for Term', MyURYFormField::TYPE_SELECT,
+                        array(
+                            'options' => $term_options,
+                            'explanation' => 'Please select what term you are scheduling for'
+                            )
+                        )
         )
         ->addField(
-                new MyURYFormField('Number', MyURYFormField::TYPE_NUMBER)
-                )
-        ->addField(
-                new MyURYFormField('Email', MyURYFormField::TYPE_EMAIL)
-                )
-        ->addField(
-                new MyURYFormField('Date', MyURYFormField::TYPE_DATE)
-                )
-        ->addField(
-                new MyURYFormField('Date/Time', MyURYFormField::TYPE_DATETIME)
-                )
-        ->addField(
-                new MyURYFormField('Member', MyURYFormField::TYPE_MEMBER)
-                )
-        ->addField(
-                new MyURYFormField('Track', MyURYFormField::TYPE_TRACK)
-                )
-        ->addField(
-                new MyURYFormField('Artist', MyURYFormField::TYPE_ARTIST)
-                )
-        ->addField(
-                new MyURYFormField('Hidden', MyURYFormField::TYPE_HIDDEN)
-                )
-        ->addField(
-                new MyURYFormField('Select', MyURYFormField::TYPE_SELECT,
-                        array('options'=>array(array('value'=>0,'text'=>'test'))))
-                )
-        ->addField(
-                new MyURYFormField('Radio', MyURYFormField::TYPE_RADIO,
-                        array('options' =>
-                            array(
-                                array('value' => 1, 'text' => 'Week 1'),
-                                array('value' => 1, 'text' => 'Weeks 2-10')
-                            )))
-                )
-        ->addField(
-                new MyURYFormField('Checkbox', MyURYFormField::TYPE_CHECK)
-                )
-        ->addField(
-                new MyURYFormField('Day', MyURYFormField::TYPE_DAY)
-                )
-        ->addField(
-                new MyURYFormField('Block Text', MyURYFormField::TYPE_BLOCKTEXT)
-                )
-        ->addField(
-                new MyURYFormField('Time', MyURYFormField::TYPE_TIME)
-                )
-        ->addField(
-                new MyURYFormField('CheckboxGroup', MyURYFormField::TYPE_CHECKGRP,
+                new MyURYFormField('Weeks', MyURYFormField::TYPE_CHECKGRP,
                         array('options' =>
                             array(
                                 new MyURYFormField('wk1', MyURYFormField::TYPE_CHECK, array('label' => 'Week 1', 'required' => false)),
@@ -71,4 +39,8 @@ $form->addField(
                                 new MyURYFormField('wk8', MyURYFormField::TYPE_CHECK, array('label' => 'Week 8', 'required' => false)),
                                 new MyURYFormField('wk9', MyURYFormField::TYPE_CHECK, array('label' => 'Week 9', 'required' => false)),
                                 new MyURYFormField('wk10', MyURYFormField::TYPE_CHECK, array('label' => 'Week 10', 'required' => false))
-                            ))));
+                            ),
+                            'explanation' => 'Select what weeks this term you will be able to do your show.'
+                        )
+                )
+);
