bb2b7744c12a3ecae1e605246bcbadeadf10a08f
Lloyd Wallis <lpw@ury.york.ac.uk>
2012-06-29 03:53:07 +0100

    myury

--- src/Public/js/myury.form.js
+++ src/Public/js/myury.form.js
@@ -21,66 +21,72 @@ $(document).ready(function() {
   /**
    * Initialises the Member autocomplete pickers where necessary
    */
-  $('fieldset.myuryfrm input.member-autocomplete').autocomplete({
-    minLength: 3,
-    source: "index.php?module=Core&action=a-findmember",
-    select: function(event, ui) {
-      $(this).val(ui.item.fname + ' ' + ui.item.sname);
-      $('#'+$(this).attr('id').replace(/-ui$/, '')).val(ui.item.memberid);
-      return false;
-    },
-    //Prevent the field blanking out when an item is given focus
-    focus: function (event, ui) {
-      return false;
-    }
-  })
-  .data("autocomplete")._renderItem = function(ul, item) {
-    return $('<li></li>').data('item.autocomplete', item)
-    .append('<a>' + item.fname + ' ' + item.sname + '</a>')
-    .appendTo(ul);
-  };
+  $('fieldset.myuryfrm input.member-autocomplete').each(function() {
+    $(this).autocomplete({
+      minLength: 3,
+      source: "index.php?module=Core&action=a-findmember",
+      select: function(event, ui) {
+        $(this).val(ui.item.fname + ' ' + ui.item.sname);
+        $('#'+$(this).attr('id').replace(/-ui$/, '')).val(ui.item.memberid);
+        return false;
+      },
+      //Prevent the field blanking out when an item is given focus
+      focus: function (event, ui) {
+        return false;
+      }
+    })
+    .data("autocomplete")._renderItem = function(ul, item) {
+      return $('<li></li>').data('item.autocomplete', item)
+      .append('<a>' + item.fname + ' ' + item.sname + '</a>')
+      .appendTo(ul);
+    };
+  });
   /**
-   * Initialises the Track autocomplete pickers where necessary
-   */
-  $('fieldset.myuryfrm input.track-autocomplete').autocomplete({
-    minLength: 3,
-    source: "index.php?module=Core&action=a-findtrack",
-    select: function(event, ui) {
-      $(this).val(ui.item.title);
-      $('#'+$(this).attr('id').replace(/-ui$/, '')).val(ui.item.trackid);
-      return false;
-    },
-    //Prevent the field blanking out when an item is given focus
-    focus: function (event, ui) {
-      return false;
-    }
-  })
-  .data("autocomplete")._renderItem = function(ul, item) {
-    return $('<li></li>').data('item.autocomplete', item)
-    .append('<a>' + item.title + '<br><span style="font-size:.8em">' + item.artist + '</span></a>')
-    .appendTo(ul);
-  };
+  * Initialises the Track autocomplete pickers where necessary
+  */
+  $('fieldset.myuryfrm input.track-autocomplete').each(function() {
+    $(this).autocomplete({
+      minLength: 3,
+      source: "index.php?module=Core&action=a-findtrack",
+      select: function(event, ui) {
+        $(this).val(ui.item.title);
+        $('#'+$(this).attr('id').replace(/-ui$/, '')).val(ui.item.trackid);
+        return false;
+      },
+      //Prevent the field blanking out when an item is given focus
+      focus: function (event, ui) {
+        return false;
+      }
+    })
+    .data("autocomplete")._renderItem = function(ul, item) {
+      return $('<li></li>').data('item.autocomplete', item)
+      .append('<a>' + item.title + '<br><span style="font-size:.8em">' + item.artist + '</span></a>')
+      .appendTo(ul);
+    };
+  });
   /**
    * Initialises the Artist autocomplete pickers where necessary
    */
-  $('fieldset.myuryfrm input.artist-autocomplete').autocomplete({
-    minLength: 3,
-    source: "index.php?module=Core&action=a-findartist",
-    select: function(event, ui) {
-      $(this).val(ui.item.title);
-      $('#'+$(this).attr('id').replace(/-ui$/, '')).val(ui.item.artistid);
-      return false;
-    },
-    //Prevent the field blanking out when an item is given focus
-    focus: function (event, ui) {
-      return false;
-    }
-  })
-  .data("autocomplete")._renderItem = function(ul, item) {
-    return $('<li></li>').data('item.autocomplete', item)
-    .append('<a>' + item.title + '</a>')
-    .appendTo(ul);
-  };
+  $('fieldset.myuryfrm input.artist-autocomplete').each(function() {
+    $(this).autocomplete({
+      minLength: 3,
+      source: "index.php?module=Core&action=a-findartist",
+      select: function(event, ui) {
+        $(this).val(ui.item.title);
+        $('#'+$(this).attr('id').replace(/-ui$/, '')).val(ui.item.artistid);
+        return false;
+      },
+      //Prevent the field blanking out when an item is given focus
+      focus: function (event, ui) {
+        return false;
+      }
+    })
+    .data("autocomplete")._renderItem = function(ul, item) {
+      return $('<li></li>').data('item.autocomplete', item)
+      .append('<a>' + item.title + '</a>')
+      .appendTo(ul);
+    };
+  });
   
   /**
    * Setup Checkbox Group select all / select none
