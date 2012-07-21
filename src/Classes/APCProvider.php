0b501dad69e9715dd36a8c8c1dfc0ab3be2ebe6b
Lloyd Wallis <lpw@ury.org.uk>
2012-07-21 21:56:40 +0100

    MyURY

--- src/Classes/APCProvider.php
+++ src/Classes/APCProvider.php
@@ -1,15 +1,39 @@
 <?php
 /**
- * Description of APCProvider
+ * This file contains the APCProvider class. The current CacheProvider is loaded as part of the bootstrap process,
+ * as configured in the Config
+ */
+
+/**
+ * APCProvider provides in-memory caching for PHP resources to increase page load times
+ * 
+ * APCProvider was the first CacheProvider implementation in MyURY. It enables Models to send cache commands to it
+ * which are then stored using the APC plugin automatically. In order for this class to work correctly, the server
+ * needs the APC PHP plugin installed on the server. It will throw an Exception if it is not.
  *
  * @author Lloyd Wallis <lpw@ury.org.uk>
  * @version 21072012
  * @package MyURY_Core
  */
 class APCProvider implements CacheProvider {
+  /**
+   * A variable to store the singleton instance
+   * @var APCProvider storage for the only APCProvider instance
+   */
   private static $me;
+  
+  /**
+   * Stores whether caching should be rnning. If not, it does not do anything on function calls
+   * @var boolean
+   */
   private $enable;
   
+  /**
+   * Constructs the Unique instance of the CacheProvider for use. Private so that instances cannot be used in ways
+   * other than those intended
+   * @param boolean $enable Whether caching is actually enabled in this request. Default true
+   * @throws MyURYException Will throw a MyURYException if the APC extension is not loaded
+   */
   private function __construct($enable = true) {
     $this->enable = $enable;
     if ($enable && !function_exists('apc_store')) {
@@ -19,22 +43,49 @@ class APCProvider implements CacheProvider {
     }
   }
   
+  /**
+   * Stores an object in the APC User Object Cache
+   * 
+   * @param String $key The unique name of the object to store. Ideally, this would use myury_{module}_{name}
+   * @param mixed $value The data to store
+   * @param int $expires The number of seconds this cache entry is valid for. Default is to last forever (0)
+   * @return boolean Whether the operation was successful (returns false if caching disabled)
+   * @assert ('myury_core_test', 'test value', 0) == true
+   */
   public function set($key, $value, $expires = 0) {
     if (!$this->enable) return false;
     return apc_store($key, $value, $expires);
   }
   
+  /**
+   * Reads a previously stored value from the APC User Object Cache and returns it
+   * 
+   * @param String $key The unique name of the object to fetch
+   * @return mixed The value of the store, or false on failure
+   * @assert ('myury_core_test') == 'test value'
+   */
   public function get($key) {
     if (!$this->enable) return false;
     return apc_fetch($key);
   }
   
+  /**
+   * Deletes a previously stored value from the APC User Object Cache
+   * 
+   * @param String $key The unique name of the object to delete
+   * @return boolean Returns whether the operaion was a success
+   * @assert ('myury_core_test') == true
+   */
   public function delete($key) {
     if (!$this->enable) return false;
     return apc_delete($key);
   }
   
   /**
+   * This function currently does nothing as it does not have an obvious APC implementation
+   * 
+   * The theory is that this will completely wipe the APC User Object Cache
+   * 
    * @todo find a way to implement this in APC
    */
   public function purge() {
@@ -42,6 +93,11 @@ class APCProvider implements CacheProvider {
     return true;
   }
   
+  /**
+   * Returns the Singleton instance of this class, creating it if necessary
+   * 
+   * @return APCProvider
+   */
   public static function getInstance() {
     if (!self::$me) {
       self::$me = new self(Config::$cache_enable);
