e68fa037db3c81a0ee32a7d4b13ace8ef3dbbf7f
Lloyd Wallis <lpw@ury.org.uk>
2012-07-21 14:47:16 +0100

    MyURY

--- src/Controllers/MyURY/Scheduler/doAllocate.php
+++ src/Controllers/MyURY/Scheduler/doAllocate.php
@@ -3,6 +3,7 @@ CoreUtils::requirePermission(AUTH_ALLOCATESLOTS);
 
 //The Form definition
 require 'Models/MyURY/Scheduler/allocatefrm.php';
+print_r($_REQUEST);
 print_r($form->readValues());
 
 throw new MyURYException('Not Implemented', MyURYException::FATAL);
