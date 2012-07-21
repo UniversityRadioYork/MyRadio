a7e71c4fafd43824443b3e51b55345b97bbdc2b7
Lloyd Wallis <lpw@ury.org.uk>
2012-07-21 15:55:17 +0100

    MyURY

--- src/Classes/MyURY/MyURYFormField.php
+++ src/Classes/MyURY/MyURYFormField.php
@@ -176,13 +176,12 @@ class MyURYFormField {
         return (int)strtotime($_REQUEST[$name]);
         break;
       case self::TYPE_CHECK:
-        echo $name;
-        return isset($_REQUEST[$name]);// && ($_REQUEST[$name] === 'On' || $_REQUEST['name'] === 'on'));
+        return (bool)(isset($_REQUEST[$name]) && ($_REQUEST[$name] === 'On' || $_REQUEST['name'] === 'on'));
         break;
       case self::TYPE_CHECKGRP:
         $return = array();
         foreach ($this->options as $option) {
-          $return[$option->getName()] = (int)$option->readValue($name);
+          $return[$option->getName()] = (int)$option->readValue($name.'-');
         }
         return $return;
         break;
