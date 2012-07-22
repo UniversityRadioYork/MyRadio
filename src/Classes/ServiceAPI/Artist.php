b14bf84330f0017faedf93a10272a13dfe8326bf
Lloyd Wallis <lpw@ury.org.uk>
2012-07-22 20:59:27 +0100

    MyURY

--- src/Classes/ServiceAPI/Artist.php
+++ src/Classes/ServiceAPI/Artist.php
@@ -30,11 +30,24 @@ class Artist extends ServiceAPI {
     throw new MyURYException('Not implemented ARtist::__construct');
   }
   
+  /**
+   * Does nothing as this isn't implemented. Derp.
+   * @param int $trackid The ID of the Track to return
+   * @throws MyURYException Not implemented - throws a fatal exception
+   */
   public static function getInstance($trackid = -1) {
     self::__wakeup();
     throw new MyURYException('Not implemented ARtist::getInstance');
   }
   
+  /**
+   * Returns an Array of Artists matching the given partial name
+   * @param String $title A partial or total title to search for
+   * @param int $limit The maximum number of tracks to return
+   * @return Array 2D with each first dimension an Array as follows:<br>
+   * title: The name of the artist<br>
+   * artistid: Always 0 until Artist support is implemented
+   */
   public static function findByName($title, $limit) {
     $title = trim($title);
     return self::$db->fetch_all('SELECT DISTINCT rec_track.artist AS title, 0 AS artistid
