c4999df5dd7033415925a3ba7e05399527022735
Lloyd Wallis <lpw@ury.org.uk>
2012-07-22 21:06:49 +0100

    MyURY

--- src/Classes/ServiceAPI/SchedulerEntry.php
+++ src/Classes/ServiceAPI/SchedulerEntry.php
@@ -17,8 +17,19 @@
  */
 class SchedulerEntry extends ServiceAPI {
   
+  /**
+   * Nothing yet.
+   * @todo write this
+   * @param int $id
+   */
   function __construct($id) {}
   
+  /**
+   * Returns a Singleton SchedulerEntry, creating if necessary
+   * @param int $id The SchedulerEntry entryid to return
+   * @return \self
+   * @throws MyURYException If an entryid is the default value
+   */
   public static function getInstance($id = 0) {
     if ($id === 0) throw new MyURYException('entryid must be specified');
     return new self((int)$id);
