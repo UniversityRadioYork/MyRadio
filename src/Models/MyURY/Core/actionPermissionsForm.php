e3d3a0dd5b25a3fda0349008e1c65717f74c9e30
Lloyd Wallis <lpw@ury.org.uk>
2012-07-24 00:56:17 +0100

    MyURY

--- src/Models/MyURY/Core/actionPermissionsForm.php
+++ src/Models/MyURY/Core/actionPermissionsForm.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * 
  * @todo Proper Documentation
@@ -13,9 +14,35 @@ $form = new MyURYForm('assign_action_permissions', $module, 'addActionPermission
         ));
 
 $form->addField(
-        new MyURYFormField('service', MyURYFormField::TYPE_SELECT,
-                array(
-                    'options' => CoreUtils::getServices(),
-                    'explanation' => 'Select a Service to apply permissions to',
-                    'label' => 'Service'
+                new MyURYFormField('service', MyURYFormField::TYPE_SELECT,
+                        array(
+                            'options' => CoreUtils::getServices(),
+                            'explanation' => 'Select a Service to apply permissions to',
+                            'label' => 'Service'
+                )))
+        ->addField(
+                new MyURYFormField('module', MyURYFormField::TYPE_TEXT,
+                        array(
+                            'explanation' => 'Type a Module to apply permissions to',
+                            'label' => 'Module'
+                ))
+        )
+        ->addField(
+                new MyURYFormField('action', MyURYFormField::TYPE_TEXT,
+                        array(
+                            'explanation' => 'Type an Action within that Module to apply permissions to.
+                                      Leave blank to apply it to all Actions.',
+                            'label' => 'Action',
+                            'required' => false
+                )))
+        ->addField(
+                new MyURYFormField('permission', MyURYFormField::TYPE_SELECT,
+                        array(
+                            'explanation' => 'Select a permission that you want to add which when granted
+                                      allows a user to perform this Action. These use boolean OR, not AND so may not
+                                      stack as you would like depending on circumstances. Leave blank to allow global
+                                      access.',
+                            'label' => 'Permission',
+                            'required' => false,
+                            'options' => CoreUtils::getAllPermissions()
                 )));
