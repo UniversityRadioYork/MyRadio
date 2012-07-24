d0399fd9bfc1bae57551c507f9263eeb15ef19fe
Lloyd Wallis <lpw@ury.org.uk>
2012-07-24 01:30:14 +0100

    MyURY

--- src/Controllers/MyURY/Core/addActionPermission.php
+++ src/Controllers/MyURY/Core/addActionPermission.php
@@ -1,4 +1,11 @@
 <?php
 /**
- * @todo Write this
+ * @todo Document properly
+ * @package MyURY_Core
+ * @author Lloyd Wallis <lpw@ury.org.uk>
+ * @version 24072012
  */
+
+require 'Models/MyURY/Core/actionPermissionsForm.php';
+
+print_r($form->readValue());
