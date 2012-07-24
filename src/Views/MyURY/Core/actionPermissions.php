45633b8bec86b0d07ef82307e928b3bbd166d234
Lloyd Wallis <lpw@ury.org.uk>
2012-07-24 02:49:17 +0100

    MyURY

--- src/Views/MyURY/Core/actionPermissions.php
+++ src/Views/MyURY/Core/actionPermissions.php
@@ -10,7 +10,7 @@
 for ($i = 0; $i < sizeof($data); $i++) {
   $data[$i]['del'] = array(
       'display' => 'text',
-      'url' => CoreUtils::makeURL('Core', 'delete', array('permissionid' => $data[$i]['actpermissionid'])),
+      'url' => CoreUtils::makeURL('Core', 'removeActionPermission', array('permissionid' => $data[$i]['actpermissionid'])),
       'value' => 'Delete'
       );
 }
