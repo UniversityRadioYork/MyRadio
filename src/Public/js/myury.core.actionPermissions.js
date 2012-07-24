64abf30315b91c3657131b5b1595999b0d0483ec
Lloyd Wallis <lpw@ury.org.uk>
2012-07-24 02:42:40 +0100

    MyURY

--- src/Public/js/myury.core.actionPermissions.js
+++ src/Public/js/myury.core.actionPermissions.js
@@ -34,10 +34,7 @@ $('.twig-datatable').dataTable({
 );
 
 $('td.column-del a').on('click', function(e) {
-  e.stopPropagation();
-  var id = $(this).parent('tr').children('td:first').attr('id');
-  console.log(id);
-  
+  e.preventDefault();
   $('<div title="Confirm Delete">Are you sure you want to remove this permission requirement? If this Action has no\n\
 other permission settings defined, it will no longer work.</div>').dialog({modal:true}).append('body');
 });
