3c4843f78cbb31f372231f474feee17e159dee37
Lloyd Wallis <lpw@ury.org.uk>
2012-07-25 22:41:16 +0100

    MyURY

--- src/Views/MyURY/Library/bootstrap.php
+++ src/Views/MyURY/Library/bootstrap.php
@@ -2,5 +2,8 @@
 require 'Views/MyURY/bootstrap.php';
 $twig->setTemplate('stripe.twig')
 	->addVariable('title', 'Library')
+        /**
+         * @todo Some kind of getModuleID($modulename) method in coreutils for make this better?
+         */
         ->addVariable('submenu', (new MyURYMenu())->getSubMenuForUser(9, $member));
 $twig->addInfo('This is a library!');
