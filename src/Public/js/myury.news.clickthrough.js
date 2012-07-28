c9a1cdebe379e4f1cd6679c04da19e49b6fea158
Lloyd Wallis <lpw@ury.org.uk>
2012-07-28 02:09:06 +0100

    MyURY

--- src/Public/js/myury.news.clickthrough.js
+++ src/Public/js/myury.news.clickthrough.js
@@ -3,10 +3,14 @@ $(document).ready(function() {
     //Prevent closing without clicking continue
     closeOnEscape: false,
     open: function(event, ui) { $(".ui-dialog-titlebar-close", ui.dialog || ui).hide(); },
+    //Make the page unusable
+    modal: true,
+    //Pretty button
     buttons: {
       'Continue': function() {
         alert('TODO: Mark read');
       }
-    }
+    },
+    width: 600
   });
 });
