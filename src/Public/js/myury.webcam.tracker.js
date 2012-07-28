4953d0f7e1327acc3cedfe2a0120c0ec8a777895
Lloyd Wallis <lpw@ury.org.uk>
2012-07-28 16:00:47 +0100

    MyURY

--- src/Public/js/myury.webcam.tracker.js
+++ src/Public/js/myury.webcam.tracker.js
@@ -18,17 +18,17 @@ var webcam = {
         if (data >= 24*60*60) {
           sub = Math.floor(data/24*60*60);
           time = time + sub +' days, '
-          data -= sub;
+          data -= sub*24*60*60;
         }
         if (data >= 3600) {
           sub = Math.floor(data/3600);
           time = time + sub + ' hours, ';
-          data -= sub;
+          data -= sub*3600;
         }
         if (data >= 60) {
           sub = Math.floor(data/60);
           time = time + sub + ' minutes, ';
-          data -= sub;
+          data -= sub*60;
         }
         time = time + data + ' seconds';
         $('#webcam-time-counter-value').html(time);
