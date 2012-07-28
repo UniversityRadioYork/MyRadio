4955c8c54f608ae7515b22c560881419d3c8befd
Lloyd Wallis <lpw@ury.org.uk>
2012-07-28 15:54:28 +0100

    MyURY

---
+++ src/Controllers/MyURY/Webcam/a-trackViewer.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Ajaj request to increment track viewer counter
+ */
+
+if (isset($_SESSION['webcam_lastcounterincrement']) && $_SESSION['webcam_lastcounterincrement'] < time()-10) {
+  require 'Controllers/Errors/400.php';
+}
+
+$data = MyURY_Webcam::incrementViewCounter(User::getInstance());
+
+require 'Views/MyURY/Core/datatojson.php';
