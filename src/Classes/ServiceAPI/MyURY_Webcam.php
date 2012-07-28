2dd188f2a46c7bad12cec1728effa9a5c2e0e209
Lloyd Wallis <lpw@ury.org.uk>
2012-07-28 15:58:41 +0100

    MyURY

--- src/Classes/ServiceAPI/MyURY_Webcam.php
+++ src/Classes/ServiceAPI/MyURY_Webcam.php
@@ -17,11 +17,11 @@ class MyURY_Webcam extends ServiceAPI {
   public static function incrementViewCounter(User $user) {
     //Get the current view counter. We do this as a separate query in case the row doesn't exist yet
     $counter = self::$db->fetch_one('SELECT timer FROM webcam.memberviews WHERE memberid = $1', array($user->getID()));
-    print_r($counter);
     if (empty($counter)) {
       $counter = 0;
       $sql = 'INSERT INTO webcam.memberviews (memberid, timer) VALUES ($1, $2)';
     } else {
+      $counter = $counter['timer'];
       $sql = 'UPDATE webcam.memberviews SET timer=$2 WHERE memberid=$1';
     }
     $counter += 15;
