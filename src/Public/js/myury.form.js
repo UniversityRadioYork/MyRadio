312b0598a4d7df8dd33fb82461ca4f7634ca2eda
Lloyd Wallis <lpw@ury.org.uk>
2012-07-29 12:20:38 +0100

    MyURY

--- src/Public/js/myury.form.js
+++ src/Public/js/myury.form.js
@@ -139,10 +139,10 @@ $(document).ready(function() {
     var newid = origid + $('#'+origid+'-counter').val();
     $('#'+origid+'-counter').val(parseInt($('#'+origid+'-counter').val())+1);
     
-    $('#'+origid).clone().attr('id', newid).val('').insertBefore(this);
+    $('#'+origid).clone().attr('id', newid).val('').append('<br>').insertBefore(this);
     
     //For autocomplete fields, they have a ui field which is what is very visible. This needs cloning and setting up
-    var autocomplete = $('#'+origid+'-ui').clone().attr('id', newid+'-ui').val('').insertBefore(this);
+    var autocomplete = $('#'+origid+'-ui').clone().attr('id', newid+'-ui').val('').append('<br>').insertBefore(this);
     if ($(autocomplete).hasClass('member-autocomplete')) {
       MyURYForm.setUpMemberFields();
     }
