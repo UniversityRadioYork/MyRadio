c9ac6d9c85d3f69e773982361b402c9d0c44807d
Lloyd Wallis <lpw@ury.org.uk>
2012-08-01 22:26:29 +0100

    MyURY

--- src/Controllers/MyURY/Scheduler/upgrade.php
+++ src/Controllers/MyURY/Scheduler/upgrade.php
@@ -8,6 +8,6 @@
 $db = Database::getInstance();
 
 //Type = 3 Limits to shows
-$shows = $db->fetch_all('SELECT * FROM sched_entry WHERE type=3 GROUP BY summary');
+$shows = $db->fetch_all('SELECT * FROM sched_entry WHERE entrytypeid=3 GROUP BY summary');
 
 echo nl2br(print_r($shows, true));
