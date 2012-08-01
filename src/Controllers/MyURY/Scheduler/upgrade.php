9610b6d23ed48d28179f270c7b1c9f9a7dc927bc
Lloyd Wallis <lpw@ury.org.uk>
2012-08-01 22:21:46 +0100

    MyURY

---
+++ src/Controllers/MyURY/Scheduler/upgrade.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Script to migrate shows from the old schema to Scheduler 2.0
+ * 
+ * Currently only migrates "No Show Scheduled"
+ */
+
+$db = Database::getInstance();
+
+$shows = $db->fetch_all('SELECT * FROM sched_entry WHERE summary=\'No Show Scheduled\'');
+
+print_r($shows);
