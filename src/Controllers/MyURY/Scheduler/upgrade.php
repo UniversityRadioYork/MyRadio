e8bbf03d9e233ddfffad0535c9210b95871ebe7d
Lloyd Wallis <lpw@ury.org.uk>
2012-08-02 00:15:06 +0100

    MyURY

--- src/Controllers/MyURY/Scheduler/upgrade.php
+++ src/Controllers/MyURY/Scheduler/upgrade.php
@@ -165,8 +165,8 @@ if ($commit) {
     //Add presenters
     foreach ($show['info']['presenters'] as $presenter => $pinfo) {
       $db->query('INSERT INTO schedule.show_credit (show_id, credit_type_id, creditid, effective_from, effective_to, memberid, approvedid) VALUES
-      ($1, 1, $2, $3, $4)',
-            array($show_id, $presenter, $owner, $approving_user));
+      ($1, 1, $2, $3, $4, $5)',
+            array($show_id, $presenter, timeToTimestamp($pinfo['effective_from']), timeToTimestamp($pinfo['effective_to']), $owner, $approving_user));
     }
     
     /**
