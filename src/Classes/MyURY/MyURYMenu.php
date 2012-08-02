3b0fdf98b53a91bbad6382f47b3efb3279d4e17d
Lloyd Wallis <lpw@ury.org.uk>
2012-08-02 21:18:27 +0100

    MyURY

--- src/Classes/MyURY/MyURYMenu.php
+++ src/Classes/MyURY/MyURYMenu.php
@@ -142,6 +142,7 @@ class MyURYMenu {
         WHERE moduleid=$1 ORDER BY title ASC', array($moduleid));
       //Get permissions for each $item
       foreach ($items as $key => $item) {
+        CoreUtils::debug_for(7449, print_r($this->breakDownURL($item['url']), true));
         array_merge($items[$key], $this->breakDownURL($item['url']));
       }
 
