da30e58918045f3a6cc247268eab087c88fb91a2
Lloyd Wallis <lpw@ury.org.uk>
2012-08-16 20:10:48 +0100

    MyURY

---
+++ src/Controllers/MyURY/Scheduler/a-findshowbytitle.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This is pretty much what every Controller should look like.
+ * Some might include more than one model etc.... 
+ * 
+ * @todo Proper documentation
+ * @author Lloyd Wallis <lpw@ury.org.uk>
+ * @version 02082012
+ * @package MyURY_Core
+ */
+if (!isset($_REQUEST['term'])) throw new MyURYException('Parameter \'term\' is required but was not provided');
+
+$data = Scheduler::findShowByTitle($_REQUEST['term'], isset($_REQUEST['limit']) ? intval($_REQUEST['limit']) : Config::$ajax_limit_default);;
+require 'Views/MyURY/Core/datatojson.php';
