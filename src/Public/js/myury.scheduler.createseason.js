a9f2b6c6c2da7ed02c740a332903c42662bf1455
Lloyd Wallis <lpw@ury.org.uk>
2012-08-24 00:32:00 +0100

    MyURY

--- src/Public/js/myury.scheduler.createseason.js
+++ src/Public/js/myury.scheduler.createseason.js
@@ -7,13 +7,12 @@ $(document).ready(function() {
    */
   $('#sched_season-day-repeater, #sched_season-stime-repeater').hide();
   /**
-   * Tell the credittype add link to trigger the credits add action
+   * This is all horrible.
    */
   $('#sched_season-etime-repeater').on('click', function(e) {
     $('#sched_season-day-repeater').trigger('click');
     $('#sched_season-stime-repeater').parent().parent().find('label').clone().insertBefore($('#sched_season-stime-repeater').parent());
     $('#sched_season-stime-repeater').trigger('click');
-    $('#sched_season-etime-repeater').parent().parent().find('label').clone().insertBefore($('#sched_season-etime-repeater').parent().parent().last('input'));
-    console.log($('#sched_season-etime-repeater').parent().parent().last('input'));
+    $('#sched_season-etime-repeater').parent().parent().find('label').clone().insertBefore($('#sched_season-etime-repeater').parent().parent().children('input[type="text"]:last'));
   });
 });
