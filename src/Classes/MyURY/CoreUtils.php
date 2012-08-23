a4522c5bdcde0b855a45ba5cca376268540726a7
Lloyd Wallis <lpw@ury.org.uk>
2012-08-23 16:22:43 +0100

    MyURY

--- src/Classes/MyURY/CoreUtils.php
+++ src/Classes/MyURY/CoreUtils.php
@@ -313,6 +313,7 @@ class CoreUtils {
     $db = Database::getInstance();
     $result = $db->fetch_column('SELECT moduleid FROM myury.modules WHERE serviceid=$1 AND name=$2',
             array($service, $module));
+    print_r(array($service,$module));exit;
     
     if (empty($result)) {
       //The module needs creating
