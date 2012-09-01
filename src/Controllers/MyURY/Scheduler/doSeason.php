7f946dd64b7b6a002e4553418dd3188f1a800084
Lloyd Wallis <lpw@ury.org.uk>
2012-09-01 22:23:29 +0100

    MyURY

---
+++ src/Controllers/MyURY/Scheduler/doSeason.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * 
+ * @todo Proper Documentation
+ * @author Lloyd Wallis <lpw@ury.org.uk>
+ * @version 21072012
+ * @package MyURY_Scheduler
+ */
+
+//The Form definition
+require 'Models/MyURY/Scheduler/showfrm.php';
+
+echo nl2br(print_r($form->readValues(),true));
+
+try {
+  MyURY_Show::create($form->readValues());
+} catch (MyURYException $e) {
+  require 'Views/Errors/500.php';
+  exit;
+}
+
+header('Location: '.CoreUtils::makeURL('Scheduler', 'myShows'));
