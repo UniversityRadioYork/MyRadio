2a3e3fc7a1041fb775c3fa9cf6a2709f2bba0442
Lloyd Wallis <lpw@ury.org.uk>
2012-09-17 19:38:27 +0100

    MyURY

--- src/Views/MyURY/Scheduler/default.php
+++ src/Views/MyURY/Scheduler/default.php
@@ -1,8 +1,7 @@
 <?php
 require 'Views/MyURY/Scheduler/bootstrap.php';
-
 $twig->setTemplate('table.twig')
         ->addVariable('tablescript', 'myury.scheduler.pending')
         ->addVariable('heading', 'Scheduler')
-        ->addVariable('tabledata', CoreUtils::setToDataSource($pending_allocations))
+        ->addVariable('tabledata', ServiceAPI::setToDataSource($pending_allocations))
         ->render();
