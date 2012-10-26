1190492c3196e851694d89c3aef4cbd2dd62673b
Andrew Durant <aj-dj@tiscali.co.uk>
2012-10-26 13:32:50 +0100

    quick tidy

--- src/Models/MyURY/Profile/user.php
+++ src/Models/MyURY/Profile/user.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * @todo Proper Documentation
- * @todo Probably make everything transport via arrays
  * @author Andy Durant <aj@ury.org.uk>
  * @version 26102012
  * @package MyURY_Profile
@@ -10,18 +9,3 @@
 $user = User::getInstance();
 
 $userData = $user->getData();
-
-/**
-        ->addVariable('sex', $sex)
-        ->addVariable('collegeid', $collegeid)
-        ->addVariable('college', $college)
-        ->addVariable('phone', $phone)
-        ->addVariable('uni', $uni)
-        ->addVariable('email', $email)
-        ->addVariable('local_alias', $local_alias)
-        ->addVariable('local_account', $local_account)
-        ->addVariable('receive_email', $receive_email)
-        ->addVariable('account_locked', $account_locked)
-        ->addVariable('last_login', $last_login)
- * 
- */
