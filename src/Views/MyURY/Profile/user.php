225a99b0d12142867851ab86ba0696842d73edf4
Andrew Durant <aj-dj@tiscali.co.uk>
2012-10-26 13:24:06 +0100

    move to array

--- src/Views/MyURY/Profile/user.php
+++ src/Views/MyURY/Profile/user.php
@@ -8,22 +8,12 @@
  */
 require 'Views/MyURY/Profile/bootstrap.php';
 
+var_dump($userData);
 
 $twig->setTemplate('MyURY/Profile/user.twig')
         ->addVariable('title', 'View Member')
         ->addVariable('heading', 'View Profile')
-        ->addVariable('name', $name)
-        ->addVariable('sex', $sex)
-        ->addVariable('collegeid', $collegeid)
-        ->addVariable('college', $college)
-        ->addVariable('phone', $phone)
-        ->addVariable('uni', $uni)
-        ->addVariable('email', $email)
-        ->addVariable('local_alias', $local_alias)
-        ->addVariable('local_account', $local_account)
-        ->addVariable('receive_email', $receive_email)
-        ->addVariable('account_locked', $account_locked)
-        ->addVariable('last_login', $last_login)
+        ->addVariable('user', $userData)
         // @todo use an array for all this data
         // @todo use a separate array for years paid
         // @todo use an array for officerships held
