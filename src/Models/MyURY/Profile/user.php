3c0c20027e9e984a1d933f08d4b99d46d5b48886
Andrew Durant <aj-dj@tiscali.co.uk>
2012-10-26 12:54:50 +0100

    profile page model

--- src/Models/MyURY/Profile/user.php
+++ src/Models/MyURY/Profile/user.php
@@ -1,8 +1,38 @@
 <?php
 /**
  * @todo Proper Documentation
- * @todo This.
- * @author Lloyd Wallis <lpw@ury.org.uk>
- * @version 21072012
+ * @todo Probably make everything transport via arrays
+ * @author Andy Durant <aj@ury.org.uk>
+ * @version 26102012
  * @package MyURY_Profile
+ */
+
+$user = new User($memberid);
+
+$name = $user->getName();
+$sex = $user->getSex();
+$collegeid = $user->getCollegeID();
+$college = $user->getCollege();
+$phone = $user->getPhone();
+$uni = $user->getUniAccount();
+$email = $user->getEmail();
+$local_alias = $user->getLocalAlias();
+$local_account = $user->getLocalName();
+$receive_email = $user->getReceiveEmail();
+$account_locked = $user->getAccountLocked();
+
+
+/**
+        ->addVariable('sex', $sex)
+        ->addVariable('collegeid', $collegeid)
+        ->addVariable('college', $college)
+        ->addVariable('phone', $phone)
+        ->addVariable('uni', $uni)
+        ->addVariable('email', $email)
+        ->addVariable('local_alias', $local_alias)
+        ->addVariable('local_account', $local_account)
+        ->addVariable('receive_email', $receive_email)
+        ->addVariable('account_locked', $account_locked)
+        ->addVariable('last_login', $last_login)
+ * 
  */
