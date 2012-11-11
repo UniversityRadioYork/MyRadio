36fbe3fe900c5c465e162b3bd7c3061f877a7ac0
aj <aj@ury.york.ac.uk>
2012-11-11 23:23:21 +0000

    comments!

--- src/Models/MyURY/Profile/user.php
+++ src/Models/MyURY/Profile/user.php
@@ -6,16 +6,21 @@
  * @package MyURY_Profile
  */
 
+// Set if trying to view another member's profile page
 $getUserId = $_GET['memberid'];
 
+// If trying to view another member, and has permissions to, then load that member
 if (isset($getUserId) && $member->hasAuth(204)) {
   $user = User::getInstance($getUserId);
 }
+// If trying to view another member, and doesn't have permissions to the 403 them
 else if (isset($getUserId) && !User::hasAuth(204)) {
   require 'Controllers/Errors/403.php';
 }
+// Or just load their own profile
 else {
   $user = User::getInstance();
 }
 
+// Get the selected users data
 $userData = $user->getData();
