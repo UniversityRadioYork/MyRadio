ea5ab30011ff2f7942c0ae0139da5427dc5d150c
aj <aj@ury.york.ac.uk>
2012-11-11 23:09:48 +0000

    get user

--- src/Models/MyURY/Profile/user.php
+++ src/Models/MyURY/Profile/user.php
@@ -6,6 +6,16 @@
  * @package MyURY_Profile
  */
 
-$user = User::getInstance();
+$memberid = $_GET['memberid'];
+
+if (isset($memberid) && User::hasAuth(204)) {
+  $user = User::getInstance($memberid);
+}
+else if (isset($memberid) && !User::hasAuth(204)) {
+  require 'Controllers/Errors/403.php';
+}
+else {
+  $user = User::getInstance();
+}
 
 $userData = $user->getData();
