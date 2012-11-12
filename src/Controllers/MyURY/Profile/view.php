a249f3e26a5479bbd112937884f67851d5572501
aj <aj@ury.york.ac.uk>
2012-11-12 19:28:55 +0000

    helps to add the new file

---
+++ src/Controllers/MyURY/Profile/view.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * 
+ * @todo Proper Documentation
+ * @todo Permissions
+ * @author Andy Durant <aj@ury.org.uk>
+ * @version 21072012
+ * @package MyURY_Profile
+ */
+require 'Models/MyURY/Profile/user.php';
+require 'Views/MyURY/Profile/user.php';
