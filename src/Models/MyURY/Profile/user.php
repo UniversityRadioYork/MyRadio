757bccaa0e6a288842c1eef9bbc301c4c69dd6d0
aj <aj@ury.york.ac.uk>
2012-11-14 13:57:32 +0000

    only collect the get, if set

--- src/Models/MyURY/Profile/user.php
+++ src/Models/MyURY/Profile/user.php
@@ -7,7 +7,9 @@
  */
 
 // Set if trying to view another member's profile page
-$getUserId = $_GET['memberid'];
+if (isset($_GET['memberid'])) {
+  $getUserId = $_GET['memberid'];
+}
 
 // If trying to view another member, and has permissions to, then load that member
 if (isset($getUserId) && $member->hasAuth(AUTH_VIEWOTHERMEMBERS)) {
