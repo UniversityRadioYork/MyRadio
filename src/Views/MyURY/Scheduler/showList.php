38c5759f0bb586687a0c0ebfe2b3f7d1cefe8a82
lpw <lpw@144.32.76.124>
2012-12-19 02:43:39 +0000

    Changed to use getDataSource

--- src/Views/MyURY/Scheduler/showList.php
+++ src/Views/MyURY/Scheduler/showList.php
@@ -2,30 +2,9 @@
 
 require 'Views/MyURY/Scheduler/bootstrap.php';
 
-$tabledata = array();
-foreach ($shows as $show) {
-  $tabledata[] = array(
-      'title' => $show->getMeta('title'),
-      'seasons' => $show->getNumberOfSeasons(),
-      'editlink' => array(
-          'display' => 'icon',
-          'value' => 'script',
-          'title' => 'Edit Show',
-          'url' => CoreUtils::makeURL('Scheduler', 'editShow', array('showid' => $show->getID()))),
-      'applylink' => array('display' => 'icon',
-          'value' => 'calendar',
-          'title' => 'Apply for a new Season',
-          'url' => CoreUtils::makeURL('Scheduler', 'createSeason', array('showid' => $show->getID()))),
-      'micrositelink' => array('display' => 'icon',
-          'value' => 'extlink',
-          'title' => 'View Show Microsite',
-          'url' => $show->getWebpage())
-  );
-}
-
 $twig->setTemplate('MyURY/Scheduler/showList.twig')
         ->addVariable('heading', 'My Shows')
-        ->addVariable('tabledata', $tabledata)
+        ->addVariable('tabledata', ServiceAPI::setToDataSource($shows))
         ->addVariable('tablescript', 'myury.scheduler.showlist');
 if (isset($_REQUEST['msg'])) {
   switch($_REQUEST['msg']) {
