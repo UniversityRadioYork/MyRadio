1d3ed414eec405252869d3357c8744e1bd855169
lpw <lpw@144.32.76.124>
2012-12-19 02:44:02 +0000

    Added All Shows controller

---
+++ src/Controllers/MyURY/Scheduler/allShows.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * 
+ * @todo Proper Documentation
+ * @author Lloyd Wallis <lpw@ury.org.uk>
+ * @version 21072012
+ * @package MyURY_Scheduler
+ */
+
+$shows = MyURY_Show::getAllShows();
+require 'Views/MyURY/Scheduler/showList.php';
