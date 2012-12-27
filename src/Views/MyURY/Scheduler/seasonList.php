81098064ab400001f53087acf9a489fee7dcfa06
lpw <lpw@144.32.76.124>
2012-12-27 00:08:04 +0000

    Updated SeasonList to not use Pending Allocations datatable definition

--- src/Views/MyURY/Scheduler/seasonList.php
+++ src/Views/MyURY/Scheduler/seasonList.php
@@ -1,7 +1,7 @@
 <?php
 require 'Views/MyURY/Scheduler/bootstrap.php';
 $twig->setTemplate('table.twig')
-        ->addVariable('tablescript', 'myury.scheduler.pending')
+        ->addVariable('tablescript', 'myury.scheduler.seasonlist')
         ->addVariable('title', 'Seasons of '.$show->getMeta('title'))
         ->addVariable('tabledata', ServiceAPI::setToDataSource($seasons))
         ->render();
