ce23a38ae28a7a4c23ac246bd6a9b54648d91b0b
Lloyd Wallis <lpw@ury.org.uk>
2013-03-14 13:27:15 +0000

    nipsweb

--- src/Controllers/NIPSWeb/Core/default.php
+++ src/Controllers/NIPSWeb/Core/default.php
@@ -6,5 +6,10 @@
  * @version 11032013
  * @package MyURY_NIPSWeb
  */
-require 'Models/MyURY/Core/menu.php';
-require 'Views/MyURY/Core/menu.php';
+require 'Views/MyURY/bootstrap.php';
+
+$twig->setTemplate('NIPSWeb/menu.twig')
+        ->addVariable('title', 'Show Planner')
+        ->addVariable('heading', 'Show Planner')
+        ->addVariable('tracks', MyURY_Timeslot::getInstance($_SESSION['timeslotid'])->getShowPlan())
+        ->render();
