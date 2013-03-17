68eecf4d430b7e69ebceed8c30234388412c6714
Lloyd Wallis <lpw@ury.org.uk>
2013-03-17 12:00:26 +0000

    Disable caching

---
+++ src/Controllers/NIPSWeb/Core/create_token.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Creates a NIPSWeb Play Token for the Current User and the given trackid
+ * 
+ * @author Lloyd Wallis <lpw@ury.org.uk>
+ * @version 17032013
+ * @package MyURY_NIPSWeb
+ */
+NIPSWeb_PlayToken::createToken($_REQUEST['trackid']);
+
+require 'Views/MyURY/Core/nocontent.php';
