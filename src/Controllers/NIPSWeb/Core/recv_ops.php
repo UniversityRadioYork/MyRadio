bd81c1864ed584c13ff7cca0490a9e58a9149657
Lloyd Wallis <lpw@ury.org.uk>
2013-04-16 21:49:33 +0100

    js api

---
+++ src/Controllers/NIPSWeb/Core/recv_ops.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Creates a NIPSWeb Edit Token for the current session
+ * 
+ * @author Lloyd Wallis <lpw@ury.org.uk>
+ * @version 16042013
+ * @package MyURY_NIPSWeb
+ */
+$data = NIPSWeb_Token::getEditToken();
+
+require 'Views/MyURY/Core/datatojson.php';
