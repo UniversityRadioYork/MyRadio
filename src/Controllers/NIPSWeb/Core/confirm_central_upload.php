b6032ea100209da9afbee5925b824b54f7a27338
Lloyd Wallis <lpw@ury.org.uk>
2013-04-18 14:12:50 +0100

    js api

---
+++ src/Controllers/NIPSWeb/Core/confirm_central_upload.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Saves a cached upload into the URY Central Database
+ * 
+ * @author Lloyd Wallis <lpw@ury.org.uk>
+ * @version 18042013
+ * @package MyURY_NIPSWeb
+ */
+$data = MyURY_Track::identifyAndStoreTrack($_REQUEST['fileid'], $_REQUEST['title'], $_REQUEST['artist']);
+
+require 'Views/MyURY/Core/datatojson.php';
