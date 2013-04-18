1829590b68dc730d497c949b609f21694522806f
Lloyd Wallis <lpw@ury.org.uk>
2013-04-18 15:39:45 +0100

    js api

--- src/Controllers/NIPSWeb/Core/confirm_central_upload.php
+++ src/Controllers/NIPSWeb/Core/confirm_central_upload.php
@@ -7,5 +7,6 @@
  * @package MyURY_NIPSWeb
  */
 $data = MyURY_Track::identifyAndStoreTrack($_REQUEST['fileid'], $_REQUEST['title'], $_REQUEST['artist']);
+$data['fileid'] = $_REQUEST['fileid'];
 
 require 'Views/MyURY/Core/datatojson.php';
