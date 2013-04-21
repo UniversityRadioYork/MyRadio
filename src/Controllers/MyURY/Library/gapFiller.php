2d2d5aedda4cf16c0f6da0d960b970eec31845d4
Lloyd Wallis <lpw@ury.org.uk>
2013-04-21 15:34:37 +0100

    Things

--- src/Controllers/MyURY/Library/gapFiller.php
+++ src/Controllers/MyURY/Library/gapFiller.php
@@ -8,18 +8,26 @@
 
 $albums = MyURY_Album::findByName('URY Downloads', 10);
 
+$cacher = APCProvider::getInstance();
+
+$checked = $cacher->get('myuryLibraryGapFillerCheckedTracks') || array();
+
 $limit = 50;
 $updated = array();
 foreach ($albums as $album) {
   $tracks = $album->getTracks();
   foreach ($tracks as $track) {
     if ($limit <= 0) break;
+    if (in_array($track->getID(), $checked));
     $track->updateInfoFromLastfm();
     $updated[] = $track;
+    $checked[] = $track->getID();
     usleep(200000);
     $limit--;
   }
 }
+
+$cacher->set('myuryLibraryGapFillerCheckedTracks', $checked);
 
 require 'Views/MyURY/Library/bootstrap.php';
 $twig->setTemplate('table.twig')
