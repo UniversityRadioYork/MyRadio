66bad3c5e666bc7af76d1895331f8f4d40428efb
Lloyd Wallis <lpw@ury.org.uk>
2013-04-26 18:08:24 +0100

    I should use better commit message

---
+++ src/Controllers/MyURY/Library/findDuplicate.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Scan music library, finding tracks that seem to exist more than once
+ * @author Lloyd Wallis <lpw@ury.york.ac.uk>
+ * @version 24052013
+ * @package MyURY_Library
+ */
+//This stores the last ID checked so that we can collect Tracks in batches, preventing us from using ALL the RAM
+$finalid = 0;
+//Stores trackids of tracks that have been searched through already
+$alreadydone = array();
+//Stores MyURY_Tracks that are duplicates
+$duplicates = array();
+//We use this here to keep track of the query counter
+$db = Database::getInstance();
+//If query count goes over this, bail and show what you've found so far
+$query_limit = 25000;
+
+do {
+  //Get the next batch of tracks from where we left off
+  $tracks = MyURY_Track::findByOptions(array('limit' => 500, 'digitised' => true, 'idsort' => true,
+              'custom' => 'trackid > ' . $finalid));
+
+  foreach ($tracks as $track) {
+    //If this has already appeared as a duplicate, don't search again or we'll duplicate the duplications
+    if (in_array($track->getID(), $alreadydone))
+      continue;
+
+    //Find tracks that match this name and artist
+    $matches = MyURY_Track::findByOptions(
+                    array('title' => $track->getTitle(),
+                        'artist' => $track->getArtist(),
+                        'limit' => 0,
+                        'precise' => true)
+    );
+
+    //If there's more than one match, then there are duplicates for this item
+    if (sizeof($matches) > 1) {
+      foreach ($matches as $match) {
+        $alreadydone[] = $match->getID();
+        $duplicates[] = $match;
+      }
+    }
+
+    //Log the latest ID used
+    $finalid = $track->getID();
+
+    //Remove the Singleton store's built in reference to this track to reduce memory usage
+    $track->removeInstance();
+    unset($track);
+
+    //Kill the loop if we've used too many queries
+    if ($db->getCounter() > $query_limit)
+      break;
+  }
+  echo "$finalid ({$db->getCounter()}/" . sizeof($duplicates) . ")<br>";
+  gc_collect_cycles();
+
+  if ($db->getCounter() > $query_limit)
+    break;
+} while (!empty($tracks));
+
+require 'Views/MyURY/Library/bootstrap.php';
+$twig->setTemplate('table.twig')
+        ->addVariable('tablescript', 'myury.datatable.default')
+        ->addVariable('title', 'Duplicate Tracks')
+        ->addVariable('tabledata', CoreUtils::dataSourceParser($duplicates))
+        ->render();
