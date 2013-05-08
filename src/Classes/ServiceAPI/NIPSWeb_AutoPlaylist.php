8ac0060a4cfba5d64cbf74e4dc020a2c647353b8
Andy Durant <andy@ajdurant.co.uk>
2013-05-08 18:34:15 +0100

    getAllAutoPlaylists

--- src/Classes/ServiceAPI/NIPSWeb_AutoPlaylist.php
+++ src/Classes/ServiceAPI/NIPSWeb_AutoPlaylist.php
@@ -98,8 +98,7 @@ class NIPSWeb_AutoPlaylist extends ServiceAPI {
     return $this->auto_playlist_id;
   }
 
-  public static function getAllAutoPlaylists($editable_only = false) {
-    if ($editable_only && !User::getInstance()->hasAuth(AUTH_EDITCENTRALRES)) return array();
+  public static function getAllAutoPlaylists() {
     $result = self::$db->fetch_column('SELECT auto_playlist_id FROM bapsplanner.auto_playlists ORDER BY name');
     $response = array();
     foreach ($result as $id) {
