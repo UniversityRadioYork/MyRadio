9f5d224fa53e61a051342eb542597d9f08017578
Anthony Williams <anthony@Thinkpad-T400>
2013-05-09 13:07:51 +0100

    My first push in a while

---
+++ src/Controllers/MyURY/Library/Search.php
@@ -0,0 +1,8 @@
+<?php
+require 'Views/MyURY/Library/bootstrap.php';
+$twig->addInfo('SEARCH PAGE GOES HERE')
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
