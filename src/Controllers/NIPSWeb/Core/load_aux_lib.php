13f908444b107bebcce2cd13624e9501a375ca74
Andrew Durant <ajdjster@gmail.com>
2013-05-12 15:30:22 +0100

    auxid

--- src/Controllers/NIPSWeb/Core/load_aux_lib.php
+++ src/Controllers/NIPSWeb/Core/load_aux_lib.php
@@ -3,14 +3,18 @@
  * Loads a NIPSWeb Auxillary playlist
  * 
  * @author Lloyd Wallis <lpw@ury.org.uk>
- * @version 06042013
+ * @author Andy Durant <aj@ury.org.uk>
+ * @version 20130512
  * @package MyURY_NIPSWeb
  */
 
-if (is_numeric($_REQUEST['libraryid'])) {
-  $data = NIPSWeb_ManagedPlaylist::getInstance($_REQUEST['libraryid'])->getItems();
-} else {
-  $data = NIPSWeb_ManagedUserPlaylist::getInstance($_REQUEST['libraryid'])->getItems();
+if (preg_match('/^aux-.*$/', $_REQUEST['libraryid']) === 1) {
+  $libraryid = str_replace('aux-','',$_REQUEST['libraryid']);
+  $data = NIPSWeb_ManagedPlaylist::getInstance($libraryid)->getItems();
+} 
+else {
+  $libraryid = str_replace('user-','',$_REQUEST['libraryid']);
+  $data = NIPSWeb_ManagedUserPlaylist::getInstance($libraryid)->getItems();
 }
 
 require 'Views/MyURY/Core/datatojson.php';
