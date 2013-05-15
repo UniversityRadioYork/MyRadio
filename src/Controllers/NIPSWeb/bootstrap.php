7afd76a5fdb0e26d41c479210858555534ff969d
Lloyd Wallis <lpw@ury.org.uk>
2013-05-15 16:57:36 +0100

    Autoloader fix

--- src/Controllers/NIPSWeb/bootstrap.php
+++ src/Controllers/NIPSWeb/bootstrap.php
@@ -14,7 +14,7 @@
 spl_autoload_register(function($class){
   $class .= '.php';
   $info = CoreUtils::getServiceVersionForUser(CoreUtils::getServiceId('NIPSWeb'), User::getInstance());
-  if (file_exists($info['path'].'/Classes/NIPSWeb/'.$class)) {
+  if (stream_resolve_include_path($info['path'].'/Classes/NIPSWeb/'.$class)) {
     require_once $info['path'].'/Classes/NIPSWeb/'.$class;
   }
 });
