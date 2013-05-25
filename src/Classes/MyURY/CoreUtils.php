c4f5df9167af130dd1cb083e09a8f336d0ea9765
Lloyd Wallis <lpw@ury.org.uk>
2013-05-25 07:31:56 +0100

    Refactoring, removed Services

--- src/Classes/MyURY/CoreUtils.php
+++ src/Classes/MyURY/CoreUtils.php
@@ -47,6 +47,7 @@ class CoreUtils {
      * This is better than file_exists because it ensures that the response is valid for a version which has the file
      * when live does not
      */
+    echo 'Controllers/' . $module . '/' . $action . '.php';
     return is_string(stream_resolve_include_path('Controllers/' . $module . '/' . $action . '.php'));
   }
 
