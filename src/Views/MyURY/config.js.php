3947a4a7cdfe4895fbd4fa30d3c61516dd3d2b1d
Lloyd Wallis <lpw@ury.org.uk>
2013-05-25 15:26:07 +0100

    Refactoring, removed Services

--- src/Views/MyURY/config.js.php
+++ src/Views/MyURY/config.js.php
@@ -10,4 +10,4 @@ header('Cache-Control: max-age=86400, must-revalidate');
 header('Expires: ', date('r', time()+86400));
 header('HTTP/1.1 200 OK');
 
-echo 'mConfig='. json_encode($conf).';';
+echo 'window.mConfig='. json_encode($conf).';';
