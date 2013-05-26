4715b3795a0ce85e99c3895e2ef0097d65438cd0
Lloyd Wallis <lpw@ury.org.uk>
2013-05-26 15:09:42 +0100

    Mail send

---
+++ src/Models/Mail/sendfrm.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Form to compose an email to a mailing list
+ * @author Lloyd Wallis <lpw@ury.org.uk>
+ * @version 20130526
+ * @package MyURY_Mail
+ */
+
+$form = (new MyURYForm('mail_send', 'Mail', 'doSend',
+                array(
+                    'debug' => true,
+                    'title' => 'Send Email'
+                )
+        ))->addField(
+                new MyURYFormField('subject', MyURYFormField::TYPE_TEXT,
+                        array(
+                            'explanation' => 'Subject of the email',
+                            'label' => 'Subject'
+                        )
+                )
+        )->addField(
+                new MyURYFormField('body', MyURYFormField::TYPE_BLOCKTEXT,
+                        array(
+                            'explanation' => '',
+                            'label' => ''
+                        )
+                )
+        );
