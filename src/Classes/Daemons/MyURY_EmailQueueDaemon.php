b62dab4861a66e60887426ed99934029a9f148a5
Lloyd Wallis <lpw@ury.org.uk>
2013-05-26 20:14:21 +0100

    email things

---
+++ src/Classes/Daemons/MyURY_EmailQueueDaemon.php
@@ -0,0 +1,21 @@
+<?php
+
+class MyURY_EmailQueueDaemon {
+  public static function isEnabled() { return true; }
+  
+  public static function run() {
+    echo "Checking for unsent emails...\n";
+    //Get up to 5 unsent emails
+    $db = Database::getInstance();
+    
+    $result = $db->fetch_column('SELECT emailid FROM mail.email WHERE
+      (email_id IN (SELECT DISTINCT email_id FROM mail.email_recipient_user WHERE sent=\'f\')
+      OR email_id IN (SELECT DISTINCT email_id FROM mail.email_recipient_list WHERE sent=\'f\')
+      AND timestamp > NOW() LIMIT 1');
+    
+    foreach ($result as $email) {
+      echo "Sending email $email\n";
+      MyURY_Email::getInstance($email)->send();
+    }
+  }
+}
