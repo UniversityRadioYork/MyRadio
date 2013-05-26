9ddd16cdfc335d2cbe5b6ba7aaf59ab96a45108d
Lloyd Wallis <lpw@ury.org.uk>
2013-05-26 20:17:20 +0100

    email things

--- src/Classes/Daemons/MyURY_EmailQueueDaemon.php
+++ src/Classes/Daemons/MyURY_EmailQueueDaemon.php
@@ -7,7 +7,7 @@ class MyURY_EmailQueueDaemon {
     //Get up to 5 unsent emails
     $db = Database::getInstance();
     
-    $result = $db->fetch_column('SELECT emailid FROM mail.email WHERE
+    $result = $db->fetch_column('SELECT email_id FROM mail.email WHERE
       (email_id IN (SELECT DISTINCT email_id FROM mail.email_recipient_user WHERE sent=\'f\')
       OR email_id IN (SELECT DISTINCT email_id FROM mail.email_recipient_list WHERE sent=\'f\'))
       AND timestamp > NOW() LIMIT 1');
