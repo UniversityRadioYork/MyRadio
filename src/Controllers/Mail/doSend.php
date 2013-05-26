da78c22c11f208daa1bab7d454aee478ed255b22
Lloyd Wallis <lpw@ury.org.uk>
2013-05-26 19:57:59 +0100

    email things

--- src/Controllers/Mail/doSend.php
+++ src/Controllers/Mail/doSend.php
@@ -13,4 +13,4 @@ $info = $form->readValues();
 
 MyURYEmail::sendEmailToList(MyURY_List::getInstance($info['list']), $info['subject'], $info['body'], User::getInstance());
 
-header('Location: '.$_SERVER['HTTP_REFERER'].'?message='.base64_encode('Message sent!'));
+header('Location: '.$_SERVER['HTTP_REFERER'] . (strstr($_SERVER['HTTP_REFERER'], '?') !== false ? '&' : '?') . 'message='.base64_encode('Message sent!'));
