a8351d2bed96478d5e7dcedc8ae6685d0197ff91
Lloyd Wallis <lpw@ury.org.uk>
2013-06-26 12:51:34 +0100

    getTimestamp bugfix

---
+++ src/Controllers/Stats/mostMessagedTimeslotYear.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The most messaged timeslots this academic year
+ * 
+ * @author Lloyd Wallis <lpw@ury.org.uk>
+ * @version 20130626
+ * @package MyURY_Stats
+ */
+require 'Views/bootstrap.php';
+
+CoreUtils::requireTimeslot();
+
+$twig->setTemplate('table.twig')
+        ->addVariable('title', 'Most messaged timeslots this academic year')
+        ->addVariable('heading', 'Most messaged timeslots this academic year')
+        ->addVariable('tabledata', MyURY_Timeslot::getMostMessaged(strtotime(CoreUtils::getAcademicYear().'-09-01')))
+        ->addVariable('tablescript', 'myury.datatable.default')
+        ->render();
