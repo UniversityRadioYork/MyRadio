4717180df1c5e09a1d7f361eef020836eb99bd9d
Lloyd Wallis <lpw@ury.org.uk>
2013-06-26 13:40:08 +0100

    stats

--- src/Controllers/Stats/mostListenedTimeslotYear.php
+++ src/Controllers/Stats/mostListenedTimeslotYear.php
@@ -14,5 +14,5 @@ $twig->setTemplate('table.twig')
         ->addVariable('title', 'Most listened to timeslots this academic year')
         ->addVariable('heading', 'Most listened to timeslots this academic year')
         ->addVariable('tabledata', MyURY_Timeslot::getMostListened(strtotime(CoreUtils::getAcademicYear().'-09-01')))
-        ->addVariable('tablescript', 'myury.stats.mostmessagedtimeslot')
+        ->addVariable('tablescript', 'myury.stats.mostlistenedtimeslot')
         ->render();
