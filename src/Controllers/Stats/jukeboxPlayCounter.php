879e9d3ebe1ebcb4f8627b6414c0abf4f5a3fd32
Lloyd Wallis <lpw@ury.org.uk>
2013-07-07 21:58:40 +0100

    New timeinput Template

---
+++ src/Controllers/Stats/jukeboxPlayCounter.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The most listened to timeslots this academic year
+ * 
+ * @author Lloyd Wallis <lpw@ury.org.uk>
+ * @version 20130626
+ * @package MyURY_Stats
+ */
+require 'Views/bootstrap.php';
+
+CoreUtils::requireTimeslot();
+
+$start = isset($_GET['rangesel-starttime']) ? strtotime($_GET['rangesel-starttime']) : null;
+$end = isset($_GET['rangesel-endtime']) ? strtotime($_GET['rangesel-endtime']) : null;
+
+$twig->setTemplate('table_timeinput.twig')
+        ->addVariable('title', 'Jukebox Track Play Counter')
+        ->addVariable('heading', 'Jukebox Track Play Counter')
+        ->addVariable('tabledata', MyURY_TracklistItem::getTracklistStatsForJukebox($start, $end))
+        ->addVariable('tablescript', 'myury.datatable.default')
+        ->render();
