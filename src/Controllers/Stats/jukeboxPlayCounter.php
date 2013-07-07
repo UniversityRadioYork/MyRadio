79d1f55910678e78390eef7889c6bfdc55ef2efe
Lloyd Wallis <lpw@ury.org.uk>
2013-07-07 23:57:37 +0100

    UI Improvement - fill in starttime/endtime values

--- src/Controllers/Stats/jukeboxPlayCounter.php
+++ src/Controllers/Stats/jukeboxPlayCounter.php
@@ -9,11 +9,13 @@
 require 'Views/bootstrap.php';
 
 $start = isset($_GET['rangesel-starttime']) ? strtotime($_GET['rangesel-starttime']) : time()-(86400*28);
-$end = isset($_GET['rangesel-endtime']) ? strtotime($_GET['rangesel-endtime']) : null;
+$end = isset($_GET['rangesel-endtime']) ? strtotime($_GET['rangesel-endtime']) : time();
 
 $twig->setTemplate('table_timeinput.twig')
         ->addVariable('title', 'Jukebox Track Play Counter')
         ->addVariable('heading', 'Jukebox Track Play Counter')
         ->addVariable('tabledata', MyURY_TracklistItem::getTracklistStatsForJukebox($start, $end))
         ->addVariable('tablescript', 'myury.stats.jukeboxplaycounter')
+        ->addVariable('starttime', CoreUtils::happyTime($start))
+        ->addVariable('endtime', CoreUtils::happyTime($end))
         ->render();
