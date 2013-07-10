60dbda24ee236a5a0cb4480991dcfa7eebf6799e
Lloyd Wallis <lpw@ury.org.uk>
2013-07-10 19:18:08 +0100

    Improved telnet reading

--- src/Classes/iTones/iTones_Utils.php
+++ src/Classes/iTones/iTones_Utils.php
@@ -81,13 +81,11 @@ class iTones_Utils extends ServiceAPI {
     }
 
     fwrite(self::$telnet_handle, $command . "\n");
-    echo $command."\n";
     $response = '';
     $line = '';
     do {
       $response .= $line;
       $line = fgets(self::$telnet_handle, 1048576); //Read a max of 1MB of data
-      echo "$line";
     } while (trim($line) !== 'END');
     
     
