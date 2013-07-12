0cd7c13932bd0cfdc47989c6332af0cec4954fb3
Lloyd Wallis <lpw@ury.org.uk>
2013-07-12 21:54:14 +0100

    Added a proxy for JS files on non-default version

--- src/Controllers/MyURY/StaticProxy.php
+++ src/Controllers/MyURY/StaticProxy.php
@@ -4,23 +4,20 @@
  * This provides access to these, at the cost of a substantial overhead.
  */
 
-$explode = array_reverse(explode('/', $_GET[0]));
+if (empty($_GET[0])) {
+  require 'default.php';
+  exit;
+}
 
 //For config.js, this is a Controller in this module.
-if ($explode[0] === 'config.js') require __DIR__.'/config.js.php'; exit;
-
-$path_prefix = '';
-
-for ($i = 1; $i < sizeof($explode); $i++) {
-  if ($explode[$i] === '.' or $explode[$i] === '..') exit; //DIRECTORY TRAVERSAL!
-  
-  if ($explode[$i] === 'img' or $explode[$i] === 'js') {
-    //We know where we are
-    $path = __DIR__.'/../Public'.$path_prefix.'/'.$explode[0];
-    header('Content-Type: '.mime_content_type($path));
-    echo file_get_contents($path);
-    exit;
-  }
-  
-  $path_prefix .= '/'.$explode[$i];
+if ($_GET[0] === 'config.js') {
+  require __DIR__.'/config.js.php';
+  exit;
 }
+
+$prefix = __DIR__.'/../../Public/';
+
+if (strstr($_GET[0], '..') !== false) exit;
+
+header('Content-Type: '.mime_content_type($prefix.$_GET[0]));
+echo file_get_contents($prefix.$_GET[0]);
