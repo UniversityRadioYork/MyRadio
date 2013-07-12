05014324492ca27210d4aa888c79d69ec060a806
Lloyd Wallis <lpw@ury.org.uk>
2013-07-12 14:11:50 +0100

    Work on Forms

---
+++ src/Models/iTones/editplaylistfrm.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * As well as eventually becoming a iTones Playlist Editor, this is also the test form for repeating fieldsets.
+ *
+ * @author Lloyd Wallis <lpw@ury.org.uk>
+ * @version 20130712
+ * @package MyURY_iTones
+ */
+$form = new MyURYForm('itones_playlistedit', $module, 'editPlaylist',
+                array(
+                    'debug' => true,
+                    'title' => 'Edit Campus Jukebox Playlist'
+        ));
+
+$form->addField(
+        new MyURYFormField('tracks', MyURYFormField::TYPE_TABULARSET,
+                array('options' => array(
+                    new MyURYFormField('track', MyURYFormField::TYPE_TRACK)
+                    )
+                )
+        )
+);
