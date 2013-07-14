4e3fd8641ffd2e12cf168be63ac54145e4e4738c
Lloyd Wallis <lpw@ury.org.uk>
2013-07-14 15:51:23 +0100

    Version debugging

--- src/Controllers/brokerVersion.php
+++ src/Controllers/brokerVersion.php
@@ -17,6 +17,7 @@ $versions = CoreUtils::getServiceVersions();
 // If the version selector has just been submitted, update the session
 if (isset($_REQUEST['svc_version'])) {
   $serviceid = Config::$service_id;
+  print_r($versions);
   foreach ($versions as $version) {
     if ($version['version'] === $_POST['svc_version']) {
       $_SESSION['myury_svc_version_'.$serviceid] = $version['version'];
