c5e2ed96cd1dccbc83e3586875e784220fa41739
Lloyd Wallis <lpw@ury.org.uk>
2013-07-17 15:38:20 +0100

    Implemented quickAdd

---
+++ src/Controllers/Profile/quickAdd.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Allows creation of new URY members!
+ * 
+ * @author Lloyd Wallis <lpw@ury.org.uk>
+ * @version 20130717
+ * @package MyURY_Profile
+ */
+User::getQuickAddFormForm()->render();
