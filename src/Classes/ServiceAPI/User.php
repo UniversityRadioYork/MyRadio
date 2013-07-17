ace2abc30f1c99503c9c2c46050be9caa734a4ce
Lloyd Wallis <lpw@ury.org.uk>
2013-07-17 14:54:30 +0100

    Added create User functionality

--- src/Classes/ServiceAPI/User.php
+++ src/Classes/ServiceAPI/User.php
@@ -766,8 +766,7 @@ class User extends ServiceAPI {
     }
     
     //Check if it looks like the user might already exist
-    if (!empty(User::findByEmail($params['eduroam'])) or !empty(User::findByEmail($params['email']))) {
-      print_r(User::findByEmail($params['email']));
+    if (User::findByEmail($params['eduroam']) !== null or User::findByEmail($params['email']) !== null) {
       throw new MyURYException('This user already appears to exist. Their eduroam or email is already used.');
     }
     
