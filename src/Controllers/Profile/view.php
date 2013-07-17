9ad51a5a2d95aed396a6a69f44a7fd6c0ba8b3e4
Lloyd Wallis <lpw@ury.org.uk>
2013-07-17 22:31:29 +0100

    More granular permissions for viewing profiles, and new MyURY_Photo class

--- src/Controllers/Profile/view.php
+++ src/Controllers/Profile/view.php
@@ -11,6 +11,7 @@
  */
 // Set if trying to view another member's profile page
 $user = User::getInstance(empty($_REQUEST['memberid']) ? -1 : $_REQUEST['memberid']);
+$visitor = User::getInstance();
 
 //Add global user data
 $userData = array(
@@ -28,7 +29,7 @@ if ($user->isOfficer()) {
   $userData['email'] = $user->getPublicEmail();
 }
 
-if (User::getInstance()->hasAuth(AUTH_VIEWOTHERMEMBERS)) {
+if ($visitor->hasAuth(AUTH_VIEWOTHERMEMBERS)) {
   $userData['email'] = $user->getEmail();
   $userData['eduroam'] = $user->getEduroam();
   $userData['local_alias'] = $user->getLocalAlias();
@@ -36,6 +37,7 @@ if (User::getInstance()->hasAuth(AUTH_VIEWOTHERMEMBERS)) {
   $userData['account_locked'] = $user->getAccountLocked();
   $userData['last_login'] = $user->getLastLogin();
   $userData['payment'] = $user->getAllPayments();
+  $userData['receive_email'] = $user->getReceiveEmail();
 }
 
 foreach ($userData['training'] as $k => $v) {
@@ -46,8 +48,18 @@ $template = CoreUtils::getTemplateObject()->setTemplate('Profile/user.twig')
         ->addVariable('title', 'View Profile')
         ->addVariable('user', $userData);
 
-if ($user === User::getInstance() or User::getInstance()->hasAuth(AUTH_EDITANYPROFILE)) {
-  $template->addInfo('<a href="'.CoreUtils::makeURL('Profile', 'edit',
-          array('memberid' => $user->getID())).'">Click here to update this profile</a>');
+if ($user === $visitor or $visitor->hasAuth(AUTH_EDITANYPROFILE)) {
+  $template->addVariable('editurl', '<a href="'.CoreUtils::makeURL('Profile', 'edit',
+          array('memberid' => $user->getID())).'">Edit Profile</a>');
+}
+if ($visitor->hasAuth(AUTH_IMPERSONATE) &&
+        (!$user->hasAuth(AUTH_BLOCKIMPERSONATE or $visitor->hasAuth(AUTH_IMPERSONATE_BLOCKED_USERS)))) {
+  $template->addVariable('impersonateurl',
+          '<a href="'.Config::$shib_url.'/impersonate.php?memberid='.$user->getID().'">Impersonate User</a>');
+}
+if ($visitor->hasAuth(AUTH_LOCK)) {
+  $template->addVariable('impersonateurl',
+          '<a href="'.CoreUtils::makeURL('Profile', 'lock',
+          array('memberid' => $user->getID())).'/impersonate.php?memberid='.$user->getID().'">Disable Account</a>');
 }
 $template->render();
