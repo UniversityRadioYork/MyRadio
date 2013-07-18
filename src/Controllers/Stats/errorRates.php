d854ce8f3a9d252798ffdebb3bbe937b234f5beb
Lloyd Wallis <lpw@ury.org.uk>
2013-07-18 17:55:38 +0100

    Stopped error spam

--- src/Controllers/Stats/errorRates.php
+++ src/Controllers/Stats/errorRates.php
@@ -6,9 +6,18 @@
  * @version 20130624
  * @package MyURY_Stats
  */
+$options = array(
+    'title' => 'MyURY Service Stats',
+    'series' => array(
+        array('targetAxisIndex' => 0),
+        array('targetAxisIndex' => 0),
+        array('targetAxisIndex' => 1)
+    )
+);
 CoreUtils::getTemplateObject()->setTemplate('linegraph.twig')
         ->addVariable('title', 'MyURY Error Rates')
         ->addVariable('data', json_encode(CoreUtils::getErrorStats()))
+        ->addVariable('options', json_encode($options))
         ->addVariable('caption', 'This graph shows information error rate statistics for the last 24 hours.
           Peaks in this graph can suggest a problem.')
         ->render();
