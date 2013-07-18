17d9941510f709e0d5e9fea6d30edd99c44e3a42
Lloyd Wallis <lpw@ury.org.uk>
2013-07-18 01:27:09 +0100

    Error rate stats

--- src/Controllers/Stats/errorRates.php
+++ src/Controllers/Stats/errorRates.php
@@ -9,4 +9,6 @@
 CoreUtils::getTemplateObject()->setTemplate('linegraph.twig')
         ->addVariable('title', 'MyURY Error Rates')
         ->addVariable('data', json_encode(CoreUtils::getErrorStats()))
+        ->addVariable('caption', 'This graph shows information error rate statistics for the last 24 hours.
+          Peaks in this graph can suggest a problem.')
         ->render();
