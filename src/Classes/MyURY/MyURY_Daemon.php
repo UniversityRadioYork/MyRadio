5659dcc1b62bb5a6f0eeca0c1c413f2bccbf3545
Lloyd Wallis <lpw@ury.org.uk>
2013-07-19 20:23:23 +0100

    Apparently I broke daemons

--- src/Classes/MyURY/MyURY_Daemon.php
+++ src/Classes/MyURY/MyURY_Daemon.php
@@ -5,11 +5,11 @@ abstract class MyURY_Daemon {
   public static function run() {throw new MyURYException('NO RUN METHOD FOR '.get_called_class().'!');}
   
   protected static function getVal($key) {
-    return json_decode(file_get_contents(Config::$daemon_lock_file), true)[$key];
+    return @json_decode(file_get_contents(Config::$daemon_lock_file), true)[$key];
   }
   
   protected static function setVal($key, $value) {
-    $data = json_decode(file_get_contents(Config::$daemon_lock_file), true);
+    $data = @json_decode(file_get_contents(Config::$daemon_lock_file), true);
     $data[$key] = $value;
     
     file_put_contents(Config::$daemon_lock_file, json_encode($data));
