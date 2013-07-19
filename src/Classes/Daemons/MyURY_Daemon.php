402a5247a74b6473de415550d51db405183c5322
Lloyd Wallis <lpw@ury.org.uk>
2013-07-19 20:04:37 +0100

    Added Fingerprinter correction proposals

--- src/Classes/Daemons/MyURY_Daemon.php
+++ src/Classes/Daemons/MyURY_Daemon.php
@@ -2,7 +2,7 @@
 
 abstract class MyURY_Daemon {
   public static function isEnabled() { return false; }
-  public static function run();
+  public abstract static function run();
   
   protected static function getCache($key) {
     $prov = Config::$cache_provider;
