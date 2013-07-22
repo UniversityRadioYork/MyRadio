06ecc7c60da3f754aac38d5c367d21549e64ad8d
Lloyd Wallis <lpw@ury.org.uk>
2013-07-22 20:35:20 +0100

    Improved Track Edit form

--- src/Controllers/MyURY/a-findalbum.php
+++ src/Controllers/MyURY/a-findalbum.php
@@ -8,7 +8,11 @@
  * @version 20130722
  * @package MyURY_Core
  */
-if (!isset($_REQUEST['term'])) $data = array(); else {
+if (isset($_REQUEST['id'])) {
+  $data = MyURY_Album::getInstance($_REQUEST['id']);
+} elseif (!isset($_REQUEST['term'])) {
+  $data = array();
+} else {
   $data = MyURY_Album::findByName($_REQUEST['term'],
           isset($_REQUEST['limit']) ? intval($_REQUEST['limit']) : Config::$ajax_limit_default);
 }
