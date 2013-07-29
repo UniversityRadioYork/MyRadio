bba780f0c801916be297d4abec235b9c2fc5a756
Lloyd Wallis <lpw@ury.org.uk>
2013-07-29 02:38:39 +0100

    Done playing.

--- src/Controllers/api/default.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'Classes/Vendor/Restler/vendor/restler.php';
-use Luracast\Restler\Restler;
-
-$r = new Restler();
-$r->addAPIClass('CoreUtils'); // repeat for more
-$r->handle(); //serve the response
