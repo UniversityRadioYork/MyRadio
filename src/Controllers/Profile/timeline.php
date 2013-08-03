bcccd60cb4811417d31b1f2917cd755640743292
Lloyd Wallis <lpw@ury.org.uk>
2013-08-03 15:35:12 +0100

    Timeline\!

--- src/Controllers/Profile/timeline.php
+++ src/Controllers/Profile/timeline.php
@@ -7,9 +7,11 @@
  * @version 21072012
  * @package MyURY_Profile
  */
-$data = User::getInstance(isset($_GET['memberid']) ? $_GET['memberid'] : $_SESSION['memberid'])->getTimeline();
+$user = User::getInstance(isset($_GET['memberid']) ? $_GET['memberid'] : $_SESSION['memberid']);
+$data = $user->getTimeline();
 
 CoreUtils::getTemplateObject()->setTemplate('Profile/timeline.twig')
         ->addVariable('title', 'Timeline')
         ->addVariable('timeline', $data)
+        ->addVariable('profile_name', $user->getName())
         ->render();
