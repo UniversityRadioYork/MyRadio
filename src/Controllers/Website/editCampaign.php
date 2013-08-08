8d0027cbc50b7aeb113f40199f4cab0de25eba5e
Lloyd Wallis <lpw@ury.org.uk>
2013-08-08 22:24:01 +0100

    Designed campaign edit form

--- src/Controllers/Website/editCampaign.php
+++ src/Controllers/Website/editCampaign.php
@@ -7,8 +7,8 @@
  * @package MyURY_Website
  */
 
-(new MyURYForm('test', 'Website', 'doEditCampaign'))
-        ->addField(new MyURYFormField('test', MyURYFormField::TYPE_WEEKSELECT,[
-            value => MyURY_BannerCampaign::getInstance($_REQUEST['campaignid'])->getTimeslots()
-        ]))
-                ->render();
+if (!isset($_REQUEST['campaignid'])) {
+  throw new MyURYException('You must provide a campaignid', 400);
+}
+
+MyURY_BannerCampaign::getInstance($_REQUEST['campaignid'])->getEditForm()->render();
