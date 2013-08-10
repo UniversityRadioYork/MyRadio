a9eed8eb7ef96417e5d2a4f7ffd7f7dea883d4a8
Lloyd Wallis <lpw@ury.org.uk>
2013-08-10 14:49:50 +0100

    Some joyride fixes

--- src/Controllers/Scheduler/myShows.php
+++ src/Controllers/Scheduler/myShows.php
@@ -9,8 +9,8 @@
 
 $shows = MyURY_Show::getShowsAttachedToUser();
 
-//This is a Joyride start point - if there are now shows, run the first show joyride.
-if (empty($shows)) {
+//This is a Joyride start point - if there are no shows, or it's their first season, run the first show joyride.
+if (empty($shows) or sizeof($shows) === 1 and empty($shows[0]->getSeasons())) {
   $_SESSION['joyride'] = 'first_show';
 }
 
