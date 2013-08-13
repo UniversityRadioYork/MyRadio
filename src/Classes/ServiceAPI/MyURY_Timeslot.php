09801f7d1d8348b0ce9457813938250cbcc043db
Lloyd Wallis <lpw@ury.org.uk>
2013-08-13 18:56:08 +0100

    API Work

--- src/Classes/ServiceAPI/MyURY_Timeslot.php
+++ src/Classes/ServiceAPI/MyURY_Timeslot.php
@@ -73,7 +73,7 @@ class MyURY_Timeslot extends MyURY_Scheduler_Common {
             JOIN schedule.show_season USING (show_season_id)
             WHERE show_season_timeslot_id=$1
           ) AND effective_from <= NOW() AND (effective_to IS NULL OR effective_to >= NOW()) AND approvedid IS NOT NULL
-         ORDER BY show_credit_id)) AS credit_types,
+         ORDER BY show_credit_id)) AS credit_types
       FROM schedule.show_season_timeslot WHERE show_season_timeslot_id=$1', array($timeslot_id));
     if (empty($result)) {
       //Invalid Season
