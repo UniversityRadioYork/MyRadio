e6295ed4fa6406146319df831fcb172a86048645
Lloyd Wallis <lpw@ury.org.uk>
2013-08-15 19:40:55 +0100

    Podcasts

---
+++ src/Controllers/Podcast/default.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * List a User's Podcasts
+ * @author Lloyd Wallis <lpw@ury.org.uk>
+ * @version 20130815
+ * @package MyURY_Podcast
+ */
+
+CoreUtils::getTemplateObject()->setTemplate('table.twig')
+        ->addVariable('tablescript', 'myury.datatable.default')
+        ->addVariable('title', 'My Podcasts')
+        ->addVariable('tabledata', 
+                ServiceAPI::setToDataSource(MyURY_Podcast::getPodcastsAttachedToUser()))
+        ->render();
