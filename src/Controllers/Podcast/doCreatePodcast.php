78bf4434f81b6e1152587efa2da5714411757b46
Lloyd Wallis <lpw@ury.org.uk>
2013-08-16 12:30:15 +0100

    Podcasts

---
+++ src/Controllers/Podcast/doCreatePodcast.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Render form to create a new Podcast
+ * @author Lloyd Wallis <lpw@ury.org.uk>
+ * @version 20130815
+ * @package MyURY_Podcast
+ */
+
+MyURY_Podcast::getCreateForm()->render();
