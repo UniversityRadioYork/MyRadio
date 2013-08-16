5d57912a66e527e80393b08436bd76ac09e7d1e5
Lloyd Wallis <lpw@ury.org.uk>
2013-08-16 16:32:27 +0100

    Podcasts

--- src/Controllers/MyURY/a-getuploadprogress.php
+++ src/Controllers/MyURY/a-getuploadprogress.php
@@ -7,12 +7,11 @@
  * @version 20130816
  * @package MyURY_Core
  */
-if (!function_exists("apc_fetch")) {
-  trigger_error('The APC extension is not installed.');
-} else if (!ini_get("apc.rfc1867")) {
-  trigger_error('apc.rfc1867 is not enabled. ' . ini_get("apc.rfc1867"));
+if (function_exists("uploadprogress_get_info")) {
+  $data = uploadprogress_get_info($_REQUEST['id']);
+} else {
+  trigger_error('uploadprogress PECL extension is not installed.');
+  $data = false;
 }
-
-$data = apc_fetch('upload_' . $_REQUEST['id']);
 
 require 'Views/MyURY/datatojson.php';
