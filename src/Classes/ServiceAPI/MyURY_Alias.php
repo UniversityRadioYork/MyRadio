2e4981e0f19603228d1fba8fa13557f8e6717f9b
Lloyd Wallis <lpw@ury.org.uk>
2013-08-22 23:36:31 +0100

    Added Alias boilerplate and text aliases

--- src/Classes/ServiceAPI/MyURY_Alias.php
+++ src/Classes/ServiceAPI/MyURY_Alias.php
@@ -47,7 +47,7 @@ abstract class MyURY_Alias extends ServiceAPI {
       throw new MyURYException($id.' is not a valid Alias ID!', 400);
     }
     if (!isset(self::$aliases[$id])) {
-      self::$aliases[$id] = (get_called_class())::__construct($id);
+      self::$aliases[$id] = new (get_called_class())($id);
     }
     return self::$aliases[$id];
   }
