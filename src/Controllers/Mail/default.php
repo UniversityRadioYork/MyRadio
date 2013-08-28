7692d1be82513171c83f2f0e0ce903bbd617be86
Lloyd Wallis <lpw@ury.org.uk>
2013-08-28 23:34:19 +0100

    Prettified the Mailing Lists List... list?

--- src/Controllers/Mail/default.php
+++ src/Controllers/Mail/default.php
@@ -9,7 +9,7 @@
  */
 
 CoreUtils::getTemplateObject()->setTemplate('table.twig')
-        ->addVariable('tablescript', 'myury.datatable.default')
+        ->addVariable('tablescript', 'myury.mail.default')
         ->addVariable('title', 'All Mailing Lists')
         ->addVariable('tabledata', CoreUtils::dataSourceParser(MyURY_List::getAllLists()))
         ->addInfo('You will only get any messages from URY if you are set to "Receive Email" on your <a href="'.
