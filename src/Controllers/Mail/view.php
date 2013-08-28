db27535d24146f767f7921d1e21cb75171b90c16
Lloyd Wallis <lpw@ury.org.uk>
2013-08-28 23:22:10 +0100

    Ensured memebrs can only see emails they are supposed to

--- src/Controllers/Mail/view.php
+++ src/Controllers/Mail/view.php
@@ -10,6 +10,10 @@
 
 $email = MyURYEmail::getInstance($_REQUEST['emailid']);
 
+if (!$email->isRecipient(User::getInstance())) {
+  throw new MyURYException('You can only view emails you are a recipient of.', 403);
+}
+
 CoreUtils::getTemplateObject()->setTemplate('MyURY/text.twig')
         ->addVariable('title', $email->getSubject())
         ->addVariable('text', '<a href="'.$_SERVER['HTTP_REFERER'].'">Back</a><hr>'.
