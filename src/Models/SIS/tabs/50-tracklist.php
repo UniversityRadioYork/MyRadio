42f6007007e37fdd3f1cba8538272f7e7a96c64d
Andy Durant <andy@ajdurant.co.uk>
2013-09-26 00:43:22 +0100

    boilerplate - mostly done

---
+++ src/Models/SIS/tabs/50-tracklist.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Track Listing Tab for SIS
+ * 
+ * @author Andy Durant <aj@ury.org.uk>
+ * @version 20130925
+ * @package MyURY_SIS
+ */
+
+$name = 'tracklist';
+$title = 'Track Listing';
+$enabled = true;
+$help = 'Tracklisting is a legal requirement for URY to broadcast, so you must fill this in. If you use BAPS this will be done automatically, but if you use other sources you must fill this in yourself'
+
+$template = 'SIS/tabs/tracklist.twig'
+
+/**
+ * @todo: query_bapslog
+ */
