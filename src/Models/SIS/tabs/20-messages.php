447a2b1220fc34b4a81a408395878bde0e0d02de
Andy Durant <andy@ajdurant.co.uk>
2013-09-27 00:04:08 +0100

    SIS: beginnings of class functions

--- src/Models/SIS/tabs/20-messages.php
+++ src/Models/SIS/tabs/20-messages.php
@@ -7,12 +7,13 @@
  * @package MyURY_SIS
  */
 
-$name = 'messages';
-$title = 'Messages';
-$enabled = true;
-$help = 'This is the big one, probably where you will spen most of your time in SIS. The Message tab provides you with all the comunication you can get with the listener, whether the message "Via the website" or text the studio it all comes here.'
-
-$template = 'SIS/tabs/messages.twig'
+$moduleInfo = array(
+'name' => 'messages',
+'title' => 'Messages',
+'enabled' => true,
+'help' => 'This is the big one, probably where you will spen most of your time in SIS. The Message tab provides you with all the comunication you can get with the listener, whether the message "Via the website" or text the studio it all comes here.',
+'template' => 'SIS/tabs/messages.twig'
+)
 
   /**
    * @todo: query_messages ?
