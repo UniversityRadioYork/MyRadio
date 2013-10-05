ec726f22b44702b20a74e02a99d86cac386719ac
Lloyd Wallis <lpw@ury.org.uk>
2013-10-05 12:00:36 +0100

    Bug fix for 'it doesn't work' from andy

--- src/Classes/ServiceAPI/MyURY_Timeslot.php
+++ src/Classes/ServiceAPI/MyURY_Timeslot.php
@@ -323,8 +323,7 @@ class MyURY_Timeslot extends MyURY_Metadata_Common {
       for ($i = 0; $i < $n; $i++) {
         $next = ($next instanceof MyURY_Timeslot) ? $next->getTimeslotAfter() : [];
         if (empty($next)) {
-          $nextshow = self::getNextTimeslot($timeslot->getStartTime() + 1)
-                      ->getStartTime();
+          $nextshow = self::getNextTimeslot($timeslot->getStartTime() + 1);
           $end = $next === null ? null : $nextshow->getStartTime();
           //There's not a next show, but there might be one later
           $response['next'][] = ['title' => 'Jukebox',
