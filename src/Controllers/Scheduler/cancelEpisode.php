b87b4fe4bc669a38d6f5fbd27f4b790e0781d0f9
Lloyd Wallis <lpw@ury.org.uk>
2013-10-16 23:51:34 +0100

    Added show_season_timeslot_id constraint to cancelEpisode

--- src/Controllers/Scheduler/cancelEpisode.php
+++ src/Controllers/Scheduler/cancelEpisode.php
@@ -3,10 +3,13 @@
  * Presents a form to the user to enable them to cancel an Episode
  * 
  * @author Lloyd Wallis <lpw@ury.org.uk>
- * @version 05012013
+ * @version 20131016
  * @package MyURY_Scheduler
  */
 
+if (!isset($_REQUEST['show_season_timeslot_id'])) {
+  throw new MyURYException('No timeslotid provided.', 400);
+}
 //The Form definition
 require 'Models/Scheduler/reasonfrm.php';
 //'tis a one line view
