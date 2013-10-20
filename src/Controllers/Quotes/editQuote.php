a413e2f675533be099e41594cb53861e2e27cd9c
Matt Windsor <matt.windsor@ury.org.uk>
2013-10-20 14:51:16 +0100

    Initial push of quotes stuff to make the links work for further testing.

---
+++ src/Controllers/Quotes/editQuote.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Allows the editing of quotes.
+ * @version 20131020
+ * @author Matt Windsor <matt.windsor@ury.org.uk>
+ * @package MyURY_Quotes
+ */
+
+$form = MyURY_JsonFormLoader::loadFromModule(
+  $module, 'quotefrm', 'doEditQuote',
+  []
+);
+
+if ($_REQUEST['quote_id']) {
+  $quote = MyURY_ChartRelease::getInstance($_REQUEST['quote_id']);
+
+  $form->editMode(
+    $quote->getID(),
+    array_merge(
+      [
+        'date'   => CoreUtils::happyTime($quote->getDate(), false),
+        'source' => $quote->getSource(),
+        'text'   => $quote->getText()
+      ],
+      $chart_rows_form
+    )
+  );
+
+} else {
+  $form->setTitle('Create Quote');
+  $form->setFieldValue('date', CoreUtils::happyTime(time(), false));
+}
+
+$form->render();
+?>
