04ca152dfec9f917b7dc654b832a81d1169ade46
Andy Durant <andy@ajdurant.co.uk>
2013-10-25 21:30:32 +0100

    SIS: selector fix

--- src/Controllers/SIS/selector.query.php
+++ src/Controllers/SIS/selector.query.php
@@ -2,10 +2,10 @@
 $sel = new MyURY_Selector();
 
 $status = $sel->query();
-
+$onair = $status['studio'];
+$locked = $status['lock'];
 $power = $status['power'];
 $s1power = ($power & 1);
 $s2power = ($power & 2) >> 1;
-
-print "$lastmod $s1power $s2power $status['studio'] $status['lock']";
+print "$lastmod $s1power $s2power $onair $locked";
 return;
