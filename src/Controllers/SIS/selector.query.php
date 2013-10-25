17bc7c27b53fb6aa16eba5a2d1b4a89ff3e51cea
Andy Durant <andy@ajdurant.co.uk>
2013-10-25 21:26:56 +0100

    SIS: selector debug

--- src/Controllers/SIS/selector.query.php
+++ src/Controllers/SIS/selector.query.php
@@ -7,6 +7,6 @@ $locked = (int) $status[1];
 $power = (int) $status[3];
 $s1power = ($power & 1);
 $s2power = ($power & 2) >> 1;
-print $status;
+var_dump($status);
 print "$lastmod $s1power $s2power $onair $locked";
 return;
