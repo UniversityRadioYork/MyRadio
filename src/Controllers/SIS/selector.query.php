bcfecdc29ecf531bf40a9d08169d05c5a3fe6e13
Andy Durant <andy@ajdurant.co.uk>
2013-10-25 21:24:08 +0100

    SIS: selector string index

--- src/Controllers/SIS/selector.query.php
+++ src/Controllers/SIS/selector.query.php
@@ -2,9 +2,9 @@
 $sel = new MyURY_Selector();
 
 $status = $sel->query();
-$onair = (int) $status[0][0];
-$locked = (int) $status[0][1];
-$power = (int) $status[0][3];
+$onair = (int) $status[0];
+$locked = (int) $status[1];
+$power = (int) $status[3];
 $s1power = ($power & 1);
 $s2power = ($power & 2) >> 1;
 print "$lastmod $s1power $s2power $onair $locked";
