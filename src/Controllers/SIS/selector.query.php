35697ebe2de957d310f62ce52b572d2fa157ef33
Andy Durant <andy@ajdurant.co.uk>
2013-10-25 21:32:14 +0100

    SIS: selector lastmod

--- src/Controllers/SIS/selector.query.php
+++ src/Controllers/SIS/selector.query.php
@@ -7,5 +7,6 @@ $locked = $status['lock'];
 $power = $status['power'];
 $s1power = ($power & 1);
 $s2power = ($power & 2) >> 1;
+$lastmod = 0;
 print "$lastmod $s1power $s2power $onair $locked";
 return;
