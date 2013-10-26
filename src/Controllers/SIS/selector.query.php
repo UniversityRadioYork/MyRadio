d3eb0eec21dc97abaf1f9ddd2f242380182dbf99
Andy Durant <andy@ajdurant.co.uk>
2013-10-26 01:08:28 +0100

    SIS: selector json & tidy

--- src/Controllers/SIS/selector.query.php
+++ src/Controllers/SIS/selector.query.php
@@ -1,12 +1,13 @@
 <?php
-$sel = new MyURY_Selector();
+/**
+ * Selector Query for SIS
+ * 
+ * @author Andy Durant <aj@ury.org.uk>
+ * @version 20131026
+ * @package MyURY_SIS
+ */
 
-$status = $sel->query();
-$onair = $status['studio'];
-$locked = $status['lock'];
-$power = $status['power'];
-$s1power = ($power & 1);
-$s2power = ($power & 2) >> 1;
-$lastmod = 0;
-print "$lastmod $s1power $s2power $onair $locked";
-return;
+$sel = new MyURY_Selector();
+$data = $sel->query();
+
+require 'Views/MyURY/datatojson.php';
