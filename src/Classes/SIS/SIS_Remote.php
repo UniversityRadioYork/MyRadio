4affe0f3e1af2bbe043d232f7bedecef33b9e603
Andy Durant <andy@ajdurant.co.uk>
2013-11-01 18:27:53 +0000

    SIS: test function call 1up

--- src/Classes/SIS/SIS_Remote.php
+++ src/Classes/SIS/SIS_Remote.php
@@ -20,6 +20,6 @@ class SIS_Remote extends ServiceAPI {
 		// if (!empty($response) && $response !== false) {
 		// 	return array('messages' => $response);
 		// }
-		return MyRadio_Timeslot::getInstance($session['timeslotid'])->getMessages();
+		return SIS_Messages::getMessages($session['timeslotid'], $_REQUEST['messages_highest_id']);
 	}
 }
