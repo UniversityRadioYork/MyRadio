582a7b1b7709308e847df03b07c69ed70dd136be
Andy Durant <andy@ajdurant.co.uk>
2013-11-01 18:08:07 +0000

    SIS: test function call2

--- src/Classes/SIS/SIS_Remote.php
+++ src/Classes/SIS/SIS_Remote.php
@@ -20,6 +20,6 @@ class SIS_Remote extends ServiceAPI {
 		// if (!empty($response) && $response !== false) {
 		// 	return array('messages' => $response);
 		// }
-		return 'beans';
+		return $session['timeslotid'];
 	}
 }
