a8c6cc50478e99333d5a6246cf8aab7c9aaa2206
Andy Durant <andy@ajdurant.co.uk>
2013-11-01 16:53:32 +0000

    Comet

---
+++ src/Controllers/SIS/remote.php
@@ -0,0 +1,20 @@
+<?php
+
+//Allow Session writing from other requests
+$session = $_SESSION;
+session_write_close();
+
+$pollFuncs = SIS_Utils::readPolls();
+
+$data = array();
+do {
+  foreach ($pollFuncs as $function) {
+    $temp = call_user_func($function, $session);
+    if (!empty($temp)) $data = array_merge ($data, $temp);
+  }
+  sleep(1);
+  $count++;
+} while (empty($data) && $count < 50);
+
+//Return the response data
+require 'Views/MyRadio/datatojson.php';
