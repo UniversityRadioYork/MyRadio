f63772d79bc4a5d3abd806eefcc01c0320ceab36
Andy Durant <andy@ajdurant.co.uk>
2013-11-01 17:56:22 +0000

    SIS: debug calling function

--- src/Models/SIS/tabs/20-messages.php
+++ src/Models/SIS/tabs/20-messages.php
@@ -8,19 +8,11 @@
  */
 
 
-function query_messages($session) {
-  $response = SIS_Messages::getMessages($session['timeslotid'], $_REQUEST['messages_highest_id']);
-
-  if (!empty($response) && $response !== false) {
-    return array('messages' => $response);
-  }
-};
-
 $moduleInfo = array(
 'name' => 'messages',
 'title' => 'Messages',
 'enabled' => true,
 'help' => 'This is the big one, probably where you will spen most of your time in SIS. The Message tab provides you with all the comunication you can get with the listener, whether the message "Via the website" or text the studio it all comes here.',
 'template' => 'SIS/tabs/messages.twig',
-'pollfunc' => 'query_messages'
+'pollfunc' => 'SIS_Remote::query_messages'
 );
