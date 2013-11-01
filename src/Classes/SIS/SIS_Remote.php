14290183f3f2e9f9b22dbb309d3c23eee950aa4e
Andy Durant <andy@ajdurant.co.uk>
2013-11-01 17:51:15 +0000

    pollfunctios to SIS_Remote

---
+++ src/Classes/SIS/SIS_Remote.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file provides the SIS_Remote class for MyRadio
+ * @package MyRadio_SIS
+ */
+
+/**
+ * This class has helper functions for long-polling SIS
+ * 
+ * @version 20131101
+ * @author Andy Durant <aj@ury.org.uk>
+ * @package MyRadio_SIS
+ */
+class SIS_Remote extends ServiceAPI {
+
+	public static function query_messages($session) {
+		$response = SIS_Messages::getMessages($session['timeslotid'], $_REQUEST['messages_highest_id']);
+
+		if (!empty($response) && $response !== false) {
+			return array('messages' => $response);
+		}
+	};
+}
