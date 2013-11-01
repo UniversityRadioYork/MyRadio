51a801e541602eedd4c67886f66339ddc04aacc7
Andy Durant <andy@ajdurant.co.uk>
2013-11-01 22:42:37 +0000

    SIS: module include unification

--- src/Models/SIS/tabs/50-tracklist.php
+++ src/Models/SIS/tabs/50-tracklist.php
@@ -12,7 +12,6 @@ $moduleInfo = array(
 'title' => 'Track Listing',
 'enabled' => true,
 'help' => 'Tracklisting is a legal requirement for '.Config::$short_name.' to broadcast, so you must fill this in. If you use BAPS this will be done automatically, but if you use other sources you must fill this in yourself',
-'template' => 'SIS/tabs/tracklist.twig',
 );
 
 /**
