61fdc87ddf0c0ad9604727514339a8709c6438f7
Andy Durant <andy@ajdurant.co.uk>
2013-11-01 22:42:37 +0000

    SIS: module include unification

--- src/Models/SIS/plugins/20-selector.php
+++ src/Models/SIS/plugins/20-selector.php
@@ -27,7 +27,6 @@ $moduleInfo = array(
 'enabled' => true,
 'startOpen' => false,
 'help' => 'To the right, you can see a Studio Selector button. This contains a digital version of the magic buttons you can see in each studio. Don\'t worry - most members can\'t see this so they won\'t go messing with your show from the comfort of their own homes. You must have special priveleges if you can see this! Please use it carefully - it includes a fourth button, Outside Broadcast, which usually plays placeholder noises, which are a terrible substitute for a radio show.',
-'template' => 'SIS/plugins/selector.twig',
 'vars' => $vars,
 'required_permission' => AUTH_MODIFYSELECTOR,
 'required_location' => false
--- src/Models/SIS/tabs/20-messages.php
+++ src/Models/SIS/tabs/20-messages.php
@@ -13,6 +13,5 @@ $moduleInfo = array(
 'title' => 'Messages',
 'enabled' => true,
 'help' => 'This is the big one, probably where you will spen most of your time in SIS. The Message tab provides you with all the comunication you can get with the listener, whether the message "Via the website" or text the studio it all comes here.',
-'template' => 'SIS/tabs/messages.twig',
 'pollfunc' => 'SIS_Remote::query_messages'
 );
