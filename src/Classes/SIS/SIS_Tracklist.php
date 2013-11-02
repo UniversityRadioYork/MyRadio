8835ba2a748827d336228cd52e3f9764816b9b7c
Andy Durant <andy@ajdurant.co.uk>
2013-11-02 03:32:37 +0000

    SIS check & find track

--- src/Classes/SIS/SIS_Tracklist.php
+++ src/Classes/SIS/SIS_Tracklist.php
@@ -14,6 +14,12 @@
  */
 class SIS_Tracklist extends ServiceAPI {
 
+	/**
+	 * Get track info tracklisted for a timeslot
+	 * @param  integer  $timeslotid ID of timslot to get tracklist for
+	 * @param  integer  $offset     tracklist logid to offset by
+	 * @return array                tracks in tracklist for the timeslot from the offset
+	 */
 	public static function getTrackListing($timeslotid, $offset = 0) {
 		$tracklist = MyRadio_TracklistItem::getTracklistForTimeslot($timeslotid, $offset);
 		$tracks = array();
@@ -40,4 +46,91 @@ class SIS_Tracklist extends ServiceAPI {
 		}
 		return $tracks;
 	}
+
+	/**
+	 * Adds a non-database track to the tracklist
+	 * @param  string $tname      track name
+	 * @param  string $artist     track artist
+	 * @param  string $album      track album
+	 * @param  time   $time       php time
+	 * @param  string $source     tracklistig source
+	 * @param  int    $timeslotid ID of timeslot to tracklist to
+	 * @return none
+	 */
+	 public static function insertTrackNoRec($tname, $artist, $album, $time, $source, $timeslotid) {
+	 	$tname = pg_escape_string($tname);
+		$artist = pg_escape_string($artist);
+		$album = pg_escape_string($album);
+		$source = pg_escape_string($source);
+		$timeslotid = pg_escape_string($timeslotid);
+
+		self::$db->query('BEGIN');
+		$audiologid = self::$db->query('INSERT INTO tracklist.tracklist (source, timeslotid) 
+			VALUES ($1, $2) RETURNING audiologid',
+			array($source, $timeslotid));
+
+		$r = self::$db->query('INSERT INTO tracklist.track_notrec (audiologid, artist, album, track) 
+			VALUES ($1, $2, $3, $4)',
+			array($audiologid[0], $artist, $album, $tname));
+
+		if (!$r) {
+			$query = "ROLLBACK;";
+		}
+		else {
+			$query = "COMMIT";
+		}
+		self::$db->query($query);
+	}
+
+	/**
+	 * checks if track is in database
+	 * @param  string $artist track artist
+	 * @param  string $album  track album
+	 * @param  string $tname  track name
+	 * @return array          result of db query
+	 */
+	public static function checkTrackOK($artist, $album, $tname) {
+	 	$artist = pg_escape_string($artist);
+	 	$album = pg_escape_string($album);
+	 	$tname = pg_escape_string($tname);
+
+	 	$result = self::$db->query('SELECT DISTINCT trk.title AS track, rec.title AS album, trk.artist AS artist, trk.trackid AS trackid, rec.recordid AS recordid
+	 		FROM rec_track trk
+	 		INNER JOIN rec_record rec ON ( rec.recordid = trk.recordid )
+	 		WHERE trk.artist ILIKE $1
+	 		AND rec.title ILIKE $2
+	 		AND trk.title ILIKE $3
+	 		ORDER BY trk.title ASC LIMIT 10',
+	 		array($artist, $album, $tname));
+	 	return $result;
+	}
+
+	public function insertTrackRec($trackid, $recid, $time, $source, $timeslotid) {
+		$trackid = pg_escape_string($trackid);
+		$recid = pg_escape_string($recid);
+		$time = pg_escape_string($time);
+		$source = pg_escape_string($source);
+		$timeslotid = pg_escape_string($timeslotid);
+		
+		self::$db->query('BEGIN');
+
+		$audiologid = self::$db->query('INSERT INTO tracklist.tracklist (source, timeslotid) 
+			VALUES ($1, $2) RETURNING audiologid',
+			array($source, $timeslotid));
+
+		$audiologid = pg_fetch_row($audiologid);
+
+		$r = self::$db->query('INSERT INTO tracklist.track_rec (audiologid, recordid, trackid) 
+			VALUES ($1, $2, $3)',
+			array($audiologid[0], $recid, $trackid));
+
+		if (!$r){
+			$query = "ROLLBACK;";
+		}
+		else {
+			$query = "COMMIT";
+		}
+		self::$db->query($query);
+	}
+	
 }
