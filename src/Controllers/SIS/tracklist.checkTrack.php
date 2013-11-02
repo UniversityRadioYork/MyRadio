0d4e38ee2aacdcfd6616c6024031ce2ae818b790
Andy Durant <andy@ajdurant.co.uk>
2013-11-02 03:32:37 +0000

    SIS check & find track

--- src/Controllers/SIS/tracklist.checkTrack.php
+++ src/Controllers/SIS/tracklist.checkTrack.php
@@ -1,9 +1,42 @@
 <?php
 /**
- * Tracklist Track Checker for SIS
+ * Tracklist Track Inserter for SIS
  * 
  * @author Andy Durant <aj@ury.org.uk>
  * @version 20131101
  * @package MyRadio_SIS
  */
 
+$artist = $_GET['artist'];
+$album = $_GET['album'];
+$tname = $_GET['tname'];
+$timeslotid = $_GET['timeslotid'];
+$where = $_GET['where'];
+
+if ($where == "notrec"){
+	SIS_Tracklist::insertTrackNoRec($tname, $artist, $album, time(), "m", $timeslotid);
+	header('HTTP/1.1 204 No Content');
+}
+
+else if($where == 'rec'){
+	$data = SIS_Tracklist::checkTrackOK($artist, $album, $tname);
+	$numrow = pg_num_rows($data);
+	$row = pg_fetch_row($data);
+	$return = 0;
+	if ($numrow != 1){
+		if($numrow == 0){
+			$return = 1;
+		}
+		elseif($numrow >= 2){
+			$return = 2;
+		}
+	}
+	elseif ($numrow == 1){
+		SIS_Tracklist::insertTrackRec($row[3], $row[4], time(), "m", $timeslotid);
+		$return = 0;
+	}
+
+	$data = array("return"=>$return, "result"=>$row);
+	//Return the response data
+	require 'Views/MyRadio/datatojson.php';
+}
