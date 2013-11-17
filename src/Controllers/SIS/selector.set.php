91c04fa0d065d0fa3f32e1ffc0d363bddf905d2d
Andy Durant <andy@ajdurant.co.uk>
2013-11-17 19:32:21 +0000

    SIS: selector - it might work

--- src/Controllers/SIS/selector.set.php
+++ src/Controllers/SIS/selector.set.php
@@ -10,41 +10,34 @@
 $src = (isset($_REQUEST['src'])) ? (int) $_REQUEST['src'] : 0;
 
 if (($src <= 0) || ($src > 8)) {
-  $data = [
-  	'status' => 'not modified'
-  	];
-  require 'Views/MyRadio/datatojson.php'
+  $data = ['myury_errors' => 'Invalid Studio ID'];
+  require 'Views/MyRadio/datatojson.php';
 }
 
 $status = MyRadio_Selector::getStatusAtTime(time());
 
 if ($src == $status['studio']) {
-  $data = [
-  	'status' => 'not modified'
-  	];
+  $data = ['myury_errors' => 'Already Selected'];
   require 'Views/MyRadio/datatojson.php';
 }
-if (($src == 1) && (!$status['s1power'])) {
-  $data = [
-  	'status' => 'not modified'
-  	];
+if ((($src == 1) && (!$status['s1power'])) ||
+	(($src == 2) && (!$status['s2power'])) ||
+	(($src == 4) && (!$status['s4power']))) {
+  $data = ['myury_errors' => 'Source '.$src.' is not powered'];
   require 'Views/MyRadio/datatojson.php';
 }
-if (($src == 2) && (!$status['s2power'])) {
-  $data = [
-  	'status' => 'not modified'
-  	];
-  require 'Views/MyRadio/datatojson.php';
-}
-if (($src == 4) && (!$status['s4power'])) {
-  $data = [
-  	'status' => 'not modified'
-  	];
-  require 'Views/MyRadio/datatojson.php';
-}
+
 if ($status['locked'] != 0) {
-  $data = [
-  	'status' => 'locked'
-  	];
+  $data = ['myury_errors' => 'Selector Locked'];
   require 'Views/MyRadio/datatojson.php';
+}
+
+$response = MyRadio_Selector::setStudio($src);
+
+if (!empty($response)) {
+	$data = $response;
+	require 'Views/MyRadio/datatojson.php';
+}
+else {
+	header('HTTP/1.1 204 No Content');
 }
