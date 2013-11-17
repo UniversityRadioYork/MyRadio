a3b045e18e2f1471084b83815bf0a828c4c8cd78
Andy Durant <andy@ajdurant.co.uk>
2013-11-17 18:49:05 +0000

    trigger error

---
+++ src/Controllers/SIS/selector.set.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Selector setter for SIS
+ * 
+ * @author Andy Durant <aj@ury.org.uk>
+ * @version 20131117
+ * @package MyRadio_SIS
+ */
+
+$src = (isset($_REQUEST['src'])) ? (int) $_REQUEST['src'] : 0;
+
+if (($src <= 0) || ($src > 8)) {
+  $data = [
+  	'status' => 'not modified'
+  	];
+  require 'Views/MyRadio/datatojson.php'
+}
+
+$status = MyRadio_Selector::getStatusAtTime(time());
+
+if ($src == $status['studio']) {
+  $data = [
+  	'status' => 'not modified'
+  	];
+  require 'Views/MyRadio/datatojson.php';
+}
+if (($src == 1) && (!$status['s1power'])) {
+  $data = [
+  	'status' => 'not modified'
+  	];
+  require 'Views/MyRadio/datatojson.php';
+}
+if (($src == 2) && (!$status['s2power'])) {
+  $data = [
+  	'status' => 'not modified'
+  	];
+  require 'Views/MyRadio/datatojson.php';
+}
+if (($src == 4) && (!$status['s4power'])) {
+  $data = [
+  	'status' => 'not modified'
+  	];
+  require 'Views/MyRadio/datatojson.php';
+}
+if ($status['locked'] != 0) {
+  $data = [
+  	'status' => 'locked'
+  	];
+  require 'Views/MyRadio/datatojson.php';
+}
