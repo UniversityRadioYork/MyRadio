1ba7ce5eff8d8c3b562239fd1fc83ff8769fd425
Andy Durant <andy@ajdurant.co.uk>
2013-11-19 23:26:21 +0000

    SIS: selector stop dual response

--- src/Controllers/SIS/selector.set.php
+++ src/Controllers/SIS/selector.set.php
@@ -8,36 +8,34 @@
  */
 
 $src = (isset($_REQUEST['src'])) ? (int) $_REQUEST['src'] : 0;
+$status = MyRadio_Selector::getStatusAtTime(time());
 
 if (($src <= 0) || ($src > 8)) {
   $data = ['myury_errors' => 'Invalid Studio ID'];
   require 'Views/MyRadio/datatojson.php';
 }
-
-$status = MyRadio_Selector::getStatusAtTime(time());
-
-if ($src == $status['studio']) {
+elseif ($src == $status['studio']) {
   $data = ['myury_errors' => 'Already Selected'];
   require 'Views/MyRadio/datatojson.php';
 }
-if ((($src == 1) && (!$status['s1power'])) ||
+elseif ((($src == 1) && (!$status['s1power'])) ||
 	(($src == 2) && (!$status['s2power'])) ||
 	(($src == 4) && (!$status['s4power']))) {
   $data = ['myury_errors' => 'Source '.$src.' is not powered'];
   require 'Views/MyRadio/datatojson.php';
 }
-
-if ($status['locked'] != 0) {
+elseif ($status['locked'] != 0) {
   $data = ['myury_errors' => 'Selector Locked'];
   require 'Views/MyRadio/datatojson.php';
 }
-
-$response = MyRadio_Selector::setStudio($src);
-
-if (!empty($response)) {
-	$data = $response;
-	require 'Views/MyRadio/datatojson.php';
-}
 else {
-	header('HTTP/1.1 204 No Content');
+  $response = MyRadio_Selector::setStudio($src);
+
+  if (!empty($response)) {
+  	$data = $response;
+  	require 'Views/MyRadio/datatojson.php';
+  }
+  else {
+  	header('HTTP/1.1 204 No Content');
+  }
 }
