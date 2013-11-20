93cf94ab98fbe234d3d28cf752c086176abbd1a0
Andy Durant <andy@ajdurant.co.uk>
2013-11-20 20:18:28 +0000

    themeswitcher

---
+++ src/Models/SIS/plugins/50-themeswitcher.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Themeswitcher Plugin for SIS
+ * 
+ * @author Andy Durant <aj@ury.org.uk>
+ * @version 20131120
+ * @package MyRadio_SIS
+ */
+
+$moduleInfo = array(
+'name' => 'themeswitcher',
+'title' => 'Theme Switcher',
+'enabled' => true,
+'startOpen' => true,
+'help' => '',
+);
