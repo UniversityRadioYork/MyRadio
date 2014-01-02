13a5a25ad5a5175bc0a82769dfe29234a4df3b23
Lloyd Wallis <lpw@ury.org.uk>
2014-01-02 16:36:43 +0000

    Implemented traditional_auth for backwards compatibility with Shibbobleh clients

---
+++ src/Controllers/traditional_auth.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Provides basic login requirement functionality to other PHP web systems,
+ * backwards compatible with the old Shibbobleh system URY use to use
+ */
+
+//Load the basic MyRadio framework
+require_once __DIR__.'/cli_common.php';
+
+//Check the current authentication status of the user
+if ((!isset($_SESSION['memberid']) or $_SESSION['auth_use_locked'])
+        && (!defined('SHIBBOBLEH_ALLOW_READONLY') or SHIBBOBLEH_ALLOW_READONLY === false)) {
+  //Authentication is required.
+  header('HTTP/1.1 403 Forbidden');
+  header('Location: ' . CoreUtils::makeURL('MyRadio', 'login', ['next' => $_SERVER['REQUEST_URI']]));
+  exit;
+}
+
+//Check if the current app needs a timeslot selected
+if ((!isset($_SESSION['timeslotid']) or $_SESSION['timeslotid'] === null) && (defined('SHIBBOBLEH_REQUIRE_TIMESLOT') and SHIBBOBLEH_REQUIRE_TIMESLOT)) {
+  //Timeslot needs configuring
+  header('HTTP/1.1 403 Forbidden');
+  header('Location: ' . CoreUtils::makeURL('MyRadio', 'timeslot', ['next' => $_SERVER['REQUEST_URI']]));
+  exit;
+}
