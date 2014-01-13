330f7e4d3d28a9e2f2bbe209e376bfb6e8a4aeb5
Matt Windsor <matt.windsor@ury.org.uk>
2014-01-13 03:56:45 +0000

    Add initial add quote controller.

---
+++ src/Controllers/Quotes/addQuote.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Allows the addition of new quotes.
+ *
+ * @version 20140113
+ * @author  Matt Windsor <mattbw@ury.org.uk>
+ * @package MyRadio_Quotes
+ */
+
+MyRadio_JsonFormLoader::loadFromModule(
+    $module,
+    'addQuote',
+    'doAddQuote',
+    []
+)->render;
