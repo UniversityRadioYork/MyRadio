606c141e4ed583795db351e5e64e7e565e7c91ce
Matt Windsor <matt.windsor@ury.org.uk>
2014-01-13 15:45:02 +0000

    Also rename editChartType form.

--- src/Controllers/Charts/editChartType.php
+++ src/Controllers/Charts/editChartType.php
@@ -1,13 +1,13 @@
 <?php
 /**
  * Allows the editing of chart types.
- * @version 20130426
- * @author Matt Windsor <matt.windsor@ury.org.uk>
+ * @version 20140113
+ * @author  Matt Windsor <matt.windsor@ury.org.uk>
  * @package MyRadio_Charts
  */
 
 $form = MyRadio_JsonFormLoader::loadFromModule(
-  $module, 'charttypefrm', 'doEditChartType'
+  $module, 'editChartType', 'doEditChartType'
 );
 
 $chart_type = MyRadio_ChartType::getInstance($_REQUEST['chart_type_id']);
