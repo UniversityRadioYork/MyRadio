9fa022a11a6c0316e6c297871eef88797e31b2aa
Matt Windsor <matt.windsor@ury.org.uk>
2014-01-17 19:50:37 +0000

    Fix it 'til it works.

--- src/Classes/ServiceAPI/MyRadio_Podcast.php
+++ src/Classes/ServiceAPI/MyRadio_Podcast.php
@@ -359,11 +359,41 @@ class MyRadio_Podcast extends MyRadio_Metadata_Common {
   }
 
   /**
-   * Gets the current podcast cover for a podcast.
+   * Sets the current podcast cover for this podcast.
+   *
+   * @param string $url  The URL of the incoming podcast cover.
+   */
+  public function setCover($url) {
+    // TODO: Plumb this into the metadata system.
+    //       At time of writing, MyRadio's metadata system doesn't do images.
+    if (empty($url)) {
+      throw new MyRadioException('URL is blank.');
+    }
+
+    self::$db->query('
+      INSERT INTO
+        uryplayer.podcast_image_metadata(
+          metadata_key_id, podcast_id, memberid, approvedid,
+          metadata_value, effective_from, effective_to
+        )
+      VALUES
+        (10, $1, $2, $2, $3, NOW(), NULL),
+        (11, $1, $2, $2, $3, NOW(), NULL)
+      ',
+      [
+        $this->getID(),
+        MyRadio_User::getInstance()->getID(),
+        $url
+      ]
+    );
+  }
+
+  /**
+   * Gets the current podcast cover for this podcast.
    *
    * @return string  The URL of the current podcast cover.
    */
-  function getCover() {
+  public function getCover() {
     // TODO: Plumb this into the metadata system.
     //       At time of writing, MyRadio's metadata system doesn't do images.
     return self::$db->fetch_one('
