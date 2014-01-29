6bdeec567059d03f2d3bad8bb027319014249528
Sam Willcocks <samw@ury.org.uk>
2014-01-29 13:09:38 +0000

    Change to POST

--- src/Controllers/Profile/markPaid.php
+++ src/Controllers/Profile/markPaid.php
@@ -6,7 +6,7 @@
 * @version 20140129
 * @package MyRadio_Profile
 */
-$user = MyRadio_User::getInstance($_POST['memberid']);
+$user = MyRadio_User::getInstance($_REQUEST['memberid']);
 $user->setPayment(Config::$membership_fee);
 
 CoreUtils::backWithMessage('Payment data updated');
