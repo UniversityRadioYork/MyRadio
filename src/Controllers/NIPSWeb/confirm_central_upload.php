28d1e3c2ae856ffe0b93cd54d84fcf7f9f8c9781
Liam Fraser <liam@liamfraser.co.uk>
2014-02-02 14:38:43 +0000

    Add track position

--- src/Controllers/NIPSWeb/confirm_central_upload.php
+++ src/Controllers/NIPSWeb/confirm_central_upload.php
@@ -9,7 +9,8 @@
 $data = MyRadio_Track::identifyAndStoreTrack($_REQUEST['fileid'],
                                              $_REQUEST['title'],
                                              $_REQUEST['artist'],
-                                             $_REQUEST['album']);
+                                             $_REQUEST['album'],
+                                             $_REQUEST['position']);
 $data['fileid'] = $_REQUEST['fileid'];
 
 require 'Views/MyRadio/datatojson.php';
