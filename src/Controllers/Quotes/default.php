d7018d96fec29baf5d6218a170effaa7da79f3a9
Andy Durant <andy@ajdurant.co.uk>
2014-03-03 22:32:35 +0000

    QuotesFix - dedupe

--- src/Controllers/Quotes/default.php
+++ src/Controllers/Quotes/default.php
@@ -1,7 +1,6 @@
 <?php
 
 CoreUtils::getTemplateObject()->setTemplate('table.twig')
-    ->addVariable('tablescript', 'myury.datatable.default')
     ->addVariable('title', 'Quotes')
     ->addVariable('tabledata', ServiceAPI::setToDataSource(MyRadio_Quote::getAll()))
     ->addVariable('tablescript', 'myradio.quotes')
