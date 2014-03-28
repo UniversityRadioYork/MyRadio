b15cfeb0f6898f37778d6ab39724fcff43888096
Lord Aro <charlespigott@googlemail.com>
2014-03-28 15:30:00 +0000

    Make sure _SESSION is at least defined to default values for the API

---
+++ src/Classes/MyRadio/MyRadioNullSession.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+* Null session handler.
+*
+* @author Charles Pigott <lordaro@ury.org.uk>
+ */
+class MyRadioNullSession extends MyRadioSession
+{
+    public function __construct()
+    {
+        $this->db = NULL;
+    }
+
+    /**
+     * Clear up old session entries in the database
+     * This should be called automatically by PHP every one in a while
+     */
+    public function gc($lifetime)
+    {
+        return true;
+    }
+
+    /**
+     * Reads the session data from the database. If no data exists, creates an
+     * empty row
+     */
+    public function read($id)
+    {
+        return false;
+    }
+
+    /**
+     * Writes changes to the session data to the database
+     */
+    public function write($id, $data)
+    {
+        return empty($data);
+    }
+
+    /**
+     * Deletes the session entry from the database
+     */
+    public function destroy($id)
+    {
+        return !empty($id);
+    }
+}
