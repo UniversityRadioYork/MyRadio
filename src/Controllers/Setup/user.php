907b9e64b16f1c651e7ae06086a7f1dd8fab466e
Lloyd Wallis <flump5281@gmail.com>
2014-05-28 04:36:15 +0100

    Implemented more stages of the setup guide, enabling configuring initial access control data, preconfigured string values, and an administrative user account.

---
+++ src/Controllers/Setup/user.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Sets up the initial admin user for MYRadio
+ *
+ * @version 20140515
+ * @author Lloyd Wallis <lpw@ury.org.uk>
+ * @package MyRadio_Core
+ */
+
+//Attempt to Database
+require_once 'Classes/Database.php';
+require_once 'Interfaces/CacheProvider.php';
+require_once 'Classes/APCProvider.php';
+require_once 'Classes/MyRadioEmail.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	MyRadio_User::create(
+		$_REQUEST['first-name'],
+		$_REQUEST['last-name'],
+		null,
+		$_REQUEST['gender'],
+		null,
+		$_REQUEST['email'],
+		empty($_REQUEST['phone']) ? null : $_REQUEST['phone'],
+		true,
+		Config::$membership_fee,
+		$_REQUEST['password']
+	);
+	header('Location: ?c=save');
+} else {
+	CoreUtils::getTemplateObject()
+		->setTemplate('Setup/user.twig')
+		->addVariable('title', 'User')
+		->addVariable('db_error', isset($_GET['err']))
+		->render();
+}
