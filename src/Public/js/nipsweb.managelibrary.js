UniversityRadioYork/MyRadio

commit a48890e3e5b1955bb5cde8022e5de59c8046b28b

Undo accidentally merging in STU
This reverts commit bc25206907552b26e42dc7d7eb0139fce93884bb.

src/Public/js/nipsweb.managelibrary.js
@@ -79,14 +79,22 @@ $(document).ready(function () {
             console.log(file.name + ' (id#' + i + ') has uploaded in ' + time);
             $('#central-status').html('Uploaded ' + file.name);
 
-            var from_lastfm = true;
-
             if (response['status'] === 'FAIL') {
                 //An error occurred
                 $('#central-result').append('<div class="ui-state-error">' + file.name + ': ' + response['error'] + '</div>');
                 return;
-            } else if (response['status'] === 'NO_LASTFM_MATCH') {
-                from_lastfm = false;
+            }
+
+            var manual_track = false;
+            if (response.analysis.length === 0) {
+                var manual_div = document.getElementById('track-manual-entry');
+                if (manual_div !== null) {
+                    // If the div exists, then the user has permission to upload a track
+                    // manually, so display the div and set manual_track to true.
+                    manual_div.style.display = 'block';
+                    manual_track = true;
+                }
+                return;
             }
 
             // Track info.
@@ -98,34 +106,20 @@ $(document).ready(function () {
 
             // Build a list of tracks from the lastfm responses and store it in a drop
             // down list
-            var track_data = "";
-            if (from_lastfm) {
-                track_data = $('<select></select>')
+            var select = "";
+            if (!manual_track) {
+                select = $('<select></select>')
                     .attr('name', response.fileid).attr('id', 'centralupload-' + i);
                 $.each(response.analysis, function (key, value) {
-                    track_data.append('<option value="' + value.title + ':-:' + value.artist + '">' + value.title + ' by ' + value.artist + '</option>');
+                    select.append('<option value="' + value.title + ':-:' + value.artist + '">' + value.title + ' by ' + value.artist + '</option>');
                 });
-            } else {
-                track_data = $('<fieldset id="manualupload-' + i + '">' +
-                    '<legend>' + file.name + ' - Track not found. Please enter the details manually:</legend>' +
-                    '<div class="myradiofrmfield-container">' +
-                    '   <label for="manualupload-' + i + '-title">Title</label>' +
-                    '   <input type="text" class="myradiofrmfield required" name="manualupload-' + i + '-title" id="manualupload-' + i + '-title">' +
-                    '   <label for="manualupload-' + i + '-artist">Artist</label>' +
-                    '   <input type="text" class="myradiofrmfield required" name="manualupload-' + i + '-artist" id="manualupload-' + i + '-artist">' +
-                    '   <label for="manualupload-' + i + '-album">Album</label>' +
-                    '   <input type="text" class="myradiofrmfield required" name="manualupload-' + i + '-album" id="manualupload-' + i + '-album">' +
-                    '   <label for="manualupload-' + i + '-position">Track Position</label>' +
-                    '   <input type="text" class="myradiofrmfield required" name="manualupload-' + i + '-position" id="manualupload-' + i + '-position">' +
-                    '</div>' +
-                    '</fieldset>');
             }
 
             // The submit part
             var submit = $('<a href="javascript:">Save to Database</a>').click(function () {
                 console.log('Saving track to database');
 
-                if (from_lastfm) {
+                if (!manual_track) {
                     var select = $(this).parent().find('select').val();
                     track_fileid = $(this).parent().find('select').attr('name');
                     track_title = select.replace(/:-:.*$/, '');
@@ -134,13 +128,13 @@ $(document).ready(function () {
                     track_position = "FROM_LASTFM";
                 } else {
                     track_fileid = response.fileid;
-                    track_title = $('#manualupload-' + i + '-title').val();
-                    track_artist = $('#manualupload-' + i + '-artist').val();
-                    track_album = $('#manualupload-' + i + '-album').val();
-                    track_position = $('#manualupload-' + i + '-position').val();
+                    track_title = document.getElementById('track-manual-entry-title').val();
+                    track_artist = document.getElementById('track-manual-entry-artist').val();
+                    track_album = document.getElementById('track-manual-entry-album').val();
+                    track_position = document.getElementById('track-manual-entry-position').val();
                 }
 
-                $(this).hide().parent().append('<div id="confirminator-' + (track_fileid.replace(/\.mp3/, '')) + '">Saving (this may take a few minutes)...</div>');
+                $(this).hide().parent().append('<div id="confirminator-' + (fileid.replace(/\.mp3/, '')) + '">Saving (this may take a few minutes)...</div>');
                 $.ajax({
                     url: myury.makeURL('NIPSWeb', 'confirm_central_upload'),
                     data: {
@@ -163,11 +157,8 @@ $(document).ready(function () {
                 });
             });
 
-            var container = $('<div></div>');
-            if (from_lastfm) {
-                container.append('<label for="centralupload-' + i + '">' + file.name + '</label>');
-            }
-            container.append(track_data)
+            var container = $('<div></div>').append('<label for="centralupload-' + i + '">' + file.name + '</label>')
+                .append(select)
                 .append(submit);
             $('#central-result').append(container);
         }

src/Classes/ServiceAPI/MyRadio_Track.php
@@ -481,7 +481,7 @@ class MyRadio_Track extends ServiceAPI
         }
 
         $analysis = self::identifyUploadedTrack(Config::$audio_upload_tmp_dir . '/' . $filename);
-        if (isset($analysis['status']) && ($analysis['status'] === 'FAIL' || $analysis['status'] === 'NO_LASTFM_MATCH')) {
+        if (isset($analysis['status']) && $analysis['status'] === 'FAIL') {
             $analysis['fileid'] = $filename;
             return $analysis;
         } else {
@@ -508,21 +508,10 @@ class MyRadio_Track extends ServiceAPI
         //echo (empty($GLOBALS['syspath']) ? '' : $GLOBALS['syspath']).'lastfm-fpclient -json ' . $path;
 
         $lastfm = json_decode($response, true);
-        $lastfm = [];
 
         if (empty($lastfm)) {
-            if (CoreUtils::hasPermission(AUTH_UPLOADMUSICMANUAL)) {
-                return [
-                    'status' => 'NO_LASTFM_MATCH',
-                    'error' => 'Track not found in Last FM.'
-                ];
-            }
-            else {
-                return [
-                    'status' => 'FAIL',
-                    'error' => 'This track could not be identified. Please email the track to track.requests@ury.org.uk.'
-                ];
-            }
+            return ['status' => 'NO_LASTFM_MATCH',
+                    'error' => 'Track not found in Last FM.'];
         } else {
             if (isset($lastfm['tracks']['track']['mbid'])) {
                 //Only one match
@@ -549,14 +538,14 @@ class MyRadio_Track extends ServiceAPI
     public static function identifyAndStoreTrack($tmpid, $title, $artist, $album, $position)
     {
         $ainfo = null;
-        if ($album === "FROM_LASTFM") {
+        if ($album == "FROM_LASTFM") {
             // Get the album info if we're getting it from lastfm
             $ainfo = self::getAlbumDurationAndPositionFromLastfm($title, $artist);
         } else {
             // Use the album title the user has provided. Use an existing album
             // if we already have one of that title. If not, create one.
             $myradio_album = MyRadio_Album::findOrCreate($album, $artist);
-            $ainfo = array('duration' => null, 'position' => intval($position), 'album' => $myradio_album);
+            $ainfo = array('duration' => null, 'position' => int($position), 'album' => $myradio_album);
         }
 
         // Get the track duration from the file if it isn't already set

src/Controllers/NIPSWeb/manage_library.php
@@ -3,12 +3,15 @@
  * Main renderer for NIPSWeb
  *
  * @author Lloyd Wallis <lpw@ury.org.uk>
- * @author Andy Durant <aj@ury.org.uk>
- * @version 20140610
+ * @version 20130525
  * @package MyRadio_NIPSWeb
  */
+$template = 'NIPSWeb/manage_library.twig';
+if (CoreUtils::hasPermission(AUTH_UPLOADMUSICMANUAL)) {
+    $template = 'NIPSWeb/manage_library_manual.twig';
+}
 
-CoreUtils::getTemplateObject()->setTemplate('NIPSWeb/manage_library.twig')
+CoreUtils::getTemplateObject()->setTemplate($template)
     ->addVariable(
         'reslists',
         CoreUtils::dataSourceParser(
@@ -18,7 +21,4 @@ CoreUtils::getTemplateObject()->setTemplate('NIPSWeb/manage_library.twig')
                 'user' => NIPSWeb_ManagedUserPlaylist::getAllManagedUserPlaylistsFor(MyRadio_User::getInstance())
             ]
         )
-    )->addVariable(
-        'AUTH_UPLOADMUSICMANUAL',
-        true
     )->render();

src/Templates/NIPSWeb/manage_library_manual.twig
@@ -2,18 +2,29 @@
 <html>
   <head>
 {% include 'parts/base_head.twig' %}
-      <link rel="stylesheet" type="text/css" href="{{baseurl}}css/planner.css" />
+      <link rel="stylesheet" type="text/css" href="{{shiburl}}css/planner.css" />
       <style>html,body{background: none; box-shadow: none; width: 800px; min-width: 800px; height: auto;}</style>
     </head>
     <body>
   {% include 'NIPSWeb/res_selector.twig' %}<a href="#" onclick="res_type_sel_change_handler()">Update</a>
         <div id="central-container" class="res-container ui-widget-content ui-helper-hidden">
-          <p>You can upload track to the central database by dragging them into the box below. I'll try to analyse the file and if I can figure out what it is, then I'll upload it for you. If the track is of poor quality or too unknown to be recognised, then this tool will not allow tracks to be uploaded.</p>
+          <p>You can upload track to the central database by dragging them into the box below. I'll try to analyse the file and if I can figure out what it is, then I'll upload it for you. If the track is of poor quality then this tool will not allow tracks to be uploaded. If the track is not recognised, you will be asked to enter the track details manually.</p>
           <div id="central-dragdrop" class="ui-widget-header">Drag MP3 files here to upload<div id="lastfm-attr">
               <a href="http://www.last.fm/">Powered by <img src="http://cdn.last.fm/flatness/badges/lastfm_black_small.gif" /></a>
           </div></div>
           <div class="ui-state-highlight" id="central-status"></div>
           <div class="ui-widget-content" id="central-result"></div>
+ 
+          <div id="track-manual-entry" style="display: none;">
+              <p>Track not found. Please enter the details manually:</p>
+              <form>
+              Title: <input type="text" id="track-manual-entry-title"><br>
+              Artist: <input type="text" id="track-manual-entry-artist">
+              Album: <input type="text" id="track-manual-entry-album">
+              Track Position: <input type="text" id="track-manual-entry-position">
+              </form>
+          </div>
+
         </div>
         <div id="managed-container" class="res-container ui-widget-content ui-helper-hidden">Managed playlists are controlled using <a href="/itones/" target="_blank">Campus Playout Manager</a>. Entering them there also adds them to the wonderful Campus Jukebox</div>
         <div id="res-container" class="res-container ui-widget-content ui-helper-hidden">
@@ -24,6 +35,6 @@
         </div>
         <script type="text/javascript">window.memberid = parseInt({{memberid}});</script>
   {% include 'parts/base_foot.twig' %}
-      <script type="text/javascript" src="{{baseurl}}js/vendor/jquery.filedrop.js"></script>
+      <script type="text/javascript" src="{{baseurl}}js/jquery.filedrop.js"></script>
       <script type="text/javascript" src="{{baseurl}}js/nipsweb.managelibrary.js"></script>
       </body></html>

commit 28f0e574c300a7da8d8dd8014167e4ef4ed80461

Correct regression in a48890e caused by reverting bc25206, which changed the name of a variable.

src/Public/js/nipsweb.managelibrary.js
@@ -134,7 +134,7 @@ $(document).ready(function () {
                     track_position = document.getElementById('track-manual-entry-position').val();
                 }
 
-                $(this).hide().parent().append('<div id="confirminator-' + (fileid.replace(/\.mp3/, '')) + '">Saving (this may take a few minutes)...</div>');
+                $(this).hide().parent().append('<div id="confirminator-' + (track_fileid.replace(/\.mp3/, '')) + '">Saving (this may take a few minutes)...</div>');
                 $.ajax({
                     url: myury.makeURL('NIPSWeb', 'confirm_central_upload'),
                     data: {