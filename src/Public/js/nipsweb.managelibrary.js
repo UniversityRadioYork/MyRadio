0747947afe842254dc081c8d5037f4d9d5388cbf
Lloyd Wallis <flump5281@gmail.com>
2014-11-01 20:27:53 +0000

    Fixed aux library uploading

--- src/Public/js/nipsweb.managelibrary.js
+++ src/Public/js/nipsweb.managelibrary.js
@@ -2,6 +2,9 @@ var Library = function() {
     var auxid = null;
     var allowed_mp3 = ['audio/mpeg3', 'audio/x-mpeg-3', 'audio/mpeg', 'audio/x-mpeg',
             'audio/mp3', 'audio/x-mp3', 'audio/mpg', 'audio/mpg3', 'audio/mpegaudio'];
+    var allowed_all = ['audio/mpeg3', 'audio/x-mpeg-3', 'audio/mpeg', 'audio/x-mpeg',
+            'audio/mp3', 'audio/x-mp3', 'audio/mpg', 'audio/mpg3', 'audio/mpegaudio', 'audio/wav', 'audio/x-wav',
+            'audio/mp4a-latm', 'audio/mp4', 'audio/aac']
 
     //Converts bytes to human readable numbers
     var byteSize = function(size) {
@@ -42,10 +45,10 @@ var Library = function() {
                 message = 'Please don\'t upload too many files at once.';
                 break;
             case 'FileTooLarge':
-                message = file.name + ' is too big. Please upload files smaller than 100MB.';
+                message = file.name + ' is too big. Please upload files smaller than 15MB.';
                 break;
             case 'FileTypeNotAllowed':
-                message = 'That file is not a valid audio file.';
+                message = file.name + ' is not a valid audio file.';
                 break;
             default:
                 message = 'An unknown error occured: ' + err;
@@ -62,7 +65,7 @@ var Library = function() {
             error: filedrop_error_handler,
             allowedfiletypes: allowed_mp3,
             maxfiles: 20,
-            maxfilesize: 100,
+            maxfilesize: 15,
             queuefiles: 1,
             drop: function () {
                 $('#central-status').html('Reading file (0%)...');
@@ -185,7 +188,7 @@ var Library = function() {
                             } else {
                                 result.removeClass('alert-info')
                                     .addClass('alert-danger')
-                                    .html('<div class="glyphicon glyphicon-exclamation-sign">&nbsp;' + track_title + ': ' + data.error);
+                                    .html('<div class="glyphicon glyphicon-exclamation-sign"></div>&nbsp;' + track_title + ': ' + data.error);
                             }
                         }
                     });
@@ -197,11 +200,91 @@ var Library = function() {
                 $('#central-result').append(result);
             }
         });
-    }
+    };
+
+    /** Auxillary Database Handler **/
+    var auxDbInit = function () {
+        $('#res-dragdrop').filedrop({
+            url: myury.makeURL('NIPSWeb', 'upload_aux'),
+            paramname: 'audio',
+            error: filedrop_error_handler,
+            allowedfiletypes: allowed_all,
+            maxfiles: 20,
+            maxfilesize: 15,
+            queuefiles: 1,
+            drop: function () {
+                $('#res-status').html('Reading file (0%)...');
+            },
+            uploadStarted: function (i, file, total) {
+                $('#res-status').html('Uploading ' + file.name + '... (' + byteSize(file.size) + ')');
+            },
+            progressUpdated: function (i, file, progress) {
+                $('#res-status').html('Reading ' + file.name + ' (' + progress + '%)...');
+            },
+            uploadFinished: function (i, file, response, time) {
+                $('#res-status').html('Uploaded ' + file.name);
+
+                var result = $('<div class="alert"></div>');
+                if (response['status'] == 'FAIL') {
+                    //An error occurred
+                    result.addClass('alert-danger').append('<span class="error">' + response['error'] + '</span>');
+                } else {
+                    result.addClass('alert-info').append('<div id="resupload-' + i + '">' +
+                        file.name + ': <input type="text" class="title" name="' +
+                        response.fileid + '" id="resuploadname-' + i +
+                        '" placeholder="Enter a helpful name..." /></div>');
+
+                    if (window.auxid.match(/^aux-\d+$/)) {
+                        //This is a central one - it can have an expiry
+                        result.append(
+                            $('<input type="text" placeholder="Expiry date" />').addClass('date').attr('id', 'resuploaddate-' + i).datepicker({
+                                dateFormat: 'dd/mm/yy'
+                            }))
+                            .append('<em>Leave blank to never expire</em>&nbsp;&nbsp;');
+                    }
+                    result.append('<div id="confirminator-' + (response.fileid.replace(/\.mp3/, '')) + '"></div>');
+                    result.append($('<a href="javascript:">Save</a>').click(function () {
+                        var title = result.find('input.title').val();
+                        var expire = result.find('input.date').val() || null;
+                        var fileid = result.find('input.title').attr('name');
+
+                        result.html('Adding <em>' + title + '</em> to library');
+                        $.ajax({
+                            url: myury.makeURL('NIPSWeb', 'confirm_aux_upload'),
+                            data: {
+                                auxid: window.auxid,
+                                fileid: fileid,
+                                title: title,
+                                expires: expire
+                            },
+                            dataType: 'json',
+                            type: 'get',
+                            success: function (data) {
+                                data.fileid = data.fileid.replace(/\.mp3/, '');
+                                if (data.status == 'OK') {
+                                    result.removeClass('alert-info')
+                                            .addClass('alert-success')
+                                            .html('<div class="glyphicon glyphicon-ok"></div><em>' + title + '</em> added to library');
+                                } else {
+                                    result.removeClass('alert-info')
+                                            .addClass('alert-danger')
+                                            .html('<div class="glyphicon glyphicon-exclamation-sign"></div><em>' + title + '</em> could not be added to library<br>'
+                                                + data.error);
+                                }
+                            }
+                        });
+                    }));
+
+                    $('#res-result').append(result);
+                }
+            }
+        });
+    };
 
     var initialise = function() {
         $('#res-type-sel').on('click', res_type_sel_change_handler);
         centralDbInit();
+        auxDbInit();
         $('#central-status, #res-status').html('Ready');
     };
 
@@ -216,99 +299,4 @@ var Library = function() {
 $(document).ready(function () {
     library = Library();
     library.initialise();
-
-
-    /** Auxillary Database Handler **/
-    $('#res-dragdrop').filedrop({
-        url: myury.makeURL('NIPSWeb', 'upload_aux'),
-        paramname: 'audio',
-        error: function (err, file) {
-            switch (err) {
-            case 'BrowserNotSupported':
-                $('body').html('<div class="ui-state-error"><span class="ui-icon ui-icon-alert"></span>Something went wrong - are you sure you are using Google Chrome or Mozilla Firefox? We\'ve had reports of issues with Firefox on Linux too...</div>');
-                break;
-            case 'TooManyFiles':
-                $('body').prepend('<div class="ui-state-error"><span class="ui-icon ui-icon-alert"></span>Please don\'t upload too many files at once</div>');
-                break;
-            case 'FileTooLarge':
-                $('body').prepend('<div class="ui-state-error"><span class="ui-icon ui-icon-alert"></span>That file (' + file.name + ') is too big. Please upload files smaller than 100MB</div>');
-                break;
-            case 'FileTypeNotAllowed':
-                $('body').prepend('<div class="ui-state-error"><span class="ui-icon ui-icon-alert"></span>That file is not a valid audio file</div>');
-                break;
-            case 'NoFileDropped':
-                $('body').prepend('<div class="ui-state-error"><span class="ui-icon ui-icon-alert"></span>Hmm, I didn\'t see a file get dropped... please make sure you are using Google Chrome or Mozilla Firefox for best results.</div>');
-                break;
-            case 'NoFilesVar':
-                $('body').prepend('<div class="ui-state-error"><span class="ui-icon ui-icon-alert"></span>Odd, my files have gone missing... please make sure you are using Google Chrome or Mozilla Firefox for best results.</div>');
-                break;
-            default:
-                $('body').html('<div class="ui-state-error"><span class="ui-icon ui-icon-alert"></span>An unknown error occured: ' + err + '</div>');
-            }
-        },
-        allowedfiletypes: ['audio/mpeg3', 'audio/x-mpeg-3', 'audio/mpeg', 'audio/x-mpeg',
-            'audio/mp3', 'audio/x-mp3', 'audio/mpg', 'audio/mpg3', 'audio/mpegaudio', 'audio/wav', 'audio/x-wav',
-            'audio/mp4a-latm', 'audio/mp4', 'audio/aac'
-        ],
-        maxfiles: 20,
-        maxfilesize: 100,
-        queuefiles: 1,
-        drop: function () {
-            console.log('Drop detected (auxdb).');
-            $('#res-status').html('Reading file (0%)...');
-        },
-        uploadStarted: function (i, file, total) {
-            console.log('Upload started (auxdb).');
-            $('#res-status').html('Uploading ' + file.name + '... (' + byteSize(file.size) + ')');
-        },
-        progressUpdated: function (i, file, progress) {
-            $('#res-status').html('Reading ' + file.name + ' (' + progress + '%)...');
-        },
-        uploadFinished: function (i, file, response, time) {
-            console.log(file.name + ' (id#' + i + ') has uploaded in ' + time);
-            $('#res-status').html('Uploaded ' + file.name);
-
-            if (response['status'] == 'FAIL') {
-                //An error occurred
-                $('#res-result').append('<div class="ui-state-error">' + file.name + ': ' + response['error'] + '</div>');
-                return;
-            } else {
-                $('#res-result').append('<div id="resupload-' + i + '">' + file.name + ': <input type="text" class="title" name="' + response.fileid + '" id="resuploadname-' + i + '" placeholder="Enter a helpful name..." /></div>');
-                if (window.auxid.match(/^aux-\d+$/)) {
-                    //This is a central one - it can have an expiry
-                    $('#resupload-' + i).append(
-                        $('<input type="text" placeholder="Expiry date" />').addClass('date').attr('id', 'resuploaddate-' + i).datepicker({
-                            dateFormat: 'dd/mm/yy'
-                        }))
-                        .append('<em>Leave blank to never expire</em>&nbsp;&nbsp;');
-                }
-                $('#res-result').append('<div id="confirminator-' + (response.fileid.replace(/\.mp3/, '')) + '"></div>');
-                $('#resupload-' + i).append($('<a href="javascript:">Save</a>').click(function () {
-                    var title = $(this).parent().find('input.title').val();
-                    var expire = $(this).parent().find('input.date').val() || null;
-                    var fileid = $(this).parent().find('input.title').attr('name');
-                    $(this).parent().remove();
-                    $.ajax({
-                        url: myury.makeURL('NIPSWeb', 'confirm_aux_upload'),
-                        data: {
-                            auxid: window.auxid,
-                            fileid: fileid,
-                            title: title,
-                            expires: expire
-                        },
-                        dataType: 'json',
-                        type: 'get',
-                        success: function (data) {
-                            data.fileid = data.fileid.replace(/\.mp3/, '');
-                            if (data.status == 'OK') {
-                                $('#confirminator-' + data.fileid).html('<span class="ui-icon ui-icon-circle-check" style="float:left"></span>' + data.title + ': Upload Successful');
-                            } else {
-                                $('#confirminator-' + data.fileid).html('<span class="ui-icon ui-icon-alert" style="float:left"></span>' + data.title + ': ' + data.error);
-                            }
-                        }
-                    });
-                }));
-            }
-        }
-    });
 });
