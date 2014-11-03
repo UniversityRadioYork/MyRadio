9866f609401cb4cdc8a3dafd98a37b1cbbb1870d
Lloyd Wallis <flump5281@gmail.com>
2014-11-03 22:48:35 +0000

    I swear I added these

---
+++ src/Models/SIS/modules/selector.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Selector Plugin for SIS
+ *
+ * @package MyRadio_SIS
+ */
+
+$moduleInfo = [
+    'pollfunc' => '\MyRadio\SIS\SIS_Remote::querySelector',
+    'help' => 'To the left, you can see a Studio Selector button. '
+        .'This contains a digital version of the magic buttons you can see in each studio. '
+        .'Don\'t worry - most members can\'t see this so they won\'t go messing with your show from the comfort of '
+        .'their own homes. You must have special priveleges if you can see this! Please use it carefully - '
+        .'it includes a fourth button, Outside Broadcast, which usually plays placeholder noises, '
+        .'which are a terrible substitute for a radio show.',
+    'required_permission' => AUTH_MODIFYSELECTOR
+];
+
+  /**
+   * @todo: check if the OB mount is available
+   * @todo: $selectorStatusFile - use MyRadio_Selector
+   */
---
+++ src/Public/js/sis.selector.js
@@ -0,0 +1,110 @@
+/* Selector */
+var Selector = function() {
+    var studios = [
+            'Studio 1',
+            'Studio 2',
+            'Jukebox',
+            'Outside Broadcast'
+        ],
+        buttons,
+        onAir = document.createElement('span'),
+        lastTime = 0,
+        currentStudio,
+        locked = true,
+        selectStudio = function() {
+            var studio = this.getAttribute('studio');
+            if (studio == currentStudio) {
+                return;
+            }
+            if (this.getAttribute('on') == false) {
+                return;
+            }
+            
+            if (locked) {
+                myury.createDialog('Error', 'Could not change studio.<br>Studio selector is currently locked out.');
+                return;
+            }
+
+            $.get(myury.makeURL('SIS', 'selector.set'), {src: studio}, function(data) {
+                if (data['error'] == 'locked') {
+                    myury.createDialog('Selector Error', 'Could not change studio; studio selector is currently locked out.');
+                    return;
+                }
+                if (data['error']) {
+                    myury.createDialog('Selector Error', data['error']);
+                    return;
+                }
+                update(data);
+            });
+        },
+        update = function(data) {
+            var liveStatus, s;
+            lastTime = parseInt(data['lastmod']);
+            this.registerParam('selector-lasttime', lastTime);
+
+            if (!data['s1power']) {
+                buttons[0].setAttribute('title', studios[0] + ' Powered Off');
+                buttons[0].setAttribute('class', 'selbtn poweredoff');
+                buttons[0].setAttribute('on', 'false');
+            } else {
+                liveStatus = (data['studio'] == 1) ? 's1on' : 's1off';
+                buttons[0].setAttribute('title', studios[0]);
+                buttons[0].setAttribute('class', 'selbtn poweredon ' + liveStatus);
+                buttons[0].setAttribute('on', 'true');
+            }
+
+            if (!data['s2power']) {
+                buttons[1].setAttribute('title', studios[1] + ' Powered Off');
+                buttons[1].setAttribute('class', 'selbtn poweredoff');
+                buttons[1].setAttribute('on', 'false');
+            } else {
+                liveStatus = (data['studio'] == 2) ? 's2on' : 's2off';
+                buttons[1].setAttribute('title', studios[1]);
+                buttons[1].setAttribute('class', 'selbtn poweredon ' + liveStatus);
+                buttons[1].setAttribute('on', 'true');
+            }
+
+            liveStatus = (data['studio'] == 3) ? 's3on' : 's3off';
+            buttons[2].setAttribute('class', 'selbtn poweredon ' + liveStatus);
+
+            s = studios[data['studio'] - 1] + ' On Air';
+            currentStudio = data['studio'];
+            if (data['lock'] != '0') {
+              s = s + '<small> &mdash; Locked</small>';
+              locked = true;
+            }
+            onAir.innerHTML = s;
+        };
+
+	return {
+        name: 'Studio Selector',
+        type: 'plugin',
+        initialise: function() {
+            var table = document.createElement('table'),
+                row = document.createElement('tr'),
+                button;
+            table.setAttribute('id', 'selector-buttons');
+            table.appendChild(row);
+
+            for (i in studios) {
+                i = parseInt(i);
+                button = document.createElement('td');
+                button.setAttribute('class', 'selbtn s' + (i+1) + 'off');
+                button.setAttribute('id', 's' + i);
+                button.setAttribute('title', studios[i]);
+                button.setAttribute('studio', (i + 1));
+                button.setAttribute('on', 'true');
+                button.addEventListener('click', selectStudio);
+                row.appendChild(button);
+            }
+            
+            onAir.innerHTML = 'Selector unavailable &mdash; Locked';
+            this.appendChild(table);
+            this.appendChild(onAir);
+            this.registerParam('selector-lasttime', lastTime);
+        },
+        update: update
+    }
+}
+
+sis.registerModule('selector', new Selector());
