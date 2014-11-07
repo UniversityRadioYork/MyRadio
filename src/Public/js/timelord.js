21c99214f4b016282e4475a3f08243fe67217031
Andy Durant <andy@ajdurant.co.uk>
2014-11-07 15:51:55 +0000

    edit jingle text - fix #217

--- src/Public/js/timelord.js
+++ src/Public/js/timelord.js
@@ -235,8 +235,8 @@ window.timelord = {
                             (45 - date.getSeconds()) + '...</span>');
                     $('#next-show').show();
                 } else if (date.getSeconds() <= 52) {
-                    timelord.setCurrentShow('<span class="news">Jingle in ' +
-                            (52 - date.getSeconds()) + '...</span>');
+                    timelord.setCurrentShow('<span class="news">' +
+                            (52 - date.getSeconds()) + 's to talk...</span>');
                 } else {
                     timelord.setCurrentShow('<span class="news">'+mConfig.short_name+' News</span>');
                 }
