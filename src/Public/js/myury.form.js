afe81f5ef8685e2219186874854130e55211ce05
Lloyd Wallis <flump5281@gmail.com>
2014-12-27 23:06:57 +0000

    Nope, I'm just a muppet

--- src/Public/js/myury.form.js
+++ src/Public/js/myury.form.js
@@ -3,6 +3,25 @@
  * This file initialises jQuery validation, autocompletes and other resources
  * needed for a MyRadio Form
  */
+jQuery.validator.addMethod("dateITA", function(value, element) {
+    var check = false;
+    var re = /^\d{1,2}\/\d{1,2}\/\d{4}$/;
+    if (re.test(value)) {
+        var adata = value.split('/');
+        var gg = parseInt(adata[0],10);
+        var mm = parseInt(adata[1],10);
+        var aaaa = parseInt(adata[2],10);
+        var xdata = new Date(aaaa,mm-1,gg);
+        if ((xdata.getFullYear() === aaaa) && (xdata.getMonth() === mm - 1) && (xdata.getDate() === gg)) {
+            check = true;
+        } else {
+            check = false;
+        }
+    } else {
+        check = false;
+    }
+    return this.optional(element) || check;
+}, "Please enter a valid date.");
 window.MyRadioForm = {
   gCheckedValue: null,
   setUpMemberFields: function() {
