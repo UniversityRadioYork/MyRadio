7fbdbdf4f06baaf590feeb215543ff2455649252
Lloyd Wallis <flump5281@gmail.com>
2015-01-07 22:17:09 +0000

    Improve explicit checker

--- src/Classes/Daemons/MyRadio_ExplicitDaemon.php
+++ src/Classes/Daemons/MyRadio_ExplicitDaemon.php
@@ -8,6 +8,7 @@
 namespace MyRadio\Daemons;
 
 use \MyRadio\Config;
+use \MyRadio\Database;
 use \MyRadio\ServiceAPI\MyRadio_Track;
 
 /**
@@ -20,6 +21,7 @@ use \MyRadio\ServiceAPI\MyRadio_Track;
 class MyRadio_ExplicitDaemon extends \MyRadio\MyRadio\MyRadio_Daemon
 {
     private static $digitised_only = true;
+
     /**
      * If this method returns true, the Daemon host should run this Daemon. If it returns false, it must not.
      * It is currently enabled because we have a lot of labels that needed filling in for Tracklisting.
@@ -32,11 +34,16 @@ class MyRadio_ExplicitDaemon extends \MyRadio\MyRadio\MyRadio_Daemon
 
     public static function run()
     {
+        $db = Database::getInstance();
+
         $tracks = MyRadio_Track::findByOptions(
-            ['clean' => 'u',
-            'limit' => 25,
-            'random' => true,
-            'digitised' => self::$digitised_only]
+            [
+                'clean' => 'u',
+                'limit' => 25,
+                'random' => true,
+                'digitised' => self::$digitised_only,
+                'custom' => 'trackid NOT IN (SELECT trackid FROM music.explicit_checked)'
+            ]
         );
         
         if (empty($tracks)) {
@@ -44,14 +51,16 @@ class MyRadio_ExplicitDaemon extends \MyRadio\MyRadio\MyRadio_Daemon
         }
 
         foreach ($tracks as $track) {
-            $q = str_replace(' ', '+', trim($track->getTitle() . ' ' . $track->getArtist()));
+            $q = trim($track->getTitle() . ' ' . $track->getArtist());
             $data = json_decode(
                 file_get_contents(
                     'http://itunes.apple.com/search?term='
-                    . $q . '&entity=song&limit=5'
+                    . urlencode($q) . '&entity=song&limit=5'
                 ),
                 true
             );
+
+            dlog('Checking ' . $q . ' (' . $data['resultCount'] . ' matches)', 4);
 
             for ($i = 0; $i < $data['resultCount']; $i++) {
                 /**
@@ -64,15 +73,17 @@ class MyRadio_ExplicitDaemon extends \MyRadio\MyRadio\MyRadio_Daemon
                     $clean = $data['results'][$i]['trackExplicitness'] == 'explicit'
                             ? 'n' : 'y';
                     $track->setClean($clean);
-
-                    dlog(
+                    
+                    dlog('Setting Explicicity of ' . 
                         $track->getTitle() . ' (' . $track->getAlbum()->getID()
-                        . '/' . $track->getID() . ') is ' . $clean,
+                        . '/' . $track->getID() . ') as ' . $clean,
                         2
                     );
                     break;
                 }
             }
+
+            $db->query('INSERT INTO music.explicit_checked VALUES ($1)', [$track->getID()]);
         }
     }
 }
