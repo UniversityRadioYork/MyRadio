b00d9ae7c39ab966d07874a03fa2a14dba3772e9
Lloyd Wallis <flump5281@gmail.com>
2015-01-09 23:31:25 +0000

    Add missing event listener for obit plugin

--- src/Public/js/sis.obit.js
+++ src/Public/js/sis.obit.js
@@ -3,6 +3,9 @@ var Obit = function() {
     var button = document.createElement('button');
     button.className = 'btn btn-danger';
     button.innerHTML = 'Start Obituary Procedure';
+    button.addEventListener('click', function() {
+        window.open(myury.makeURL('Scheduler', 'stop'), 'Stop Broadcast');
+    });
 
     return {
         name: 'Stop Broadcast',
