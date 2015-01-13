c27c20b7f30e917ee75c74e0fc210bce87d137ca
Sam Willcocks <sam@wlcx.cc>
2015-01-13 23:01:46 +0000

    How do i php

--- src/Controllers/Scheduler/shows.php
+++ src/Controllers/Scheduler/shows.php
@@ -12,7 +12,7 @@ use \MyRadio\ServiceAPI\ServiceAPI;
 use \MyRadio\ServiceAPI\MyRadio_Show;
 
 // Get public shows (type 1) and check if all=true, in which case all shows, not just this term's (false)
-$shows = MyRadio_Show::getAllShows(1, !(isset($_GET["all"] && $_GET("all") == 'true'));
+$shows = MyRadio_Show::getAllShows(1, !( isset($_GET["all"]) && ($_GET["all"] == 'true')));
 CoreUtils::getTemplateObject()->setTemplate('table.twig')
     ->addVariable('title', 'All Shows')
     ->addVariable('tabledata', ServiceAPI::setToDataSource($shows))
