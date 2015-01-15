377ef326d2c53f10cd42d178e7b56be40fc288c1
Sam Willcocks <sam@wlcx.cc>
2015-01-15 04:26:51 +0000

    Change title appropriately

--- src/Controllers/Scheduler/shows.php
+++ src/Controllers/Scheduler/shows.php
@@ -15,7 +15,7 @@ $all = (isset($_REQUEST["all"]) && $_REQUEST["all"] === 'true');
 // Get public shows (type 1) and get all shows (!$all === false) or just this term's (true)
 $shows = MyRadio_Show::getAllShows(1, !$all);
 CoreUtils::getTemplateObject()->setTemplate('table.twig')
-    ->addVariable('title', 'All Shows')
+    ->addVariable('title', $all ? 'All Shows': 'This Term\'s Shows')
     ->addVariable('tabledata', ServiceAPI::setToDataSource($shows))
     ->addVariable('tablescript', 'myury.scheduler.showlist')
     ->render();
