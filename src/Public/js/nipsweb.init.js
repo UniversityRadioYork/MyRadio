850fc4baa1141e2b886d83063e4ce31a591f8f99
Lloyd Wallis <lpw@ury.org.uk>
2015-02-21 21:01:43 +0000

    Replace tabs with spaces

--- src/Public/js/nipsweb.init.js
+++ src/Public/js/nipsweb.init.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-	planner = NIPSWeb(false);
-	planner.initialiseUI();
-	planner.initialisePlayer('0');
-	planner.initialisePlayer('1');
-	planner.initialisePlayer('2');
-	planner.initialisePlayer('3');
-	$('#notice').hide();
+    planner = NIPSWeb(false);
+    planner.initialiseUI();
+    planner.initialisePlayer('0');
+    planner.initialisePlayer('1');
+    planner.initialisePlayer('2');
+    planner.initialisePlayer('3');
+    $('#notice').hide();
 });
