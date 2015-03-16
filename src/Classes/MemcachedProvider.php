d73eee7770c3cf1b1479b9ce2739ef9f6328f566
Lloyd Wallis <lpw@ury.org.uk>
2015-03-16 22:20:29 +0000

    Update comment

--- src/Classes/MemcachedProvider.php
+++ src/Classes/MemcachedProvider.php
@@ -11,7 +11,7 @@ use \Memcached;
 /**
  * MemcachedProvider provides in-memory caching for PHP resources to increase page load times
  *
- * MemcachedProvider was the first CacheProvider implementation in MyRadio. It enables Models to send cache commands to it
+ * MemcachedProvider was the second CacheProvider implementation in MyRadio. It enables Models to send cache commands to it
  * which are then stored using Memcached automatically. In order for this class to work correctly, the server
  * needs the Memcached PHP plugin installed on the server. It will throw an Error and disable itself if it is not.
  *
