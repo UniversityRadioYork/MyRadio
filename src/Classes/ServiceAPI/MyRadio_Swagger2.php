2c300a039442892c0d784154193481ecaaaf1a06
Andy Durant <andy@ajdurant.co.uk>
2015-10-17 00:07:19 +0100

    POST things

--- src/Classes/ServiceAPI/MyRadio_Swagger2.php
+++ src/Classes/ServiceAPI/MyRadio_Swagger2.php
@@ -399,7 +399,13 @@ class MyRadio_Swagger2 extends MyRadio_Swagger
             } elseif ($name === 'create') {
                 $op = 'post';
                 $public_name = '';
+            } elseif ($name === 'testcredentials') {
+                $op = 'post';
+                $public_name = '/' . strtolower($name);
             } elseif (CoreUtils::startsWith($name, 'create')) {
+                $op = 'post';
+                $public_name = '/' . strtolower($name);
+            } elseif (CoreUtils::startsWith($name, 'add')) {
                 $op = 'post';
                 $public_name = '/' . strtolower($name);
             } elseif ($method->isStatic()) {
