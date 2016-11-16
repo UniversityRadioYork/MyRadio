b749a9855b77e26f8cfd7aa1b8080adc66303eeb
Samuel Hand <samuel.hand@openmailbox.org>
2016-11-16 21:25:45 +0000

    Make some things static for the quotes api

--- src/Classes/ServiceAPI/MyRadio_Quote.php
+++ src/Classes/ServiceAPI/MyRadio_Quote.php
@@ -162,7 +162,7 @@ class MyRadio_Quote extends ServiceAPI
      *
      * @return array An array of all active quotes.
      */
-    public function getAll()
+    public static function getAll()
     {
         $quote_ids = self::$db->fetchColumn(self::GET_ALL_SQL, []);
 
@@ -209,7 +209,7 @@ class MyRadio_Quote extends ServiceAPI
      *
      * @return nothing.
      */
-    public function create($data)
+    public static function create($data)
     {
         self::$db->query(
             self::INSERT_SQL,
