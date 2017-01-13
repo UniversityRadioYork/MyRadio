31f8d7d0b4fce425a3854c30cf04c36e4b654be3
Matthew Stratford <matthew@mstratford.net>
2017-01-13 17:02:06 +0000

    Even put the code in the right order too

--- src/Controllers/Webcam/archive.php
+++ src/Controllers/Webcam/archive.php
@@ -3,19 +3,18 @@
  * Controller for viewing webcam archives.
  */
 use \MyRadio\MyRadio\CoreUtils;
-use \MyRadio\ServiceAPI\MyRadio_Webcam;
+//use \MyRadio\ServiceAPI\MyRadio_Webcam;
 
-$streams = MyRadio_Webcam::getStreams();
+//$streams = MyRadio_Webcam::getStreams();
 //Skip "Live"
 /*
  * @todo This is quite a nasty way of doing it. Is there a better one?
  */
- CoreUtils::getTemplateObject()->setTemplate('MyRadio/text.twig')
-        ->addVariable('title', 'Webcams Archive')
-        ->addInfo('Still coming soon to a MyRadio near you...')
-        ->render();
 //array_shift($streams);
 
 //$times = MyRadio_Webcam::getArchiveTimeRange();
 
-
+CoreUtils::getTemplateObject()->setTemplate('MyRadio/text.twig')
+        ->addVariable('title', 'Webcams Archive')
+        ->addInfo('Still coming soon to a MyRadio near you...')
+        ->render();
