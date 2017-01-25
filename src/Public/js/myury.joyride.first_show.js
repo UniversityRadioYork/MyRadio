6164bd69014f78a01ce403e01b5f9f667366a7ba
Charles Pigott <charlespigott@googlemail.com>
2017-01-25 16:15:31 +0000

    Fix joyride actions

--- src/Public/js/myury.joyride.first_show.js
+++ src/Public/js/myury.joyride.first_show.js
@@ -5,7 +5,7 @@ $("#myury-joyride").joyride({
   postRideCallback: function (index) {
     if (index === $("#myury-joyride li").length-1) {
       //This was the last element of the page. Unless it's the last page, don't send the kill signal
-      if ("{{action}}" !== "listSessions") {
+      if (window.myradio.action !== "listSessions") {
         return;
       }
     }
