0d5783987cdebf68b9205ccac28e6add5789dbe3
Matthew Stratford <hi@mstratford.net>
2017-03-15 00:52:44 +0000

    Lastmod now gets up-to-date time

--- src/Classes/ServiceAPI/MyRadio_Selector.php
+++ src/Classes/ServiceAPI/MyRadio_Selector.php
@@ -195,10 +195,11 @@ class MyRadio_Selector
         if ($response === 'FLK') {
             throw new MyRadioException('Selector Locked');
         } elseif ($response === 'ACK') {
-            // DB may not have updaded from the physical selector, so force it.
+            // DB may not have updated from the physical selector, so force it.
             $statusUpdated = getStatusAtTime();
             $statusUpdated['selectedfrom'] = 1;
             $statusUpdated['studio'] = $studio;
+            $statusUpdated['lastmod'] = time();
             return $statusUpdated;
         }
     }
