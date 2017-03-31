7bc5dc10dae0c8548f801f9cba567043bb59eda7
Matthew Stratford <matthew@mstratford.net>
2017-03-31 16:29:04 +0100

    Redirect to next url instead (if available)

--- src/Controllers/MyRadio/login.php
+++ src/Controllers/MyRadio/login.php
@@ -16,7 +16,8 @@ use \MyRadio\MyRadio\MyRadioFormField;
  */
 
 if (isset($_SESSION['memberid'])) {
-    URLUtils::redirectWithMessage("/", "", "It seems you were trying to login while still being logged in, if that is not what happened, please contact the Computing Team.");
+    isset($_GET["next"]) ? URLUtils::redirectURI($_GET["next"]) : URLUtils::redirect(Config::$default_module);
+
 } else {
     $form = (
         new MyRadioForm(
