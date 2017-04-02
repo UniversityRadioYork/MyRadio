45eb08f1587d97890e9b8cfe6d3fb7976f87296d
Lloyd Wallis <lloyd.wallis@bbc.co.uk>
2017-04-02 01:24:50 +0100

    Update training map generator to generate one map for each status, and update the display page to enable selection.

--- src/Controllers/Stats/trainingMap.php
+++ src/Controllers/Stats/trainingMap.php
@@ -1,11 +1,27 @@
 <?php
 /**
- * Trainin Map.
+ * Training Map.
  */
 use \MyRadio\MyRadio\CoreUtils;
+use \MyRadio\ServiceAPI\MyRadio_TrainingStatus;
 
-CoreUtils::getTemplateObject()->setTemplate('MyRadio/fullimage.twig')
+function status_map($status)
+{
+  return [
+    'value' => $status->getID(),
+    'text' => $status->getTitle()
+  ];
+}
+
+$caption = 'Please select a Training Status above.';
+if (isset($_GET['id'])) {
+  $title = MyRadio_TrainingStatus::getInstance($_GET['id'])->getTitle();
+  $caption = 'This is a map of who trained who for the ' . $title . ' Training Status.';
+}
+
+CoreUtils::getTemplateObject()->setTemplate('MyRadio/trainingMap.twig')
     ->addVariable('title', 'Member Training Graph')
-    ->addVariable('caption', 'This screen, updated hourly, provides a complete map of who has trained who. Ever.')
-    ->addVariable('image', '/img/stats_training.svg')
+    ->addVariable('maps', array_map('status_map', MyRadio_TrainingStatus::getAll()))
+    ->addVariable('caption', $caption)
+    ->addVariable('image', 'img/stats_training_' . $_GET['id'] . '.svg')
     ->render();
