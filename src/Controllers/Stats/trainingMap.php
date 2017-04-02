ff163eee85183b4d134846cc70556335875900d8
Lloyd Wallis <lloyd.wallis@bbc.co.uk>
2017-04-02 01:48:24 +0100

    Tidy up

--- src/Controllers/Stats/trainingMap.php
+++ src/Controllers/Stats/trainingMap.php
@@ -14,14 +14,16 @@ function status_map($status)
 }
 
 $caption = 'Please select a Training Status above.';
+$img = '';
 if (isset($_GET['id'])) {
   $title = MyRadio_TrainingStatus::getInstance($_GET['id'])->getTitle();
   $caption = 'This is a map of who trained who for the ' . $title . ' Training Status.';
+  $img = 'img/stats_training_' . $_GET['id'] . '.svg';
 }
 
 CoreUtils::getTemplateObject()->setTemplate('MyRadio/trainingMap.twig')
     ->addVariable('title', 'Member Training Graph')
     ->addVariable('maps', array_map('status_map', MyRadio_TrainingStatus::getAll()))
     ->addVariable('caption', $caption)
-    ->addVariable('image', 'img/stats_training_' . $_GET['id'] . '.svg')
+    ->addVariable('image', $img)
     ->render();
