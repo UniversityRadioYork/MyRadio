b9191089789aef0f0b63cf8c9e54aa64b9e529f5
Matthew Stratford <matthew@mstratford.net>
2017-04-13 18:21:25 +0100

    Removed unused readonly mode & client tokens.

--- src/Controllers/NIPSWeb/default.php
+++ src/Controllers/NIPSWeb/default.php
@@ -14,20 +14,15 @@ CoreUtils::requireTimeslot();
 
 $show_title = MyRadio_Timeslot::getInstance($_SESSION['timeslotid'])->getMeta('title');
 
-if (isset($_REQUEST['readonly'])) {
-    $template = 'NIPSWeb/readonly.twig';
-    $reslists = [];
-} else {
-    $template = 'NIPSWeb/main.twig';
-    $reslists = CoreUtils::dataSourceParser(
-        [
-            'managed' => iTones_Playlist::getAlliTonesPlaylists(),
-            'auto' => NIPSWeb_AutoPlaylist::getAllAutoPlaylists(),
-            'aux' => NIPSWeb_ManagedPlaylist::getAllManagedPlaylists(),
-            'user' => NIPSWeb_ManagedUserPlaylist::getAllManagedUserPlaylistsFor(MyRadio_User::getInstance()),
-        ]
-    );
-}
+$template = 'NIPSWeb/main.twig';
+$reslists = CoreUtils::dataSourceParser(
+    [
+        'managed' => iTones_Playlist::getAlliTonesPlaylists(),
+        'auto' => NIPSWeb_AutoPlaylist::getAllAutoPlaylists(),
+        'aux' => NIPSWeb_ManagedPlaylist::getAllManagedPlaylists(),
+        'user' => NIPSWeb_ManagedUserPlaylist::getAllManagedUserPlaylistsFor(MyRadio_User::getInstance()),
+    ]
+);
 
 CoreUtils::getTemplateObject()->setTemplate($template)
     ->addVariable('title', "Show Planner")
