4428aedf5c45b1ef401a8db7bc4f3eb6b634c038
Matthew Stratford <matthew@mstratford.net>
2017-04-23 23:13:54 +0100

    Wouldn't short circuit on the default setting
    idsort is true for the default setting, so it was skipping this and
    doing the whole complicated search instead.

--- src/Classes/ServiceAPI/MyRadio_Track.php
+++ src/Classes/ServiceAPI/MyRadio_Track.php
@@ -805,7 +805,7 @@ class MyRadio_Track extends ServiceAPI
             && !$options['recordid']
             && !$options['lastfmverified']
             && !$options['random']
-            && !$options['idsort']
+            &&  $options['idsort']
             && !$options['custom']
             && !$options['nocorrectionproposed']
             && !$options['clean']
