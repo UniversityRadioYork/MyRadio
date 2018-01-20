d7e57a3f910de594848a67f2b8f0f06be75ef6e7
Matthew Stratford <matthew@mstratford.net>
2018-01-20 04:51:01 +0000

    Add UI form to add permissions.

---
+++ src/Controllers/MyRadio/addPermission.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Provides a tool to add new permissions for MyRadio Service/Module/Action systems.
+ */
+use \MyRadio\MyRadio\AuthUtils;
+use \MyRadio\MyRadio\CoreUtils;
+use \MyRadio\MyRadio\URLUtils;
+use \MyRadio\MyRadio\MyRadioForm;
+use \MyRadio\MyRadio\MyRadioFormField;
+
+/*
+ * Form definition for adding permissions.
+ */
+$form = new MyRadioForm(
+    'add_permission',
+    $module,
+    $action,
+    [
+        'title' => 'Permissions',
+        'subtitle' => 'New Permission',
+        'description' => 'This page allows you to add a new permission so that it can be added to members and modules/actions.'
+    ]
+);
+
+$form->addField(
+    new MyRadioFormField(
+        'constant',
+        MyRadioFormField::TYPE_TEXT,
+        [
+            'explanation' => 'Type a constant name (AUTH_NAME) for the permission.',
+            'label' => 'Name',
+            'required' => true,
+        ]
+    )
+)->addField(
+    new MyRadioFormField(
+        'description',
+        MyRadioFormField::TYPE_TEXT,
+        [
+            'explanation' => 'Type a description for what this permission allows.',
+            'label' => 'Description',
+            'required' => true,
+        ]
+    )
+);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    //Submitted
+    $data = $form->readValues();
+
+    $constant = $data['constant'];
+    if (substr($constant, 0, 5) == 'AUTH_' && strlen($constant) >= 6) {
+        AuthUtils::addPermission($data['description'],$constant);
+        $message = 'The permission "'. $constant .'" has been added successfully.';
+        URLUtils::redirectWithMessage('MyRadio','listPermissions',$message);
+    } else {
+        URLUtils::backWithMessage('The permission name should be a constant starting in "AUTH_".');
+    }
+
+} else {
+    //Not submitted
+    $form->render();
+}
