40f330f806f3714e62224a6254b28101b8fb746a
Matthew Stratford <hi@mstratford.net>
2018-06-18 00:11:09 +0100

    Let permissions be dealt with in UI

--- src/Controllers/Podcast/allPodcast.php
+++ src/Controllers/Podcast/allPodcast.php
@@ -6,8 +6,6 @@ use \MyRadio\MyRadio\CoreUtils;
 use \MyRadio\MyRadio\AuthUtils;
 use \MyRadio\ServiceAPI\MyRadio_Podcast;
 
-AuthUtils::requirePermission(AUTH_EDITALLPODCASTS);
-
 CoreUtils::getTemplateObject()->setTemplate('table.twig')
     ->addVariable('tablescript', 'myradio.podcasts')
     ->addVariable('title', 'Podcasts')
