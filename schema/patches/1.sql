044dde3c4299bacd7d378fc2a4b4aef0cac63501
Marks Polakovs <github@markspolakovs.me>
2019-10-25 11:57:35 +0100

    Show Colours part 1 - database schema

--- schema/patches/1.sql
+++ schema/patches/1.sql
@@ -1,21 +1,39 @@
-ALTER TABLE myury.act_permission SET SCHEMA myradio;
-ALTER TABLE myury.actions SET SCHEMA myradio;
-ALTER TABLE myury.api_class_map SET SCHEMA myradio;
-ALTER TABLE myury.api_key SET SCHEMA myradio;
-ALTER TABLE myury.api_key_auth SET SCHEMA myradio;
-ALTER TABLE myury.api_method_auth SET SCHEMA myradio;
-ALTER TABLE myury.award_categories SET SCHEMA myradio;
-ALTER TABLE myury.award_member SET SCHEMA myradio;
-ALTER TABLE myury.error_rate SET SCHEMA myradio;
-ALTER TABLE myury.menu_columns SET SCHEMA myradio;
-ALTER TABLE myury.menu_links SET SCHEMA myradio;
-ALTER TABLE myury.menu_module SET SCHEMA myradio;
-ALTER TABLE myury.menu_sections SET SCHEMA myradio;
-ALTER TABLE myury.menu_twigitems SET SCHEMA myradio;
-ALTER TABLE myury.modules SET SCHEMA myradio;
-ALTER TABLE myury.password_reset_token SET SCHEMA myradio;
-ALTER TABLE myury.photos SET SCHEMA myradio;
-ALTER TABLE myury.services SET SCHEMA myradio;
-DROP TABLE myury.services_versions;
-DROP TABLE myury.services_versions_member;
-DROP SCHEMA myury;
+SET search_path = schedule, pg_catalog;
+CREATE TABLE show_subtypes
+(
+    show_subtype_id INTEGER NOT NULL,
+    name            text    NOT NULL,
+    colour          CHAR(6) NOT NULL
+);
+
+COMMENT ON TABLE show_subtypes IS 'The various subtypes of show (music, news etc.)';
+COMMENT ON COLUMN show_subtypes.name IS 'The publicly visible name of the subtype.';
+COMMENT ON COLUMN show_subtypes.colour IS 'The colour of the subtype, as a hex colour code without the leading # (eg. E91E61)';
+
+INSERT INTO show_subtypes (show_subtype_id, name, colour)
+VALUES (1,
+        'Regular',
+        '002d5a'),
+       (2,
+        'Primetime',
+        'feb93a'),
+       (3,
+        'Events',
+        'd0011b'),
+       (4,
+        'News',
+        '5A162E'),
+       (4,
+        'Speech',
+        '2A722B'),
+       (5,
+        'Music',
+        '562179'),
+       (6,
+        'Collaboration',
+        'd14fda');
+
+-- we're not using a show_subtype join table, because it
+-- makes no sense for a show to have multiple subtypes
+ALTER TABLE show ADD COLUMN subtype INTEGER NULL DEFAULT NULL;
+COMMENT ON COLUMN show.subtype IS 'The subtype of the show - determines its colour on the website.';
