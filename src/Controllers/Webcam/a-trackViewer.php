43bcab3f94f42e7d77ff4700ce3ad513f357fea3
Matthew Stratford <hi@mstratford.net>
2019-10-28 14:07:19 +0000

    Prevent broken email when browser wakes.

--- src/Controllers/Webcam/a-trackViewer.php
+++ src/Controllers/Webcam/a-trackViewer.php
@@ -5,9 +5,11 @@
 use \MyRadio\MyRadio\URLUtils;
 use \MyRadio\ServiceAPI\MyRadio_User;
 use \MyRadio\ServiceAPI\MyRadio_Webcam;
+use \MyRadio\MyRadioException;
 
 if (isset($_SESSION['webcam_lastcounterincrement']) && $_SESSION['webcam_lastcounterincrement'] > time() - 10) {
-    require 'Controllers/Errors/400.php';
+    // Occurs when browser wakes up and tries to spam all the missed updates.
+    throw new MyRadioException("The Webcam counter was last updated too recently, this request was ingored.", 400);
 }
 $_SESSION['webcam_lastcounterincrement'] = time();
 
