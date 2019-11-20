3019b66ca54fa25acb0cf7c2ae00c8ce89540762
Marks Polakovs <github@markspolakovs.me>
2019-11-20 21:32:50 +0000

    Subtype not required for seasons

--- src/Classes/ServiceAPI/MyRadio_Season.php
+++ src/Classes/ServiceAPI/MyRadio_Season.php
@@ -414,7 +414,8 @@ class MyRadio_Season extends MyRadio_Metadata_Common
                         ),
                         'label' => 'Subtype',
                         'explanation' => 'If necessary, override the subtype for this season. '
-                            .'If you\'re not sure what you\'re doing, leave it what it is.'
+                            .'If you\'re not sure what you\'re doing, leave it what it is.',
+                        'required' => false
                     ]
                 )
             )->addField(
