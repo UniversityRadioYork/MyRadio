83d3f8dc111b0b18e8d5a1481b14918006aa9453
Marks Polakovs <github@markspolakovs.me>
2019-12-28 10:45:52 +0100

    Make jukebox getTrackToPlay more reliable
    This commit makes getTrackToPlay more reliable by properly checking if the potential track is currently being played. Previously it only checked if it was in a queue (plus some half-baked attempt to check the current queue that I don't think ever worked).
    
    This is based on this change to Jukebox: https://github.com/UniversityRadioYork/jukebox/commit/54fffed4dc2b586057e351dc76cf27c89cbf2c8b

--- src/Classes/iTones/iTones_Utils.php
+++ src/Classes/iTones/iTones_Utils.php
@@ -21,7 +21,7 @@ use MyRadio\iTones\iTones_Playlist;
 class iTones_Utils extends \MyRadio\ServiceAPI\ServiceAPI
 {
     private static $telnet_handle;
-    private static $queues = ['requests', 'main'];
+    private static $queues = ['requests'];
     private static $queue_cache = [];
     public static $ops = [];
 
@@ -86,8 +86,9 @@ class iTones_Utils extends \MyRadio\ServiceAPI\ServiceAPI
                     // These ones involve running more queries...
                     && !MyRadio_TracklistItem::getIfPlayedRecently($track)
                     && MyRadio_TracklistItem::getIfAlbumArtistCompliant($track)
-                    // And this one involves telnet!
+                    // And these ones involve telnet!
                     && !iTones_Utils::getIfQueued($track)
+                    && !iTones_Utils::getIfNowPlaying($track)
                 ) {
                     return $track;
                 }
@@ -208,6 +209,16 @@ class iTones_Utils extends \MyRadio\ServiceAPI\ServiceAPI
         }
 
         return false;
+    }
+
+    /**
+     * Check if a track is being played by Jukebox right now.
+     * @param MyRadio_Track $track
+     * @return bool
+     */
+    public static function getIfNowPlaying(MyRadio_Track $track) {
+        $id = self::telnetOp('now_playing');
+        return (int) $id === $track->getID();
     }
 
     /**
