6b9b94217d4ed4c35d05ae447ba3b237a499ae66
Charlie Root <root@ury.york.ac.uk>
2020-01-15 22:33:07 +0000

    Allow Uni MTA to accept emails from eduroam users.

--- src/Classes/ServiceAPI/MyRadio_User.php
+++ src/Classes/ServiceAPI/MyRadio_User.php
@@ -1406,8 +1406,13 @@ class MyRadio_User extends ServiceAPI implements APICaller
             if (empty($email)) {
                 continue;
             } else {
-                $data[] = [$user->getLocalAlias(), $email];
-            }
+		$local = $user->getLocalAlias();
+                $data[] = [$local, $email];
+            	$eduroam = $user->getEduroam();
+		if (!empty($eduroam) && ($eduroam !== $email)) {
+                	$data[] = [$eduroam, $email];
+		}
+	    }
         }
 
         return $data;
