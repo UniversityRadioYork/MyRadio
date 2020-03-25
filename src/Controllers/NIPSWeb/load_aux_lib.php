5259290882c3839262322e165937e1c304df9110
Marks Polakovs <github@markspolakovs.me>
2020-03-25 16:17:46 +0100

    M O R E C O R S

--- src/Controllers/NIPSWeb/load_aux_lib.php
+++ src/Controllers/NIPSWeb/load_aux_lib.php
@@ -6,6 +6,9 @@ use \MyRadio\MyRadio\URLUtils;
 use \MyRadio\NIPSWeb\NIPSWeb_ManagedPlaylist;
 use \MyRadio\NIPSWeb\NIPSWeb_ManagedUserPlaylist;
 
+header("Access-Control-Allow-Origin: " . $_SERVER["HTTP_ORIGIN"]);
+header("Access-Control-Allow-Credentials: true");
+
 if (preg_match('/^aux-.*$/', $_REQUEST['libraryid']) === 1) {
     $libraryid = str_replace('aux-', '', $_REQUEST['libraryid']);
     $data = NIPSWeb_ManagedPlaylist::getInstance($libraryid)->getItems();
