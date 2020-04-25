5b4889d79bed0196911dd076766dd65fb609dedd
cs-499243 <63954929+cs-499243@users.noreply.github.com>
2020-04-25 16:57:18 +0100

    Add new listing system
    Removes old image variable and adds in "text"

--- src/Controllers/Stats/trainingMap.php
+++ src/Controllers/Stats/trainingMap.php
@@ -10,16 +10,16 @@ $status_map = function ($status) {
 };
 
 $caption = 'Please select a Training Status above.';
-$img = '';
+
 if (isset($_GET['id'])) {
     $title = MyRadio_TrainingStatus::getInstance($_GET['id'])->getTitle();
-    $caption = 'This is a map of who trained who for the ' . $title . ' Training Status.';
-    $img = 'img/stats_training_' . $_GET['id'] . '.svg';
+    $caption = 'List of the people who have: ' . $title . '...';
+    $text = implode(" ,  ",MyRadio_TrainingStatus::getInstance($_GET['id'])->getListAll());
 }
 
 CoreUtils::getTemplateObject()->setTemplate('MyRadio/trainingMap.twig')
     ->addVariable('title', 'Member Training Graph')
     ->addVariable('maps', array_map($status_map, MyRadio_TrainingStatus::getAll()))
     ->addVariable('caption', $caption)
-    ->addVariable('image', $img)
+    ->addVariable('text', $text)
     ->render();
