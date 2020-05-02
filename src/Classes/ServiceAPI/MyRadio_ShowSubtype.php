7c49ffbcb4d1536980822e47efb1e8a9f0f826bc
Michael Grace <56653532+michael-grace@users.noreply.github.com>
2020-05-03 00:46:02 +0100

    add description field and appropriate getters

--- src/Classes/ServiceAPI/MyRadio_ShowSubtype.php
+++ src/Classes/ServiceAPI/MyRadio_ShowSubtype.php
@@ -33,6 +33,12 @@ class MyRadio_ShowSubtype extends ServiceAPI
      * @var string
      */
     private $class;
+    
+    /**
+     * The description of the subtype
+     * @var string
+    */
+    private $description;
 
     public function __construct($data)
     {
@@ -40,6 +46,7 @@ class MyRadio_ShowSubtype extends ServiceAPI
         $this->show_subtype_id = $data['show_subtype_id'];
         $this->name = $data['name'];
         $this->class = $data['class'];
+        $this->description = $data['description']
     }
 
     public function getID()
@@ -65,13 +72,23 @@ class MyRadio_ShowSubtype extends ServiceAPI
     {
         return $this->class;
     }
+    
+    /**
+     * Get the description of the subtype
+     * @return string
+    */
+    public function getDescription()
+    {
+        return $this->description;
+    }
 
     public function toDataSource($mixins = [])
     {
         return [
             'id' => $this->getID(),
             'name' => $this->getName(),
-            'class' => $this->getClass()
+            'class' => $this->getClass(),
+            'description' => $this->getDescription()
         ];
     }
 
@@ -82,7 +99,7 @@ class MyRadio_ShowSubtype extends ServiceAPI
      */
     public static function getAll()
     {
-        $sql = 'SELECT show_subtype_id, name, class FROM schedule.show_subtypes';
+        $sql = 'SELECT show_subtype_id, name, class, description FROM schedule.show_subtypes';
         $rows = self::$db->fetchAll($sql);
 
         $subtypes = [];
