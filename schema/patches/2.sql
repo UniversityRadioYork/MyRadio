25e71f76bcd28b2ab0521212efc027b2acccf4aa
Marks Polakovs <marks@markspolakovs.me>
2020-05-24 14:17:33 +0200

    Finish off query

--- schema/patches/2.sql
+++ schema/patches/2.sql
@@ -3,23 +3,51 @@ BEGIN;
 CREATE TABLE myradio.analytics (
     analytics_record_id BIGSERIAL PRIMARY KEY,
     page TEXT,
-    referrer TEXT,
-    memberid INTEGER REFERENCES public.member (memberid) NULL,
+    ref TEXT DEFAULT NULL,
+    member_officerships integer[],
+    member_shows_bucketed integer,
     session_id VARCHAR(32)
 );
 
-ALTER TABLE public.member
-    ADD COLUMN analytics_consent CHAR(1) DEFAULT 'n';
-
 CREATE FUNCTION myr_create_analytics_record(
     page TEXT,
-    referrer TEXT,
+    ref TEXT,
     memberid INTEGER,
     session_id VARCHAR(32)
 )
+RETURNS VOID
 AS $$
+    DECLARE
+        officerships INTEGER[];
+        num_shows INTEGER;
     BEGIN
-        IF (SELECT analytics_consent FROM public.member WHERE member.memberid = memberid)
+        -- Find officerships
+        SELECT array_agg(officerid) INTO officerships
+        FROM public.member_officer
+        WHERE member_officer.memberid = myr_create_analytics_record.memberid
+        AND from_date <= NOW()
+        AND (till_date IS NULL OR till_date >= (NOW() + '28 days'::INTERVAL));
+
+        -- Find number of shows
+        SELECT COUNT(*) INTO num_shows
+        FROM schedule.show
+        WHERE show.memberid=myr_create_analytics_record.memberid
+        OR show_id IN (
+               SELECT show_id FROM schedule.show_credit
+                  WHERE creditid=myr_create_analytics_record.memberid AND
+                      (effective_to >= NOW() OR effective_to IS NULL)
+        );
+
+        -- Right, we've got what we need
+        INSERT INTO myradio.analytics
+        (page, ref, member_officerships, member_shows_bucketed, session_id)
+        VALUES (
+                myr_create_analytics_record.page,
+                myr_create_analytics_record.ref,
+                officerships,
+                CEIL(num_shows / 5),
+                myr_create_analytics_record.session_id
+               );
     END;
     $$
 LANGUAGE plpgsql;
--- src/Controllers/root_web.php
+++ src/Controllers/root_web.php
@@ -5,6 +5,7 @@
  * the necessary handler.
  */
 use \MyRadio\Config;
+use MyRadio\Database;
 use \MyRadio\MyRadio\AuthUtils;
 use \MyRadio\MyRadio\CoreUtils;
 
@@ -77,6 +78,18 @@ AuthUtils::requirePermissionAuto($module, $action);
 if (isset($_REQUEST['joyride'])) {
     $_SESSION['joyride'] = $_REQUEST['joyride'];
 }
+
+// Apply analytics
+
+Database::getInstance()->query(
+    'SELECT myr_create_analytics_record($1, $2, $3, $4)',
+    [
+        $module . '/' . $action,
+        $_GET['ref'],
+        $_SESSION['memberid'],
+        session_id()
+    ]
+);
 
 //Include the requested action
 require 'Controllers/'.$module.'/'.$action.'.php';
