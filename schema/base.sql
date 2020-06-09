f92b9484c93da1764b4522685ecb4c59f633cf74
Marks Polakovs <marks@markspolakovs.me>
2020-06-09 21:38:43 +0200

    rip quotes board ????-2020

--- schema/base.sql
+++ schema/base.sql
@@ -11,7 +11,7 @@ COMMENT ON SCHEMA music IS 'Charts and possibly other stuff too.';
 CREATE SCHEMA myury;
 COMMENT ON SCHEMA myury IS 'Schema for new/migrated data for the Members Internal replacement';
 CREATE SCHEMA people;
-COMMENT ON SCHEMA people IS 'Tables for the LeRouge Extensible Roles (Or User Groups) Engine, as well as the quotes board.';
+COMMENT ON SCHEMA people IS 'Tables for the LeRouge Extensible Roles (Or User Groups).';
 CREATE SCHEMA schedule;
 COMMENT ON SCHEMA schedule IS 'Schema for the MyRadio schedule.';
 CREATE SCHEMA sis2;
@@ -740,19 +740,6 @@ As of writing the metadata schema is:
 COMMENT ON COLUMN metadata.roleid IS 'The unique ID of the role this metadatum concerns.';
 COMMENT ON COLUMN metadata.key IS 'The key of the metadatum; should fit the site schema.';
 COMMENT ON COLUMN metadata.value IS 'The value of the metadatum.';
-CREATE TABLE quote (
-    quote_id integer NOT NULL,
-    text text NOT NULL,
-    source integer NOT NULL,
-    date timestamp with time zone DEFAULT now() NOT NULL
-);
-CREATE SEQUENCE quote_quote_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-ALTER SEQUENCE quote_quote_id_seq OWNED BY quote.quote_id;
 CREATE TABLE role (
     role_id integer NOT NULL,
     alias character varying(100) NOT NULL,
@@ -2380,7 +2367,6 @@ SET search_path = people, pg_catalog;
 ALTER TABLE ONLY credit_type ALTER COLUMN credit_type_id SET DEFAULT nextval('"schedule.showcredittype_id_seq"'::regclass);
 ALTER TABLE ONLY group_root_role ALTER COLUMN group_root_role_id SET DEFAULT nextval('group_root_role_group_root_role_id_seq'::regclass);
 ALTER TABLE ONLY group_type ALTER COLUMN group_type_id SET DEFAULT nextval('group_type_group_type_id_seq'::regclass);
-ALTER TABLE ONLY quote ALTER COLUMN quote_id SET DEFAULT nextval('quote_quote_id_seq'::regclass);
 ALTER TABLE ONLY role ALTER COLUMN role_id SET DEFAULT nextval('roles_id_seq'::regclass);
 ALTER TABLE ONLY role_inheritance ALTER COLUMN role_inheritance_id SET DEFAULT nextval('role_inheritance_role_inheritance_id_seq'::regclass);
 ALTER TABLE ONLY role_text_metadata ALTER COLUMN role_text_metadata_id SET DEFAULT nextval('role_metadata_role_metadata_id_seq'::regclass);
@@ -2959,13 +2945,6 @@ ALTER TABLE ONLY group_type
 ALTER TABLE ONLY metadata
     ADD CONSTRAINT metadata_pkey PRIMARY KEY (roleid, key);
 
-
---
--- Name: quote_pkey; Type: CONSTRAINT; Schema: people
---
-
-ALTER TABLE ONLY quote
-    ADD CONSTRAINT quote_pkey PRIMARY KEY (quote_id);
 
 
 --
@@ -5609,13 +5588,6 @@ ALTER TABLE ONLY role_inheritance
 ALTER TABLE ONLY role_inheritance
     ADD CONSTRAINT parents_parentid_fkey FOREIGN KEY (parent_id) REFERENCES role(role_id) ON UPDATE CASCADE ON DELETE RESTRICT;
 
-
---
--- Name: quote_source_fkey; Type: FK CONSTRAINT; Schema: people
---
-
-ALTER TABLE ONLY quote
-    ADD CONSTRAINT quote_source_fkey FOREIGN KEY (source) REFERENCES public.member(memberid);
 
 
 --
--- src/Controllers/Quotes/default.php
+++ src/Controllers/Quotes/default.php
@@ -1,13 +1,7 @@
 <?php
 
-/**
- * Allows the addition of new quotes.
- */
 use \MyRadio\MyRadio\CoreUtils;
-use \MyRadio\ServiceAPI\MyRadio_Quote;
 
-CoreUtils::getTemplateObject()->setTemplate('table.twig')
+CoreUtils::getTemplateObject()->setTemplate('Quotes/default.twig')
     ->addVariable('title', 'Quotes')
-    ->addVariable('tabledata', CoreUtils::setToDataSource(MyRadio_Quote::getAll()))
-    ->addVariable('tablescript', 'myradio.quotes')
     ->render();
