a1aee8910b7d58f3d8acf2c3a7eaf4ec9a3c4566
Marks Polakovs <marks@markspolakovs.me>
2020-08-02 22:45:01 +0200

    "alphabetically by ID" Marks please

--- schema/patches/4.sql
+++ schema/patches/4.sql
@@ -4,7 +4,7 @@ ALTER TABLE public.mail_list
     ADD COLUMN ordering INTEGER DEFAULT 0;
 
 COMMENT ON COLUMN public.mail_list.ordering
-    IS 'The order in which to display these lists in MyRadio. Ascending, equal values sorted alphabetically by ID. Set to negative to hide from MyRadio.';
+    IS 'The order in which to display these lists in MyRadio. Ascending, equal values sorted by ID. Set to negative to hide from MyRadio.';
 
 ALTER TABLE public.mail_list
     DROP COLUMN current;
