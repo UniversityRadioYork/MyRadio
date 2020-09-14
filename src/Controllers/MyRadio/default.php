474fd7ba841b7c392e38a81a991a212c1d78e1a9
Marks Polakovs <marks@markspolakovs.me>
2020-09-14 15:34:37 +0100

    Add to home screen

--- src/Controllers/MyRadio/default.php
+++ src/Controllers/MyRadio/default.php
@@ -7,6 +7,7 @@
  */
 use \MyRadio\Config;
 use \MyRadio\MyRadio\CoreUtils;
+use MyRadio\ServiceAPI\MyRadio_Event;
 use \MyRadio\ServiceAPI\MyRadio_User;
 use \MyRadio\MyRadio\MyRadioMenu;
 use \MyRadio\MyRadio\MyRadioNews;
@@ -17,6 +18,8 @@ $menu = (new MyRadioMenu())->getMenuForUser();
 
 $news = MyRadioNews::getLatestNewsItem(Config::$news_feed, $user);
 $news_clickthrough = Config::$members_news_enable && empty($news['seen']);
+
+$events = MyRadio_Event::getNext(5);
 
 $twig = CoreUtils::getTemplateObject()->setTemplate('MyRadio/menu.twig')
         ->addVariable('title', 'Menu')
@@ -30,7 +33,8 @@ $twig = CoreUtils::getTemplateObject()->setTemplate('MyRadio/menu.twig')
         ->addVariable('is_trainer', $user->isTrainer())
         ->addVariable('has_show', $user->hasShow())
         ->addVariable('paid', $user->isCurrentlyPaid())
-        ->addVariable('contract_signed', $user->hasSignedContract());
+        ->addVariable('contract_signed', $user->hasSignedContract())
+        ->addVariable('events', $events);
 
 if (Config::$members_news_enable) {
     $twig->addVariable('news', $news);
