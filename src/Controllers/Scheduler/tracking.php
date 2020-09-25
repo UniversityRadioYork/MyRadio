8d9a6c1cab97ccece34177f114cf44d2d36acd02
michael-grace <michael.dp.grace@outlook.com>
2020-09-25 01:29:15 +0100

    tracking viewer

---
+++ src/Controllers/Scheduler/tracking.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Information about user locations
+ */
+
+use \MyRadio\MyRadio\CoreUtils;
+use \MyRadio\ServiceAPI\MyRadio_Season;
+
+$data = [];
+
+foreach (MyRadio_Season::getAllSeasonsInLatestTerm() as $season) {
+    foreach ($season->getAllTimeslots() as $timeslot) {
+        if ($timeslot->getStartTime() < time()) {
+            foreach ($timeslot->getSigninInfo() as $info) {
+                if ($info['location'] != 'Jukeboxland') {
+                    if (isset($info['user'])) {
+                        $data[] =
+                            [
+                                "type" => "URY Member",
+                                "info" => $info['user']->getName() . ($info['user']->getEduroam() ? " (" . $info['user']->getEduroam() . ")" : ""),
+                                "location" => $info['location'],
+                                "time" => CoreUtils::happyTime($info['time'])
+                            ];
+                    } else if ($info['guest_info']) {
+                        $data[] =
+                            [
+                                "type" => "Guest",
+                                "info" => $info['guest_info'],
+                                "location" => $info['location'],
+                                "time" => CoreUtils::happyTime($info['time'])
+                            ];
+                    }
+                }
+            }
+        }
+    }
+}
+
+CoreUtils::getTemplateObject()->setTemplate('table.twig')
+    ->addVariable('title', 'Tracking Information')
+    ->addVariable('tabledata', $data)
+    ->addVariable('tablescript', 'myradio.scheduler.tracking')
+    ->render();
