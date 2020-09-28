d32377304fe6825bf23c101a706fd33961103446
Matthew Stratford <hi@mstratford.net>
2020-09-28 02:55:50 +0100

    Fixes. Actually works now.

--- src/Public/js/nipsweb.import.js
+++ src/Public/js/nipsweb.import.js
@@ -9,7 +9,7 @@ var currentShowPlan;
 // Stage 1) Load the current user's shows.
 $(document).ready(
   function () {
-    myradio.callAPI("GET","user","currentuser", "","","",
+    myradio.callAPI("GET", "user", "currentuser", "", "", "",
       function (data) {
 
         for (item in data) {
@@ -24,7 +24,7 @@ $(document).ready(
 );
 
 function getUserShows(currentUserID) {
-  myradio.callAPI("GET","user","shows",currentUserID,"","",
+  myradio.callAPI("GET", "user", "shows", currentUserID, "", "",
     function (data) {
       var show;
       for (show in data) {
@@ -39,8 +39,8 @@ function getUserShows(currentUserID) {
       $("#import-show-selector").prop("disabled", false);
 
       //If we've only got one show, select that automatically.
-      if (data.payload.length == 1 ) {
-        $("#import-show-selector option").last().prop("selected",true);
+      if (data.payload.length == 1) {
+        $("#import-show-selector option").last().prop("selected", true);
         updateSeasonList();
       }
     }
@@ -49,7 +49,7 @@ function getUserShows(currentUserID) {
 
 // Stage 2) Select the show & load its seasons
 
-$("#import-show-selector").change(function() {
+$("#import-show-selector").change(function () {
   updateSeasonList();
 });
 
@@ -57,7 +57,7 @@ function updateSeasonList() {
   var selectedShowID = $("#import-show-selector").find(":selected").attr("value");
   //remove all the previously shown seasons (if any), without removing the placeholder option.
   $("#import-season-selector option:not(:disabled)").remove();
-  myradio.callAPI("GET","show","allseasons",selectedShowID,"","",
+  myradio.callAPI("GET", "show", "allseasons", selectedShowID, "", "",
     function (data) {
       var season;
       for (season in data) {
@@ -70,15 +70,15 @@ function updateSeasonList() {
           $("#import-season-selector").append("<option value='" + data.payload[season].season_id + "'>Season " + data.payload[season].season_num + "</option>");
         }
         //apparently chrome doesn't reset back to the first (and only) option sometimes.
-        $("#import-season-selector option").first().prop("selected",true);
+        $("#import-season-selector option").first().prop("selected", true);
         $("#import-season-selector").prop("disabled", false);
         //If we've only got one season, select that automatically.
-        if (data.payload.length == 1 ) {
-          $("#import-season-selector option").last().prop("selected",true);
+        if (data.payload.length == 1) {
+          $("#import-season-selector option").last().prop("selected", true);
         }
       } else {
         $("#import-season-selector").append("<option>No seasons in this show.</option>");
-        $("#import-season-selector option").last().prop("selected",true);
+        $("#import-season-selector option").last().prop("selected", true);
         $("#import-season-selector").prop("disabled", true);
       }
       updateTimeslotList();
@@ -87,7 +87,7 @@ function updateSeasonList() {
 }
 
 // Stage 3) Select this show's season & load its timeslots.
-$("#import-season-selector").change(function() {
+$("#import-season-selector").change(function () {
   updateTimeslotList();
 });
 
@@ -96,7 +96,7 @@ function updateTimeslotList() {
   //remove all the previously shown timeslots (if any), without removing the placeholder option.
   $("#import-timeslot-selector option:not(:disabled)").remove();
   if (selectedSeasonID != "null" && typeof selectedSeasonID != "undefined") {
-    myradio.callAPI("GET","season","alltimeslots",selectedSeasonID,"","",
+    myradio.callAPI("GET", "season", "alltimeslots", selectedSeasonID, "", "",
       function (data) {
         var timeslot;
         for (timeslot in data) {
@@ -109,31 +109,31 @@ function updateTimeslotList() {
             $("#import-timeslot-selector").append("<option value='" + data.payload[timeslot].timeslot_id + "'>Episode " + data.payload[timeslot].timeslot_num + " (" + data.payload[timeslot].start_time + ")</option>");
           }
           //apparently chrome doesn't reset back to the first (and only) option sometimes.
-          $("#import-timeslot-selector option").first().prop("selected",true);
+          $("#import-timeslot-selector option").first().prop("selected", true);
           $("#import-timeslot-selector").prop("disabled", false);
           //If we've only got one timeslot, select that automatically.
-          if (data.payload.length == 1 ) {
-            $("#import-timeslot-selector option").last().prop("selected",true);
+          if (data.payload.length == 1) {
+            $("#import-timeslot-selector option").last().prop("selected", true);
             loadShowPlan();
             $("#import-showplan").fadeIn();
           }
         } else {
           $("#import-timeslot-selector").append("<option>No episodes in this season.</option>");
-          $("#import-timeslot-selector option").last().prop("selected",true);
+          $("#import-timeslot-selector option").last().prop("selected", true);
           $("#import-timeslot-selector").prop("disabled", true);
           $("#import-showplan").fadeOut();
         }
       }
     );
   } else {
-    $("#import-timeslot-selector option").first().prop("selected",true);
+    $("#import-timeslot-selector option").first().prop("selected", true);
     $("#import-timeslot-selector").prop("disabled", true);
     $("#import-showplan").fadeOut();
   }
 }
 
 // Stage 4) Select the timeslot & load the showplan from it.
-$("#import-timeslot-selector").change(function() {
+$("#import-timeslot-selector").change(function () {
   loadShowPlan();
   $("#import-showplan").fadeIn();
 });
@@ -178,9 +178,9 @@ function loadChannelList() {
         itemid = "ManagedDB-" + item.managedid;
         expired = item.expired;
       }
-      disabled = (expired ? "disabled": "");
+      disabled = (expired ? "disabled" : "");
       $("#import-channel-list").append("<li class=\"" + disabled + "\"><input type=\"checkbox\" class=\"channel-list-item\" "
-        + disabled + " value=\""+ itemid + "\">"
+        + disabled + " value=\"" + itemid + "\">"
         + cleanStars + item.title + extraString + "</li>");
       $("#import-channel-filter-btns").fadeIn();
     }
@@ -189,7 +189,7 @@ function loadChannelList() {
 
 // Stage 5) Select a channel to import from.
 
-$("#import-channel-selector a").on("click","", function() {
+$("#import-channel-selector a").on("click", "", function () {
   selectChannel($(this).attr("channel"));
 }
 );
@@ -202,28 +202,28 @@ function selectChannel(channelNo) {
 
 // Stage 6) Filter the tracks quickly.
 
-var selectAll = function() {  //"select all" change
-  $(".channel-list-item").each(function(){ //iterate all listed checkbox items
+var selectAll = function () {  //"select all" change
+  $(".channel-list-item").each(function () { //iterate all listed checkbox items
     this.checked = true; //change checkbox checked status
   });
 };
-var selectNone = function() {  //"select none" change
-  $(".channel-list-item").each(function(){ //iterate all listed checkbox items
+var selectNone = function () {  //"select none" change
+  $(".channel-list-item").each(function () { //iterate all listed checkbox items
     this.checked = false; //change checkbox checked status
   });
 };
 
 function startImport(channelNo) {
   myradio.callAPI("GET", "timeslot", "userselectedtimeslot", "", "", "",
-  function (data) {
-    var item;
-    for (item in data) {
-      if (item === "myradio_errors") {
-        continue;
+    function (data) {
+      var item;
+      for (item in data) {
+        if (item === "myradio_errors") {
+          continue;
+        }
       }
-    }
-    getExistingShowPlan(data.payload.timeslot_id, channelNo);
-  });
+      getExistingShowPlan(data.payload.timeslot_id, channelNo);
+    });
 }
 
 // Stage 7) Get the new weights that new channel items need to be added to
@@ -231,29 +231,31 @@ function getExistingShowPlan(timeslotID, channelNo) {
 
   let nextChannelWeights = [];
   myradio.callAPI("GET", "timeslot", "showplan", timeslotID, "", "",
-  function (data) {
-    var item;
-    for (item in data) {
-      if (item === "myradio_errors") {
-        continue;
+    function (data) {
+      var item;
+      for (item in data) {
+        if (item === "myradio_errors") {
+          continue;
+        }
       }
-    }
-    currentShowPlan = data.payload;
-    for (channel in currentShowPlan) {
-      if (channel.length > 0) {
-        nextChannelWeights.push((channel[-1].weight)+1)
-      } else {
-        nextChannelWeights.push(0);
+      currentShowPlan = data.payload;
+      for (channelKey in currentShowPlan) {
+        const channel = currentShowPlan[channelKey];
+        console.log(channel);
+        if (channel.length > 0) {
+          nextChannelWeights.push((channel[channel.length - 1].weight) + 1)
+        } else {
+          nextChannelWeights.push(0);
+        }
       }
-    }
 
-    calculateOps(channelNo, nextChannelWeights);
-  });
+      calculateOps(timeslotID, channelNo, nextChannelWeights);
+    });
 }
 
 // Stage 8) Actually import the selected tracks.
 
-function calculateOps(channelNo, nextChannelWeights) {
+function calculateOps(timeslotID, channelNo, nextChannelWeights) {
 
 
 
@@ -261,8 +263,8 @@ function calculateOps(channelNo, nextChannelWeights) {
   if ($("input[type=checkbox]:checked").length > 0) {
     myradio.showAlert("Importing Tracks...", "warning");
     $("input[type=checkbox]:checked").each(function () {
-      const weight = newChannelWeights[channelNo];
-      newChannelWeights[weight]++;
+      const weight = nextChannelWeights[channelNo];
+      nextChannelWeights[weight]++;
 
       ops.push({
         op: "AddItem",
@@ -271,7 +273,7 @@ function calculateOps(channelNo, nextChannelWeights) {
         weight: weight
       });
     });
-    shipChanges(currentTimeslotID, ops);
+    shipChanges(timeslotID, ops);
   } else {
     myradio.showAlert("No tracks were selected.", "danger");
   }
@@ -314,6 +316,6 @@ var shipChanges = function (timeslotID, ops) {
 };
 
 // Stage 8) reload the page to show your newly imported tracks. Clicked from a showAlert.
-var reload = function() {
+var reload = function () {
   parent.location.reload();
 };
