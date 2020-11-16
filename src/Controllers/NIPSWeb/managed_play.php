9ab3dbc6b26d1ae4b52aca9010aa1a0f86be53c5
Matthew Stratford <hi@mstratford.net>
2020-11-16 22:01:51 +0000

    Update managed_play to handle auth for API keys.

--- src/Controllers/NIPSWeb/managed_play.php
+++ src/Controllers/NIPSWeb/managed_play.php
@@ -1,16 +1,24 @@
 <?php
 /**
- * Streams a managed database track.
+ * Streams a managed database item (jingles, beds etc).
  */
 use \MyRadio\MyRadioException;
+use \MyRadio\MyRadio\AuthUtils;
 use \MyRadio\NIPSWeb\NIPSWeb_Views;
 use \MyRadio\NIPSWeb\NIPSWeb_ManagedItem;
 
 header("Access-Control-Allow-Origin: " . $_SERVER["HTTP_ORIGIN"]);
 header("Access-Control-Allow-Credentials: true");
 
-if (!isset($_REQUEST['managedid'])) {
-    throw new MyRadioException('Bad Request - managedid required.', 400);
+if (
+    AuthUtils::hasPermission(AUTH_USENIPSWEB) ||
+    AuthUtils::hasPermission(AUTH_DOWNLOAD_LIBRARY)
+) {
+    if (!isset($_REQUEST['managedid'])) {
+        throw new MyRadioException('Bad Request - managedid required.', 400);
+    }
+    $managedid = (int) $_REQUEST['managedid'];
+    NIPSWeb_Views::serveMP3(NIPSWeb_ManagedItem::getInstance($managedid)->getPath());
+} else {
+    throw new MyRadioException('You do not have access to this endpoint.', 403);
 }
-$managedid = (int) $_REQUEST['managedid'];
-NIPSWeb_Views::serveMP3(NIPSWeb_ManagedItem::getInstance($managedid)->getPath());
