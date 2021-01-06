656e938b1da9b33d859c151825974c167f1b33f9
Marks Polakovs <marks@markspolakovs.me>
2021-01-06 21:20:50 +0000

    Make SIS use GeoIP 2

---
+++ src/Classes/MyRadioGeoIP.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace MyRadio;
+
+
+use GeoIp2\Database\Reader;
+
+class MyRadioGeoIP
+{
+    private static ?Reader $reader = null;
+
+    public static function wakeup()
+    {
+        if (Config::$geoip_database_path !== '') {
+            throw new MyRadioException(
+              'GeoIP database path not configured!'
+            );
+        }
+        self::$reader = new Reader(config::$geoip_database_path);
+    }
+
+    /**
+     * @return Reader
+     */
+    public static function getInstance()
+    {
+        if (self::$reader === null) {
+            throw new MyRadioException(
+                'GeoIP not initialised'
+            );
+        }
+        return self::$reader;
+    }
+}
