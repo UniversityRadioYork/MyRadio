376f7fb3e40bebc4d913883c73985634e32dd0eb
Michael Grace <56653532+michael-grace@users.noreply.github.com>
2021-01-10 20:11:08 +0000

    are you gonna finally let this pass travis?

--- src/Public/js/nipsweb.playout.js
+++ src/Public/js/nipsweb.playout.js
@@ -8,13 +8,13 @@ $(document).ready(
           function(playout_data) {
             $("#playout-check").prop("checked", playout_data.payload);
           });
-        });
+      });
   }
-)
+);
 
 $("#playout-check").change(function() {
   myradio.callAPI("GET", "timeslot", "userselectedtimeslot", "", "", "",
     function(timeslot_data) {
       myradio.callAPI("PUT", "timeslot", "playout", timeslot_data.payload.timeslot_id, "", { playout: $("#playout-check").is(":checked") });
     });
-})
+});
