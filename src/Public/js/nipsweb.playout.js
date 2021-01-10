d2b756fb575577f0ff5f4ece1cfa3c3d030382b7
Michael Grace <56653532+michael-grace@users.noreply.github.com>
2021-01-10 20:04:19 +0000

    TAKE THAT TRAVIS
    this better pass the tests now....

--- src/Public/js/nipsweb.playout.js
+++ src/Public/js/nipsweb.playout.js
@@ -1,18 +1,20 @@
+/* global myradio */
+
 $(document).ready(
-    function() {
-        myradio.callAPI("GET", "timeslot", "userselectedtimeslot", "", "", "",
-            function(timeslot_data) {
-                myradio.callAPI("GET", "timeslot", "playout", timeslot_data.payload.timeslot_id, "", "",
-                    function(playout_data) {
-                        $("#playout-check").prop("checked", playout_data.payload);
-                    });
-            });
-    }
+  function() {
+    myradio.callAPI("GET", "timeslot", "userselectedtimeslot", "", "", "",
+      function(timeslot_data) {
+        myradio.callAPI("GET", "timeslot", "playout", timeslot_data.payload.timeslot_id, "", "",
+          function(playout_data) {
+            $("#playout-check").prop("checked", playout_data.payload);
+          });
+        });
+  }
 )
 
 $("#playout-check").change(function() {
-    myradio.callAPI("GET", "timeslot", "userselectedtimeslot", "", "", "",
-        function(timeslot_data) {
-            myradio.callAPI("PUT", "timeslot", "playout", timeslot_data.payload.timeslot_id, "", { playout: $("#playout-check").is(":checked") });
-        });
+  myradio.callAPI("GET", "timeslot", "userselectedtimeslot", "", "", "",
+    function(timeslot_data) {
+      myradio.callAPI("PUT", "timeslot", "playout", timeslot_data.payload.timeslot_id, "", { playout: $("#playout-check").is(":checked") });
+    });
 })
