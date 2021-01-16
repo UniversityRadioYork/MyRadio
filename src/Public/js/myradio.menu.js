e102a09dd8b4b36e0650a4badcc26dea1b375719
michael-grace <michael.dp.grace@outlook.com>
2021-01-16 21:24:29 +0000

    apparenty some broadcast api

--- src/Public/js/myradio.menu.js
+++ src/Public/js/myradio.menu.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 $(".broadcast-unread").click(function(e) {
     myradio.callAPI("GET", "user", "name", e.target.id.toString().split("-")[1], "", "", function(userData) {
         myradio.callAPI("GET", "broadcast", "userbroadcast", "", e.target.id.toString().split("-")[1], "", function(broadcastData) {
-            myradio.createDialog(userData.payload + (userData.payload.charAt(userData.payload.length - 1) == "s" ? "'" : "'s") + " Broadcast", broadcastData[0].path);
+            myradio.createDialog(userData.payload + (userData.payload.charAt(userData.payload.length - 1) == "s" ? "'" : "'s") + " Broadcast", broadcastData.payload[0].path);
         })
     })
 });
