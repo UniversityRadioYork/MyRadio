0e9bd9d7b9e8a0dc50203d7421215c6fba2c689b
SplashyFish14 <68032832+SplashyFish14@users.noreply.github.com>
2021-02-26 21:35:47 +0000

    Update myradio.timeslot.js

--- src/Public/js/myradio.timeslot.js
+++ src/Public/js/myradio.timeslot.js
@@ -119,7 +119,7 @@ $("#timeslots").on(
                     .text("Show Location")
                 );
               var locSel = $("<select name='location'>");
-              var selectOption = $("<option disabled>Please select</option>")
+              var selectOption = $("<option disabled>Please select</option>");
               locSel.append(selectOption);
               for (var loc of window.myradio.locations) {
                 locSel.append(
