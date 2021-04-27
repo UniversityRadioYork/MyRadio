9f825b583312474f697a1de39baab45ca79bb484
Matthew Stratford <hi@mstratford.net>
2021-04-27 19:37:14 +0100

    Move selsources to separate patch file

--- schema/base.sql
+++ schema/base.sql
@@ -2108,11 +2108,6 @@ CREATE TABLE selbaps (
     bapsloc integer NOT NULL
 );
 COMMENT ON TABLE selbaps IS 'Marries selector actions with BAPS servers';
-CREATE TABLE selsources (
-    selaction integer NOT NULL,
-    sourceid character(1) NOT NULL
-);
-COMMENT ON TABLE selsources IS 'Marries selector actions with tracklist sources';
 CREATE TABLE source (
     sourceid character(1) NOT NULL,
     source text NOT NULL
@@ -6609,22 +6604,6 @@ ALTER TABLE ONLY selbaps
 
 ALTER TABLE ONLY selbaps
     ADD CONSTRAINT selbaps_selaction_fkey FOREIGN KEY (selaction) REFERENCES public.selector_actions(action);
-
-
---
--- Name: selsources_sourceid_fkey; Type: FK CONSTRAINT; Schema: tracklist
---
-
-ALTER TABLE ONLY selsources
-    ADD CONSTRAINT selsources_sourceid_fkey FOREIGN KEY (sourceid) REFERENCES tracklist.source(sourceid);
-
-
---
--- Name: selsources_selaction_fkey; Type: FK CONSTRAINT; Schema: tracklist
---
-
-ALTER TABLE ONLY selsources
-    ADD CONSTRAINT selsources_selaction_fkey FOREIGN KEY (selaction) REFERENCES public.selector_actions(action);
 
 
 --
