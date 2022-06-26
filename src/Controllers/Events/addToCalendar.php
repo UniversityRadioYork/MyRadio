13046a2245915ae9a2aadb37bedd700c2498a825
Marks Polakovs <marks@markspolakovs.me>
2022-06-26 18:48:32 +0100

    Initial iCal implementation

---
+++ src/Controllers/Events/addToCalendar.php
@@ -0,0 +1,28 @@
+<?php
+
+use MyRadio\MyRadio\CoreUtils;
+use MyRadio\MyRadio\URLUtils;
+use MyRadio\MyRadioException;
+use MyRadio\ServiceAPI\MyRadio_Event;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    switch ($_POST['action']) {
+        case 'regenerate':
+            MyRadio_Event::revokeCalendarTokenFor();
+            break;
+        default:
+            throw new MyRadioException('Unknown action', 400);
+    }
+}
+
+$token = MyRadio_Event::getCalendarTokenFor();
+if ($token === null) {
+    $token = MyRadio_Event::createCalendarTokenFor();
+}
+
+CoreUtils::getTemplateObject()
+    ->setTemplate('Events/addToCalendar.twig')
+    ->addVariable('link', URLUtils::makeURL('Events', 'iCal', [
+        'token' => $token
+    ]))
+    ->render();
---
+++ src/Controllers/Events/iCal.php
@@ -0,0 +1,37 @@
+<?php
+
+use MyRadio\Config;
+use MyRadio\MyRadio\URLUtils;
+use MyRadio\ServiceAPI\MyRadio_Event;
+use Spatie\IcalendarGenerator\Components\Calendar;
+
+if (empty($_GET['token'])) {
+    URLUtils::dataToJSON([
+        'myradio_errors' => [
+            'no_token'
+        ]
+    ]);
+    exit;
+}
+
+$memberid = MyRadio_Event::validateCalendarToken($_GET['token']);
+if ($memberid === null) {
+    URLUtils::dataToJSON([
+        'myradio_errors' => [
+            'invalid'
+        ]
+    ]);
+    exit;
+}
+// TODO: when we support RSVPs, filter here
+
+$events = MyRadio_Event::getNext(25);
+
+$cal = Calendar::create(Config::$long_name)
+    ->refreshInterval(360);
+foreach ($events as $evt) {
+    $cal = $cal->event($evt->toIcalEvent());
+}
+
+header('Content-Type: text/calendar');
+echo $cal->get();
