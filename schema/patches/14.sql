1ab17bf7d35be683b50a373a74098649a15caed6
Marks Polakovs <marks@markspolakovs.me>
2022-10-15 13:25:26 +0100

    Fix patch rebase

--- schema/patches/14.sql
+++ schema/patches/14.sql
@@ -1,9 +1,11 @@
 BEGIN;
-CREATE TABLE schedule.highlight (
-    highlight_id SERIAL PRIMARY KEY,
-    show_season_timeslot_id INTEGER UNIQUE REFERENCES schedule.show_season_timeslot(show_season_timeslot_id) ON DELETE CASCADE,
-    start_time TIMESTAMPTZ NOT NULL,
-    end_time TIMESTAMPTZ NOT NULL,
-    notes TEXT DEFAULT ''
+CREATE TABLE public.calendar_tokens (
+                                        tokenid SERIAL PRIMARY KEY,
+                                        memberid INTEGER NOT NULL REFERENCES public.member(memberid),
+                                        token_str TEXT NOT NULL UNIQUE,
+                                        revoked BOOLEAN DEFAULT 'f'
 );
+UPDATE myradio.schema
+SET value = 14
+WHERE attr='version';
 COMMIT;
---
+++ schema/patches/16.sql
@@ -0,0 +1,12 @@
+BEGIN;
+CREATE TABLE schedule.highlight (
+    highlight_id SERIAL PRIMARY KEY,
+    show_season_timeslot_id INTEGER UNIQUE REFERENCES schedule.show_season_timeslot(show_season_timeslot_id) ON DELETE CASCADE,
+    start_time TIMESTAMPTZ NOT NULL,
+    end_time TIMESTAMPTZ NOT NULL,
+    notes TEXT DEFAULT ''
+);
+UPDATE myradio.schema
+SET value = 16
+WHERE attr='version';
+COMMIT;
