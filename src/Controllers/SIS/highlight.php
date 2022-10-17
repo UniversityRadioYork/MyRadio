9941938ca16f1f8518a1cb9e24e693aeda687781
Marks Polakovs <marks@markspolakovs.me>
2022-10-17 10:53:35 +0100

    Ues timeslot from session in SIS module

--- src/Controllers/SIS/highlight.php
+++ src/Controllers/SIS/highlight.php
@@ -4,7 +4,7 @@ use MyRadio\ServiceAPI\MyRadio_Highlight;
 use MyRadio\ServiceAPI\MyRadio_Timeslot;
 
 $now = time();
-$ts = MyRadio_Timeslot::getCurrentTimeslot($now);
+$ts = MyRadio_Timeslot::getInstance($_SESSION['timeslotid']);
 if (isset($_REQUEST['lastSegment'])) {
     $hl = MyRadio_Highlight::createFromLastSegment($ts->getID(), 10, $_REQUEST['notes'] ?? '');
 } else {
