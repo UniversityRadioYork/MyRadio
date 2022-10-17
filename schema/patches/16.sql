c47d78d7b3d299d6ecc83a9ef47d9cb420caf965
Marks Polakovs <marks@markspolakovs.me>
2022-10-17 11:02:42 +0100

    Remove unique constraint from highlight timeslot

--- schema/patches/16.sql
+++ schema/patches/16.sql
@@ -1,7 +1,7 @@
 BEGIN;
 CREATE TABLE schedule.highlight (
     highlight_id SERIAL PRIMARY KEY,
-    show_season_timeslot_id INTEGER UNIQUE REFERENCES schedule.show_season_timeslot(show_season_timeslot_id) ON DELETE CASCADE,
+    show_season_timeslot_id INTEGER REFERENCES schedule.show_season_timeslot(show_season_timeslot_id) ON DELETE CASCADE,
     start_time TIMESTAMPTZ NOT NULL,
     end_time TIMESTAMPTZ NOT NULL,
     notes TEXT DEFAULT ''
