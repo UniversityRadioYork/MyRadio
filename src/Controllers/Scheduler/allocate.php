09ff4be40933408b438ea95f8e52e368803bfcbf
JP5457 <jp1869@york.ac.uk>
2023-07-15 11:27:21 +0100

    fixed allocate form

--- src/Controllers/Scheduler/allocate.php
+++ src/Controllers/Scheduler/allocate.php
@@ -9,14 +9,13 @@ use \MyRadio\MyRadioException;
 
 $current_term_info = MyRadio_Scheduler::getActiveApplicationTermInfo();
 $term_weeks = $current_term_info['weeks'];
-$term_start = $current_term_info['start'];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     //Submitted
 
     // Note Slightly ugly hack to get the season ID from the submitted form
     $season = MyRadio_Season::getInstance($_POST['sched_allocate-season_id']);
-    $data = $season->getAllocateForm($term_weeks, $term_start)->readValues();
+    $data = $season->getAllocateForm()->readValues();
     $season->schedule($data, $term_weeks);
 
     URLUtils::redirectWithMessage('Scheduler', 'default', 'Season Allocated!');
@@ -26,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         throw new MyRadioException('There is not currently a term you can apply/schedule for.', 400);
     } else {
         $season = MyRadio_Season::getInstance($_REQUEST['show_season_id']);
-        $season->getAllocateForm($term_weeks, $term_start)
+        $season->getAllocateForm()
             ->render($season->toDataSource());
     }
 }
