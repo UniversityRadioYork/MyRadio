521314e6db61d9e7abb112fb76a3fd0dee50acca
Kory <kaneanderson1909@gmail.com>
2023-12-31 05:31:45 +0000

    more tweaks

--- src/Classes/ServiceAPI/MyRadio_Messages.php
+++ src/Classes/ServiceAPI/MyRadio_Messages.php
@@ -2,12 +2,18 @@
 namespace MyRadio\ServiceAPI;
 
 use MyRadio\SIS\SIS_Messages; // Use correct case
+use MyRadio\MyRadioException; // Import missing class
 
 class MyRadio_Messages extends ServiceAPI
 {
-  public function markRead($messageId)
+  public static function markread($id = null)
   {
-    SIS_Messages::setMessageStatus(intval($_GET['id']), SIS_Messages::MSG_STATUS_READ);
-    header('HTTP/1.1 204 No Content');
+    if ($id === null) {
+      throw new MyRadioException('No message ID provided', 400); // Fix the problem by using the imported class
+    }
+
+    SIS_Messages::setMessageStatus(intval($id), SIS_Messages::MSG_STATUS_READ);
+
+    return ['status' => 204, 'content' => 'No Content'];
   }
 }
