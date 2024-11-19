a995df978bfa7105ce62040c1538402976577e47
Colin <9614541+ColinRoitt@users.noreply.github.com>
2024-11-19 15:32:23 +0000

    Update podcast art size requirements

--- src/Classes/ServiceAPI/MyRadio_Podcast.php
+++ src/Classes/ServiceAPI/MyRadio_Podcast.php
@@ -363,7 +363,7 @@ class MyRadio_Podcast extends MyRadio_Metadata_Common
                 [
                     'label' => 'Upload New Cover Photo',
                     'explanation' => 'If you haven\'t specified an existing cover photo, upload one '
-                    . 'here - it should be square and at least 400x400 pixels.',
+                    . 'here - it should be square and at least 3000x3000 pixels.',
                     'required' => false,
                 ]
             )
