3324cc57cc82933713ca58b25591363e28e4d7d5
Colin Roitt <roittcolin@gmail.com>
2025-04-16 17:27:33 +0100

    Add patch for cancel timeslot coming through API

--- src/Classes/ServiceAPI/MyRadio_Timeslot.php
+++ src/Classes/ServiceAPI/MyRadio_Timeslot.php
@@ -928,6 +928,11 @@ class MyRadio_Timeslot extends MyRadio_Metadata_Common
      */
     public function cancelTimeslot($reason)
     {
+        //  If no active session we must have come through API so use admin
+        if (!isset($_SESSION['memberid'])) {
+            $r = $this->cancelTimeslotAdmin($reason);
+            return $r;
+        }
         //Get if the User has permission to drop the episode
         if (MyRadio_User::getInstance()->hasAuth(AUTH_DELETESHOWS)) {
             //Yep, do an administrative drop
